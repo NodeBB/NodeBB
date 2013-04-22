1d35f369d9972f0a8dd340f8c0dae32adb0a3d9e
psychobunny <rodrigues.andrew@gmail.com>
2013-04-22 19:13:39 +0000

    clean up client side + config.json

---
+++ public/src/templates.js
@@ -0,0 +1,19 @@
+var templates = {};
+
+function loadTemplates(templatesToLoad) {
+	var timestamp = new Date().getTime();
+
+	for (var t in templatesToLoad) {
+		(function(template) {
+			$.get('templates/' + template + '.tpl?v=' + timestamp, function(html) {
+				templates[template] = html;
+			});
+		}(templatesToLoad[t]));
+	}
+}
+
+function templates_init() {
+	loadTemplates(['register', 'home', 'login']);	
+}
+
+templates_init();
