bdbbed18ae448162c13342be6a7846378e1d3c6a
Julian Lam <julian.lam@gmail.com>
2013-04-23 16:44:31 -0400

    adding new default config.js file, removed config from repo

--- config.default.js
+++ config.default.js
@@ -1,10 +1,10 @@
 var config = {
-	"url": "http://designcreateplay.com:4567/",
+	"url": "http://localhost:4567/",
 	"port": 4567,
 	"mailer": {
 		host: 'localhost',
 		port: '25',
-		from: 'mailer@designcreateplay.com'
+		from: 'mailer@localhost.lan'
 	}
 }
 
