c298e7002ec8bcd2e7cb6293a90566b395f39719
Julian Lam <julian.lam@gmail.com>
2013-05-01 12:54:04 -0400

    updated configuration file with redis params, and updated all redis wrapper to read configs from config file instead of defaulting to localhost -- same for express.session store

--- config.default.js
+++ config.default.js
@@ -1,11 +1,17 @@
 var config = {
 	"base_url": "http://localhost",
 	"port": 4567,
-	"url": undefined,	// Leave this alone
 	"mailer": {
 		host: 'localhost',
 		port: '25',
 		from: 'mailer@localhost.lan'
+	},
+	"redis": {
+		port: "6379",
+		host: "127.0.0.1",
+		options: {
+			
+		}
 	}
 }
 
