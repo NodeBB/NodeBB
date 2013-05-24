289219d0ee10f989a8014bc420e869ceea6a7298
Julian Lam <julian.lam@gmail.com>
2013-05-24 08:39:39 -0400

    adding email header and footer

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ global.templates = {};
 
     templates.init([
         'header', 'footer', 'logout', 'admin/header', 'admin/footer', 'admin/index',
-        'emails/reset', 'emails/reset_plaintext', 'emails/email_confirm', 'emails/email_confirm_plaintext'
+        'emails/reset', 'emails/reset_plaintext', 'emails/email_confirm', 'emails/email_confirm_plaintext',
+        'emails/header.tpl', 'emails/footer.tpl'
     ]);
     
     templates.ready(function() {
