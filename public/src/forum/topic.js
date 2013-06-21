eff3fc9b7a1397551f2e9ce29150c01a3d6379b8
Baris Soner Usakli <barisusakli@gmail.com>
2013-06-21 15:35:00 -0400

    fixed error in favouriting posts

--- public/src/forum/topic.js
+++ public/src/forum/topic.js
@@ -445,14 +445,24 @@
 				uid = ids[1];
 
 			if (thread_state.locked !== '1') {
-				if (this.children[2].className == 'icon-star-empty') {
+				var element = $(this).find('i');
+				if(element.attr('class') == 'icon-star-empty') {
+					element.attr('class', 'icon-star');
+					socket.emit('api:posts.favourite', {pid: pid, room_id: app.current_room});
+				}
+				else {
+					element.attr('class', 'icon-star-empty');
+					socket.emit('api:posts.unfavourite', {pid: pid, room_id: app.current_room});
+				}
+				
+				/*if (this.children[2].className == 'icon-star-empty') {
 					this.children[2].className = 'icon-star';
 					socket.emit('api:posts.favourite', {pid: pid, room_id: app.current_room});
 				}
 				else {
 					this.children[2].className = 'icon-star-empty';
 					socket.emit('api:posts.unfavourite', {pid: pid, room_id: app.current_room});
-				}
+				}*/
 			}
 		});
 	}
