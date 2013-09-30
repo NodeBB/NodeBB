504e2aac4aa48ff6e8566ebee1ae078e0e612fa3
Quinton Marchi <cardinal@iamcardinal.com>
2013-09-30 02:42:34 -0400

    Adds: regex for ports, mini for public/src/forum
    + Regular expression for port during install
    + Minification for public/src/forum JS files
    + Minification for public/src/forum/admin JS files

--- src/install.js
+++ src/install.js
@@ -18,7 +18,9 @@ var async = require('async'),
 		}, {
 			name: 'port',
 			description: 'Port number of your NodeBB',
-			'default': 4567
+			'default': 4567,
+            pattern: /[0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5]/,
+            message: 'Please enter a value betweeen 1 and 65535'
 		}, {
 			name: 'use_port',
 			description: 'Use a port number to access NodeBB?',
