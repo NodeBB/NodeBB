1fb09a9c8c0f4a3a7f936c80e8aa233b859c0045
Julian Lam <julian@designcreateplay.com>
2013-10-28 15:15:59 -0400

    merging @deniswolf's new tests, and double-checking that generateUUID still works

--- src/routes/debug.js
+++ src/routes/debug.js
@@ -1,12 +1,18 @@
 var	DebugRoute = function(app) {
-		var	Notifications = require('../notifications');
-
 		app.namespace('/debug', function() {
 			app.get('/prune', function(req, res) {
+				var	Notifications = require('../notifications');
+
 				Notifications.prune(new Date(), function() {
 					console.log('done');
 				});
 				res.send();
+			});
+
+			app.get('/uuidtest', function(req, res) {
+				var	Utils = require('../../public/src/utils.js');
+
+				res.send(Utils.generateUUID());
 			});
 		});
 	};
