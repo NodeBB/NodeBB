9ee250b59791ab9d7c37aafe73545d58b01d022d
Baris Usakli <barisusakli@gmail.com>
2013-12-06 16:26:07 -0500

    fixes humanreadable numbers in infinite scrolling

--- public/src/forum/unread.js
+++ public/src/forum/unread.js
@@ -79,9 +79,10 @@ define(function() {
 
 			$('#category-no-topics').remove();
 
+			html = $(html);
 			container.append(html);
 			$('span.timeago').timeago();
-			app.makeNumbersHumanReadable($(html).find('.human-readable-number'));
+			app.makeNumbersHumanReadable(html.find('.human-readable-number'));
 		}
 
 		function loadMoreTopics() {
