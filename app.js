d1d6605dcf52c085d2bc57aee45a71b3cd78a8a4
Baris Soner Usakli <barisusakli@gmail.com>
2013-12-07 16:51:47 -0500

    closes #501

--- app.js
+++ app.js
@@ -48,6 +48,13 @@
 		winston.error(err.stack);
 	};
 
+	require('child_process').exec('/usr/bin/which convert', function(err, stdout, stderr) {
+		if(err) {
+			winston.warn('Counldn\'t find convert. Did you install imagemagick? ', err.message);
+		}
+		winston.warn('Counldn\'t find convert. Did you install imagemagick? ', err);
+	});
+
 	// Log GNU copyright info along with server info
 	winston.info('NodeBB v' + pkg.version + ' Copyright (C) 2013 DesignCreatePlay Inc.');
 	winston.info('This program comes with ABSOLUTELY NO WARRANTY.');
