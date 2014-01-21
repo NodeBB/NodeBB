7c16a8be31e168ed7b5744b579df3ffa67c905b2
Baris Soner Usakli <barisusakli@gmail.com>
2014-01-20 21:29:14 -0500

    recent reply fix

--- public/src/forum/category.js
+++ public/src/forum/category.js
@@ -153,13 +153,14 @@ define(['composer'], function(composer) {
 		var replies = '';
 		for (var i = 0, numPosts = posts.length; i < numPosts; ++i) {
 
-			replies += '<li data-pid="'+ posts[i].pid +'">' +
+			replies += '<li data-pid="'+ posts[i].pid +'" class="clearfix">' +
 						'<a href="' + RELATIVE_PATH + '/user/' + posts[i].userslug + '"><img title="' + posts[i].username + '" class="img-rounded user-img" src="' + posts[i].picture + '"/></a>' +
 						'<strong><span>'+ posts[i].username + '</span></strong>' +
-						'<a href="' + RELATIVE_PATH + '/topic/' + posts[i].topicSlug + '#' + posts[i].pid + '">' +
-							'<p>' +	posts[i].content + '</p>' +
-						'</a>' +
-						'<span class="timeago pull-right" title="' + posts[i].relativeTime + '"></span>' +
+						'<p>' +	posts[i].content + '</p>' +
+						'<span class="pull-right">'+
+							'<a href="' + RELATIVE_PATH + '/topic/' + posts[i].topicSlug + '#' + posts[i].pid + '">posted</a> '+
+							'<span class="timeago" title="' + posts[i].relativeTime + '"></span>' +
+						'</span>'+
 					   '</li>';
 		}
 
