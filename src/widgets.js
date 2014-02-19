b3978d53bb0bd35c7fc3d59741c56287ab8ce56c
psychobunny <psycho.bunny@hotmail.com>
2014-02-19 17:07:47 -0500

    widgets.render

--- src/widgets.js
+++ src/widgets.js
@@ -1,9 +1,36 @@
 var async = require('async'),
 	winston = require('winston'),
+	plugins = require('./plugins'),
 	db = require('./database');
 
 
 (function(Widgets) {
+
+	Widgets.render = function(uid, area, callback) {
+		if (!area.location || !area.template) {
+			callback({
+				error: 'Missing location and template data'
+			});
+		}
+
+		var rendered = [];
+
+		Widgets.getArea(area.template, area.location, function(err, widgets) {
+			async.each(widgets, function(widget, next) {
+				plugins.fireHook('filter:widget.render:' + widget.widget, {
+					uid: uid,
+					area: area,
+					data: widget.data
+				}, function(err, html){
+					rendered.push({
+						html: html
+					});
+				});
+			}, function(err) {
+				callback(err, rendered);
+			});
+		});
+	};
 
 	Widgets.getArea = function(template, location, callback) {
 		db.getObjectField('widgets:' + template, location, function(err, widgets) {
@@ -11,14 +38,14 @@ var async = require('async'),
 		})
 	};
 
-	Widgets.setArea = function(data, callback) {
-		if (!data.location || !data.template) {
+	Widgets.setArea = function(area, callback) {
+		if (!area.location || !data.template) {
 			callback({
 				error: 'Missing location and template data'
 			});
 		}
 		
-		db.setObjectField('widgets:' + data.template, data.location, JSON.stringify(data.widgets), function(err) {
+		db.setObjectField('widgets:' + area.template, area.location, JSON.stringify(area.widgets), function(err) {
 			callback(err);
 		});
 	};
