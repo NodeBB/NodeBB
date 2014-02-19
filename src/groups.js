6496ad95e8cef514690eb0fb298ec117dd782a47
psychobunny <psycho.bunny@hotmail.com>
2014-02-19 02:07:48 -0500

    Revert "closes #992"
    This reverts commit ed4b0461b9ce6559b90391768856c38abc6f89b4.

--- src/groups.js
+++ src/groups.js
@@ -13,16 +13,14 @@
 						expand: options.expand
 					}, next);
 				}, function (err, groups) {
-					groups.forEach(function(group, g, arr) {
-						if (parseInt(group.deleted, 10) === 1) {
-							delete arr[g];
+					// Remove deleted and hidden groups from this list
+					callback(err, groups.filter(function (group) {
+						if (parseInt(group.deleted, 10) === 1 || parseInt(group.hidden, 10) === 1) {
+							return false;
+						} else {
+							return true;
 						}
-						if (parseInt(group.hidden, 10) === 1) {
-							group.deletable = 0;
-						}
-					});
-
-					callback(err, groups);
+					}));
 				});
 			} else {
 				callback(null, []);
