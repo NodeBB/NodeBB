7c49c32ad311b812747068c02ef93658d15d4290
Baris Soner Usakli <barisusakli@gmail.com>
2014-02-24 15:23:26 -0500

    scroll fix

--- public/src/forum/topic.js
+++ public/src/forum/topic.js
@@ -1055,7 +1055,6 @@ define(['composer', 'forum/pagination'], function(composer, pagination) {
 			offset = 0;
 		}
 
-
 		if($('#post_anchor_' + pid).length) {
 			return scrollToPid(pid);
 		}
@@ -1093,8 +1092,8 @@ define(['composer', 'forum/pagination'], function(composer, pagination) {
 				tid = $('#post-container').attr('data-tid');
 
 			function animateScroll() {
-				$('window,html').animate({
-					scrollTop: scrollTo.offset().top - $('#header-menu').height() - offset
+				$("html, body").animate({
+					scrollTop: (scrollTo.offset().top - $('#header-menu').height() - offset) + "px"
 				}, duration !== undefined ? duration : 400, function() {
 					updateHeader();
 				});
