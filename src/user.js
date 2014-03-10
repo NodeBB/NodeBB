a7f310b49a7e53ca8cce64293b4012d73975ceba
Julian Lam <julian@designcreateplay.com>
2014-03-10 15:31:47 -0400

    fixed #1189

--- src/user.js
+++ src/user.js
@@ -201,7 +201,7 @@ var bcrypt = require('bcryptjs'),
 		function sendDefaultSettings() {
 			callback(null, {
 				showemail: false,
-				usePagination: parseInt(meta.config.usePagination, 10) !== 0,
+				usePagination: parseInt(meta.config.usePagination, 10) === 1,
 				topicsPerPage: parseInt(meta.config.topicsPerPage, 10) || 20,
 				postsPerPage: parseInt(meta.config.postsPerPage, 10) || 10
 			});
@@ -221,7 +221,7 @@ var bcrypt = require('bcryptjs'),
 			}
 
 			settings.showemail = settings.showemail ? parseInt(settings.showemail, 10) !== 0 : false;
-			settings.usePagination = settings.usePagination ? parseInt(settings.usePagination, 10) !== 0 : parseInt(meta.config.usePagination, 10) !== 0;
+			settings.usePagination = settings.usePagination ? parseInt(settings.usePagination, 10) === 1 : parseInt(meta.config.usePagination, 10) === 1;
 			settings.topicsPerPage = settings.topicsPerPage ? parseInt(settings.topicsPerPage, 10) : parseInt(meta.config.topicsPerPage, 10) || 20;
 			settings.postsPerPage = settings.postsPerPage ? parseInt(settings.postsPerPage, 10) : parseInt(meta.config.postsPerPage, 10) || 10;
 
