4793b6b4f90c18b620b3d709f5166ecc6f8dca70
psychobunny <psycho.bunny@hotmail.com>
2014-03-09 20:08:51 -0400

    fixed invalid path for apple-touch-icon

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -311,7 +311,7 @@ middleware.routeTouchIcon = function(req, res) {
 	if (meta.config['brand:logo'] && validator.isURL(meta.config['brand:logo'])) {
 		return res.redirect(meta.config['brand:logo']);
 	} else {
-		return res.sendfile(path.join(__dirname, '../public', meta.config['brand:logo'] || '/logo.png'), {
+		return res.sendfile(path.join(__dirname, '../../public', meta.config['brand:logo'] || '/logo.png'), {
 			maxAge: app.enabled('cache') ? 5184000000 : 0
 		});
 	}
