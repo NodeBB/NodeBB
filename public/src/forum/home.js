a5f4135c6274dd4f269948c94381b3c153fd5ef4
barisusakli <barisusakli@gmail.com>
2014-03-11 15:15:48 -0400

    added socket listeners to home

--- public/src/forum/home.js
+++ public/src/forum/home.js
@@ -1,8 +1,24 @@
+'use strict';
+
 define(function() {
 	var	home = {};
 
 	home.init = function() {
 
+		app.enterRoom('home');
+
+		ajaxify.register_events([
+			'event:new_topic',
+			'event:new_post'
+		]);
+
+		socket.on('event:new_topic', function(data) {
+
+		});
+
+		socket.on('event:new_post', function(data) {
+
+		});
 	}
 
 	return home;
