585eb9e1be780f0a40b07b4f15a0404baefb35d1
Julian Lam <julian@designcreateplay.com>
2014-03-26 11:39:27 -0400

    fixed #1274

--- src/controllers/static.js
+++ src/controllers/static.js
@@ -1,17 +1,32 @@
 "use strict";
 
-var staticController = {};
+var staticController = {},
+	isApi = function(path) {
+		return !!path.match('api');
+	};
 
 staticController['404'] = function(req, res, next) {
-	res.status(404).render('404', {});
+	if (!isApi(req.path)) {
+		res.statusCode = 404;
+	}
+
+	res.render('404', {});
 };
 
 staticController['403'] = function(req, res, next) {
-	res.status(403).render('403', {});
+	if (!isApi(req.path)) {
+		res.statusCode = 403;
+	}
+
+	res.render('403', {});
 };
 
 staticController['500'] = function(req, res, next) {
-	res.status(500).render('500', {});
+	if (!isApi(req.path)) {
+		res.statusCode = 500;
+	}
+
+	res.render('500', {});
 };
 
 module.exports = staticController;
