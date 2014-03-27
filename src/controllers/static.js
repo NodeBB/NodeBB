824d849a5298a5b44452a0ef0f120a96750c6d7f
barisusakli <barisusakli@gmail.com>
2014-03-27 03:12:23 -0400

    removed req

--- src/controllers/static.js
+++ src/controllers/static.js
@@ -3,18 +3,18 @@
 var staticController = {};
 
 staticController['404'] = function(req, res) {
-	renderStatic(404, req, res);
+	renderStatic(404, res);
 };
 
 staticController['403'] = function(req, res) {
-	renderStatic(403, req, res);
+	renderStatic(403, res);
 };
 
 staticController['500'] = function(req, res) {
-	renderStatic(500, req, res);
+	renderStatic(500, res);
 };
 
-function renderStatic(statusCode, req, res) {
+function renderStatic(statusCode, res) {
 	if (!res.locals.isAPI) {
 		res.statusCode = statusCode;
 	}
