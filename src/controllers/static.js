a3842d2c612443913ac664a39309239f0f2774f0
barisusakli <barisusakli@gmail.com>
2014-03-26 23:29:50 -0400

    removed similar code

--- src/controllers/static.js
+++ src/controllers/static.js
@@ -2,28 +2,24 @@
 
 var staticController = {};
 
-staticController['404'] = function(req, res, next) {
-	if (!res.locals.isAPI) {
-		res.statusCode = 404;
-	}
-
-	res.render('404', {});
+staticController['404'] = function(req, res) {
+	renderStatic(404, req, res);
 };
 
-staticController['403'] = function(req, res, next) {
-	if (!res.locals.isAPI) {
-		res.statusCode = 403;
-	}
-
-	res.render('403', {});
+staticController['403'] = function(req, res) {
+	renderStatic(403, req, res);
 };
 
-staticController['500'] = function(req, res, next) {
+staticController['500'] = function(req, res) {
+	renderStatic(500, req, res);
+};
+
+function renderStatic(statusCode, req, res) {
 	if (!res.locals.isAPI) {
-		res.statusCode = 500;
+		res.statusCode = statusCode;
 	}
 
-	res.render('500', {});
-};
+	res.render(statusCode.toString(), {});
+}
 
 module.exports = staticController;
