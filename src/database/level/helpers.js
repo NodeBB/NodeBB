268be5f35c680aeb6216edec27d72748f5ccef5a
psychobunny <psycho.bunny@hotmail.com>
2014-04-14 17:14:09 -0400

    fixed LevelDB helpers

--- src/database/level/helpers.js
+++ src/database/level/helpers.js
@@ -7,7 +7,7 @@ helpers.iterator = function(fn, keys, value, callback) {
 	var results = [];
 
 	async.each(keys, function(key, next) {
-		module[fn](key, value, function(err, result) {
+		module.parent.exports[fn](key, value, function(err, result) {
 			results.push(result);
 			next();
 		});
