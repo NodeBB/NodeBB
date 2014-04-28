04c69a9327684abe28fade0c393c0c13863fbdc6
psychobunny <psycho.bunny@hotmail.com>
2014-04-28 17:16:22 -0400

    changed the copyright from our usernames to just NodeBB

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'NodeBB'
-copyright = u'2014, psychobunny, julianlam, barisusakli'
+copyright = u'2014, NodeBB'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
