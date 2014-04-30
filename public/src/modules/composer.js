4b18403fa62672fcc5538d08955d5ffb7a6ef911
psychobunny <psycho.bunny@hotmail.com>
2014-04-30 14:22:51 -0400

    moving controls object into its own module

--- public/src/modules/composer.js
+++ public/src/modules/composer.js
@@ -17,6 +17,10 @@ define(['taskbar'], function(taskbar) {
 				socket.emit('modules.composer.pingActive', post_uuid);
 			}
 		});
+
+		require(['composer/controls'], function(composerControls) {
+			controls = composerControls;
+		});
 	}
 
 	initialise();
@@ -415,43 +419,6 @@ define(['taskbar'], function(taskbar) {
 		});
 		thumbForm.submit();
 	}
-
-
-	/*************************************************/
-	/* Rich Textarea Controls                        */
-	/*************************************************/
-	controls.insertIntoTextarea = function(textarea, value) {
-		var $textarea = $(textarea);
-		var currentVal = $textarea.val();
-
-		$textarea.val(
-			currentVal.slice(0, textarea.selectionStart) +
-			value +
-			currentVal.slice(textarea.selectionStart)
-		);
-	};
-
-	controls.wrapSelectionInTextareaWith = function(textarea, leading, trailing){
-		if(trailing === undefined){
-			trailing = leading;
-		}
-
-		var $textarea = $(textarea);
-		var currentVal = $textarea.val()
-
-		$textarea.val(
-			currentVal.slice(0, textarea.selectionStart) +
-			leading +
-			currentVal.slice(textarea.selectionStart, textarea.selectionEnd) +
-			trailing +
-			currentVal.slice(textarea.selectionEnd)
-		);
-	};
-
-	controls.updateTextareaSelection = function(textarea, start, end){
-		textarea.setSelectionRange(start, end);
-		$(textarea).focus();
-	};
 
 	var formattingDispatchTable = {
 		'fa fa-bold': function(textarea, selectionStart, selectionEnd){
