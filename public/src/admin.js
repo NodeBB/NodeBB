033364a2a96fcef867c0b97b60da43127b5336f3
psychobunny <psycho.bunny@hotmail.com>
2014-05-15 17:16:17 -0400

    move code from footer.js into admin.js

--- public/src/admin.js
+++ public/src/admin.js
@@ -1,4 +1,5 @@
 "use strict";
+/*global app, socket*/
 
 var admin = {};
 
@@ -20,4 +21,44 @@ var admin = {};
 		});		
 	};
 
+	$(function() {
+		var menuEl = $('.sidebar-nav'),
+			liEls = menuEl.find('li'),
+			parentEl,
+			activate = function(li) {
+				liEls.removeClass('active');
+				li.addClass('active');
+			};
+
+		// also on ready, check the pathname, maybe it was a page refresh and no item was clicked
+		liEls.each(function(i, li){
+			li = $(li);
+			if ((li.find('a').attr('href') || '').indexOf(location.pathname) >= 0) {
+				activate(li);
+			}
+		});
+
+		// On menu click, change "active" state
+		menuEl.on('click', function(e) {
+			parentEl = $(e.target).parent();
+			if (parentEl.is('li')) {
+				activate(parentEl);
+			}
+		});
+	});
+
+	socket.emit('admin.config.get', function(err, config) {
+		if(err) {
+			return app.alert({
+				alert_id: 'config_status',
+				timeout: 2500,
+				title: 'Error',
+				message: 'NodeBB encountered a problem getting config',
+				type: 'danger'
+			});
+		}
+
+		// move this to admin.config
+		app.config = config;
+	});
 }());
