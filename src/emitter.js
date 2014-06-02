58912467000af7108bec3af1379a6e5a1a762ef1
psychobunny <psycho.bunny@hotmail.com>
2014-06-02 17:33:44 -0400

    emitter.all

--- src/emitter.js
+++ src/emitter.js
@@ -3,4 +3,21 @@
 var events = require('events'),
 	eventEmitter = new events.EventEmitter();
 
+
+eventEmitter.all = function(events, callback) {
+	for (var ev in events) {
+		if (events.hasOwnProperty(ev)) {
+			(function(ev) {
+				eventEmitter.on(events[ev], function() {
+					events.splice(events.indexOf(ev), 1);
+
+					if (events.length === 0) {
+						callback();
+					}
+				});
+			}(ev));
+		}
+	}
+};
+
 module.exports = eventEmitter;
