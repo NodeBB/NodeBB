0cbb4406292b4029f32146e34a81e3188dc24049
barisusakli <barisusakli@gmail.com>
2014-06-24 12:52:04 -0400

    fix blockquote toggle and use different icons

--- public/src/forum/topic.js
+++ public/src/forum/topic.js
@@ -128,12 +128,17 @@ define('forum/topic', ['forum/pagination', 'forum/infinitescroll', 'forum/topic/
 		blockquotes.each(function() {
 			var $this = $(this);
 			if ($this.find(':hidden').length && !$this.find('.toggle').length) {
-				$this.append('<i class="fa fa-ellipsis-h pointer toggle"></i>');
+				$this.append('<i class="fa fa-angle-down pointer toggle"></i>');
 			}
 		});
 
-		$('.post-container').on('click', 'blockquote .toggle', function() {
-			$(this).parent('blockquote').toggleClass('uncollapsed');
+		$('#post-container').on('click', 'blockquote .toggle', function() {
+			var blockQuote = $(this).parent('blockquote');
+			var toggle = $(this);
+			blockQuote.toggleClass('uncollapsed');
+			var collapsed = !blockQuote.hasClass('uncollapsed');
+			toggle.toggleClass('fa-angle-down', collapsed).toggleClass('fa-angle-up', !collapsed);
+
 		});
 	}
 
