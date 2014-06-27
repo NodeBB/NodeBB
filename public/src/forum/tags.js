fb72b4c8e01e6233d482afd69ff441d62166070d
barisusakli <barisusakli@gmail.com>
2014-06-26 23:05:47 -0400

    only search tag value

--- public/src/forum/tags.js
+++ public/src/forum/tags.js
@@ -11,7 +11,7 @@ define('forum/tags', function() {
 		$('#tag-search').on('input propertychange', function() {
 			$('.tag-list a').each(function() {
 				var $this = $(this)
-				$this.toggleClass('hide', $this.attr('href').indexOf($('#tag-search').val()) === -1);
+				$this.toggleClass('hide', $this.attr('data-value').indexOf($('#tag-search').val()) === -1);
 			})
 		});
 	};
