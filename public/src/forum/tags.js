94dbb15b2142e8e894c7862c897276f5976189db
barisusakli <barisusakli@gmail.com>
2014-06-26 22:57:49 -0400

    search tags

---
+++ public/src/forum/tags.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/* globals define, app */
+
+define('forum/tags', function() {
+	var Tags = {};
+
+	Tags.init = function() {
+		app.enterRoom('tags');
+
+		$('#tag-search').on('input propertychange', function() {
+			$('.tag-list a').each(function() {
+				var $this = $(this)
+				$this.toggleClass('hide', $this.attr('href').indexOf($('#tag-search').val()) === -1);
+			})
+		});
+	};
+
+	return Tags;
+});
