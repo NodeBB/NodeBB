633980ce4bdac3d58fd0e276147eb6248f9bd260
barisusakli <barisusakli@gmail.com>
2014-07-13 14:57:55 -0400

    active users cleanup

--- src/categories/activeusers.js
+++ src/categories/activeusers.js
@@ -1,23 +1,23 @@
 'use strict';
 
 var async = require('async'),
-	db = require('./../database'),
-	posts = require('./../posts'),
-	topics = require('./../topics');
+	db = require('../database');
 
 module.exports = function(Categories) {
 
 	Categories.getActiveUsers = function(cid, callback) {
-		db.getSortedSetRevRange('categories:recent_posts:cid:' + cid, 0, 99, function(err, pids) {
-			var keys = pids.map(function(pid) {
-				return 'post:' + pid;
-			});
+		async.waterfall([
+			function(next) {
+				db.getSortedSetRevRange('categories:recent_posts:cid:' + cid, 0, 99, next);
+			},
+			function(pids, next) {
+				var keys = pids.map(function(pid) {
+					return 'post:' + pid;
+				});
 
-			db.getObjectsFields(keys, ['uid'], function(err, users) {
-				if (err) {
-					return callback(err);
-				}
-
+				db.getObjectsFields(keys, ['uid'], next);
+			},
+			function(users, next) {
 				var uids = users.map(function(user) {
 					return user.uid;
 				}).filter(function(value, index, array) {
@@ -25,7 +25,7 @@ module.exports = function(Categories) {
 				}).slice(0, 24);
 
 				callback(null, uids);
-			});
-		});
+			}
+		], callback);
 	};
 };
