100328ef06160a466b1cf810481580ae99bdfcd0
barisusakli <barisusakli@gmail.com>
2014-08-05 22:52:33 -0400

    shorter markAsUnread

--- src/categories.js
+++ src/categories.js
@@ -237,11 +237,8 @@ var db = require('./database'),
 	};
 
 	Categories.markAsUnreadForAll = function(cid, callback) {
-		db.delete('cid:' + cid + ':read_by_uid', function(err) {
-			if(typeof callback === 'function') {
-				callback(err);
-			}
-		});
+		callback = callback || function() {};
+		db.delete('cid:' + cid + ':read_by_uid', callback);
 	};
 
 	Categories.hasReadCategories = function(cids, uid, callback) {
