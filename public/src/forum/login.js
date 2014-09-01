fa2bf7c908b29d096a3af26cb68338ad641443b6
Julian Lam <julian@designcreateplay.com>
2014-09-01 00:05:16 -0400

    added disabled class back when form is submitting... aka clickspam handling

--- public/src/forum/login.js
+++ public/src/forum/login.js
@@ -5,13 +5,31 @@ define('forum/login', function() {
 	var	Login = {};
 
 	Login.init = function() {
-		$('#login').on('click', function(e) {
-			$('#login-error-notify').hide();
+		var errorEl = $('#login-error-notify'),
+			submitEl = $('#login'),
+			formEl = $('#login-form');
+
+		submitEl.on('click', function(e) {
+			e.preventDefault();
+
+			if (!$('#username').val() || !$('#password').val()) {
+				translator.translate('[[error:invalid-username-or-password]]', function(translated) {
+					errorEl.find('p').text(translated)
+					errorEl.show();
+				});
+			} else {
+				errorEl.hide();
+
+				if (!submitEl.hasClass('disabled')) {
+					submitEl.addClass('disabled');
+					formEl.submit();
+				}
+			}
 		});
 
 		$('#login-error-notify button').on('click', function(e) {
 			e.preventDefault();
-			$('#login-error-notify').hide();
+			errorEl.hide();
 		});
 
 		$('#content #username').focus();
