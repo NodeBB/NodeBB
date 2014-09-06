edd73b5be2d630a8bae9fca3a5fd1913dfe3b8f4
barisusakli <barisusakli@gmail.com>
2014-09-06 03:35:28 -0400

    again

--- src/topics.js
+++ src/topics.js
@@ -244,10 +244,7 @@ var async = require('async'),
 	};
 
 	Topics.getTopicWithPosts = function(tid, set, uid, start, end, reverse, callback) {
-		if (end - start > 50) {
-			var e = new Error('TOO LARGE');
-			winston.warn('GET_TOPIC_WITH_POSTS set, start, end, uid, tid', set, start, end, uid, tid, e.stack);
-		}
+
 		Topics.getTopicData(tid, function(err, topicData) {
 			if (err || !topicData) {
 				return callback(err || new Error('[[error:no-topic]]'));
@@ -268,7 +265,10 @@ var async = require('async'),
 							if (err) {
 								return next(err);
 							}
-
+							if (posts.length > 100) {
+								var e = new Error('TOO LARGE');
+								winston.warn('GET_TOPIC_WITH_POSTS set, start, end, uid, tid', set, start, end, uid, tid, e.stack);
+							}
 							Topics.addPostData(posts, uid, next);
 						});
 					});
@@ -337,7 +337,7 @@ var async = require('async'),
 				Topics.addPostData(postData, uid, callback);
 			});
 		});
-	}
+	};
 
 	Topics.getTeasers = function(tids, uid, callback) {
 		if(!Array.isArray(tids)) {
