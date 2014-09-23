48c3619028680b01b40be3e66a83b109886fd7fe
barisusakli <barisusakli@gmail.com>
2014-09-22 22:17:50 -0400

    lowered chat zindex

--- public/src/modules/chat.js
+++ public/src/modules/chat.js
@@ -262,7 +262,7 @@ define('chat', ['taskbar', 'string', 'sounds', 'forum/chats'], function(taskbar,
 	module.center = function(chatModal) {
 		chatModal.css("left", Math.max(0, (($(window).width() - $(chatModal).outerWidth()) / 2) + $(window).scrollLeft()) + "px");
 		chatModal.css("top", Math.max(0, $(window).height() / 4 - $(chatModal).outerHeight() / 2));
-		chatModal.css("zIndex", 2000);
+		chatModal.css("zIndex", 100);
 		chatModal.find('#chat-message-input').focus();
 		return chatModal;
 	};
