e8dc9fed3c4cad422d538e47a0aa34ac90f89667
barisusakli <barisusakli@gmail.com>
2014-09-29 00:20:50 -0400

    remove unnecessary index
    the _key:1 score:-1 index already handles reverse sorting on score

--- src/database/mongo.js
+++ src/database/mongo.js
@@ -108,12 +108,6 @@
 					}
 				});
 
-				db.collection('objects').ensureIndex({_key :1, score: 1}, {background:true}, function(err) {
-					if(err) {
-						winston.error('Error creating index ' + err.message);
-					}
-				});
-
 				db.collection('objects').ensureIndex({_key :1, value: -1}, {background:true}, function(err) {
 					if(err) {
 						winston.error('Error creating index ' + err.message);
