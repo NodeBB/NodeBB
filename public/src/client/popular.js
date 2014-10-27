a4d671f8b8acf9c9b744c48e2c0f99707f46b386
barisusakli <barisusakli@gmail.com>
2014-10-26 21:44:09 -0400

    fix popular pill selection

--- public/src/client/popular.js
+++ public/src/client/popular.js
@@ -12,8 +12,26 @@ define('forum/popular', ['forum/recent', 'forum/infinitescroll'], function(recen
 			$(this).addClass('hide');
 		});
 
-		recent.selectActivePill();
+		selectActivePill();
 	};
+
+	function selectActivePill() {
+		var active = getActiveSection();
+
+		$('.nav-pills li').removeClass('active');
+		$('.nav-pills li a').each(function() {
+			var $this = $(this);
+			if ($this.attr('href').match(active)) {
+				$this.parent().addClass('active');
+				return false;
+			}
+		});
+	};
+
+	function getActiveSection() {
+		parts = window.location.href.split('/');
+		return parts[parts.length - 1];
+	}
 
 	return Popular;
 });
