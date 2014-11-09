263f479b204da93e78d357b7c0c1bbc01d0172e7
barisusakli <barisusakli@gmail.com>
2014-11-09 01:12:24 -0500

    posts.js refactor pt1

---
+++ src/posts/create.js
@@ -0,0 +1,86 @@
+'use strict';
+
+var async = require('async'),
+
+	db = require('../database'),
+	plugins = require('../plugins'),
+	user = require('../user'),
+	topics = require('../topics'),
+	categories = require('../categories');
+
+
+module.exports = function(Posts) {
+	Posts.create = function(data, callback) {
+		var uid = data.uid,
+			tid = data.tid,
+			content = data.content,
+			timestamp = data.timestamp || Date.now();
+
+
+		if (!uid && parseInt(uid, 10) !== 0) {
+			return callback(new Error('[[error:invalid-uid]]'));
+		}
+
+		var postData;
+
+		async.waterfall([
+			function(next) {
+				db.incrObjectField('global', 'nextPid', next);
+			},
+			function(pid, next) {
+
+				postData = {
+					'pid': pid,
+					'uid': uid,
+					'tid': tid,
+					'content': content,
+					'timestamp': timestamp,
+					'reputation': 0,
+					'votes': 0,
+					'editor': '',
+					'edited': 0,
+					'deleted': 0
+				};
+
+				if (data.toPid) {
+					postData.toPid = data.toPid;
+				}
+
+				plugins.fireHook('filter:post.save', postData, next);
+			},
+			function(postData, next) {
+				db.setObject('post:' + postData.pid, postData, next);
+			},
+			function(next) {
+				async.parallel([
+					function(next) {
+						user.onNewPostMade(postData, next);
+					},
+					function(next) {
+						topics.onNewPostMade(postData, next);
+					},
+					function(next) {
+						categories.onNewPostMade(postData, next);
+					},
+					function(next) {
+						db.sortedSetAdd('posts:pid', timestamp, postData.pid, next);
+					},
+					function(next) {
+						db.incrObjectField('global', 'postCount', next);
+					}
+				], function(err) {
+					if (err) {
+						return next(err);
+					}
+					plugins.fireHook('filter:post.get', postData, next);
+				});
+			},
+			function(postData, next) {
+				plugins.fireHook('action:post.save', postData);
+				next(null, postData);
+			}
+		], callback);
+	};
+};
+
+
---
+++ src/posts/user.js
@@ -0,0 +1,163 @@
+'use strict';
+
+var async = require('async'),
+
+	db = require('../database'),
+	user = require('../user'),
+	groups = require('../groups'),
+	meta = require('../meta'),
+	websockets = require('../socket.io'),
+	postTools = require('../postTools'),
+	plugins = require('../plugins'),
+	privileges = require('../privileges');
+
+
+module.exports = function(Posts) {
+
+	Posts.getUserInfoForPosts = function(uids, uid, callback) {
+		async.parallel({
+			groups: function(next) {
+				groups.getUserGroups(uids, next);
+			},
+			userData: function(next) {
+				user.getMultipleUserFields(uids, ['uid', 'username', 'userslug', 'reputation', 'postcount', 'picture', 'signature', 'banned', 'status'], next);
+			},
+			online: function(next) {
+				websockets.isUsersOnline(uids, next);
+			}
+		}, function(err, results) {
+			if (err) {
+				return callback(err);
+			}
+
+			var userData = results.userData;
+			for(var i=0; i<userData.length; ++i) {
+				userData[i].groups = results.groups[i];
+				userData[i].status = results.online[i] ? (userData[i].status || 'online') : 'offline';
+			}
+
+			async.map(userData, function(userData, next) {
+				userData.uid = userData.uid || 0;
+				userData.username = userData.username || '[[global:guest]]';
+				userData.userslug = userData.userslug || '';
+				userData.reputation = userData.reputation || 0;
+				userData.postcount = userData.postcount || 0;
+				userData.banned = parseInt(userData.banned, 10) === 1;
+				userData.picture = userData.picture || user.createGravatarURLFromEmail('');
+
+				async.parallel({
+					signature: function(next) {
+						if (parseInt(meta.config.disableSignatures, 10) === 1) {
+							userData.signature = '';
+							return next();
+						}
+						postTools.parseSignature(userData, uid, next);
+					},
+					customProfileInfo: function(next) {
+						plugins.fireHook('filter:posts.custom_profile_info', {profile: [], uid: userData.uid}, next);
+					}
+				}, function(err, results) {
+					if (err) {
+						return next(err);
+					}
+
+					userData.custom_profile_info = results.customProfileInfo.profile;
+
+					plugins.fireHook('filter:posts.modifyUserInfo', userData, next);
+				});
+			}, callback);
+		});
+	};
+
+	Posts.isOwner = function(pid, uid, callback) {
+		uid = parseInt(uid, 10);
+		if (Array.isArray(pid)) {
+			if (!uid) {
+				return callback(null, pid.map(function() {return false;}));
+			}
+			Posts.getPostsFields(pid, ['uid'], function(err, posts) {
+				if (err) {
+					return callback(err);
+				}
+				posts = posts.map(function(post) {
+					return post && parseInt(post.uid, 10) === uid;
+				});
+				callback(null, posts);
+			});
+		} else {
+			if (!uid) {
+				return callback(null, false);
+			}
+			Posts.getPostField(pid, 'uid', function(err, author) {
+				callback(err, parseInt(author, 10) === uid);
+			});
+		}
+	};
+
+	Posts.isModerator = function(pids, uid, callback) {
+		if (!parseInt(uid, 10)) {
+			return callback(null, pids.map(function() {return false;}));
+		}
+		Posts.getCidsByPids(pids, function(err, cids) {
+			if (err) {
+				return callback(err);
+			}
+			user.isModerator(uid, cids, callback);
+		});
+	};
+
+	Posts.getPostsByUid = function(callerUid, uid, start, end, callback) {
+		user.getPostIds(uid, start, end, function(err, pids) {
+			if (err) {
+				return callback(err);
+			}
+
+			privileges.posts.filter('read', pids, callerUid, function(err, pids) {
+				if (err) {
+					return callback(err);
+				}
+				getPosts(pids, callerUid, function(err, posts) {
+					if (err) {
+						return callback(err);
+					}
+					callback(null, {posts: posts, nextStart: end + 1});
+				});
+			});
+		});
+	};
+
+	Posts.getFavourites = function(uid, start, end, callback) {
+		db.getSortedSetRevRange('uid:' + uid + ':favourites', start, end, function(err, pids) {
+			if (err) {
+				return callback(err);
+			}
+
+			getPosts(pids, uid, function(err, posts) {
+				if (err) {
+					return callback(err);
+				}
+				callback(null, {posts: posts, nextStart: end + 1});
+			});
+		});
+	};
+
+	function getPosts(pids, uid, callback) {
+		if (!Array.isArray(pids) || !pids.length) {
+			return callback(null, []);
+		}
+
+		Posts.getPostSummaryByPids(pids, uid, {stripTags: false}, function(err, posts) {
+			if (err) {
+				return callback(err);
+			}
+
+			if (!Array.isArray(posts) || !posts.length) {
+				return callback(null, []);
+			}
+
+			callback(null, posts);
+		});
+	}
+
+
+};
