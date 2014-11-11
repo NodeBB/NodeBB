db157da3fba65075aaeb5fe0c5d40dd7eee98630
psychobunny <psycho.bunny@hotmail.com>
2014-11-11 11:44:49 -0500

    fixes potential IE8 crash (both toISOString and console.log don't exist)
    not that client-side calls this anyways

--- public/src/utils.js
+++ public/src/utils.js
@@ -233,16 +233,11 @@
 		},
 
 		toISOString: function(timestamp) {
-			if(!timestamp) {
+			if (!timestamp || !Date.prototype.toISOString) {
 				return '';
 			}
 
-			try {
-				return new Date(parseInt(timestamp, 10)).toISOString();
-			} catch(e){
-				console.log(timestamp, e.stack);
-			}
-			return '';
+			return Date.prototype.toISOString ? new Date(parseInt(timestamp, 10)).toISOString() : timestamp;
 		},
 
 		tags : ['a', 'abbr', 'acronym', 'address', 'applet', 'area', 'article', 'aside', 'audio', 'b', 'base', 'basefont', 'bdi', 'bdo', 'big', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption', 'center', 'cite', 'code', 'col', 'colgroup', 'command', 'datalist', 'dd', 'del', 'details', 'dfn', 'dialog', 'dir', 'div', 'dl', 'dt', 'em', 'embed', 'fieldset', 'figcaption', 'figure', 'font', 'footer', 'form', 'frame', 'frameset', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head', 'header', 'hr', 'html', 'i', 'iframe', 'img', 'input', 'ins', 'kbd', 'keygen', 'label', 'legend', 'li', 'link', 'map', 'mark', 'menu', 'meta', 'meter', 'nav', 'noframes', 'noscript', 'object', 'ol', 'optgroup', 'option', 'output', 'p', 'param', 'pre', 'progress', 'q', 'rp', 'rt', 'ruby', 's', 'samp', 'script', 'section', 'select', 'small', 'source', 'span', 'strike', 'strong', 'style', 'sub', 'summary', 'sup', 'table', 'tbody', 'td', 'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr', 'track', 'tt', 'u', 'ul', 'var', 'video', 'wbr'],
