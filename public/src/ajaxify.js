45affa3043058fb4f79749488961176279ead41c
psychobunny <psycho.bunny@hotmail.com>
2014-11-13 14:04:29 -0500

    closes #2399

--- public/src/ajaxify.js
+++ public/src/ajaxify.js
@@ -259,6 +259,7 @@ $(document).ready(function() {
 
 		$('document').ready(function () {
 			if (!window.history || !window.history.pushState) {
+				templates.registerLoader(ajaxify.loadTemplate);
 				templatesModule.refresh(app.load);
 				return; // no ajaxification for old browsers
 			}
