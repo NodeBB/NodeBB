458ee7cca8ca0c6d48c7ba8137d5e6278d163349
psychobunny <psycho.bunny@hotmail.com>
2014-11-15 14:56:04 -0500

    acute ocd attack

--- public/src/modules/taskbar.js
+++ public/src/modules/taskbar.js
@@ -2,119 +2,107 @@
 /*global define, app, templates*/
 
 define('taskbar', function() {
-	var taskbar = {
-		initialized: false,
-		init: function() {
-			var self = this;
+	var taskbar = {};
 
-			templates.parse('modules/taskbar', {}, function(html) {
-				self.taskbar = $(html);
-				self.tasklist = self.taskbar.find('ul');
-				$(document.body).append(self.taskbar);
+	taskbar.init = function() {
+		var self = this;
 
-				// Posts bar events
-				self.taskbar.on('click', 'li', function() {
-					var	$btn = $(this),
-						module = $btn.attr('data-module'),
-						uuid = $btn.attr('data-uuid');
+		templates.parse('modules/taskbar', {}, function(html) {
+			self.taskbar = $(html);
+			self.tasklist = self.taskbar.find('ul');
+			$(document.body).append(self.taskbar);
 
-					require([module], function(module) {
-						if (!$btn.hasClass('active')) {
-							taskbar.minimizeAll();
-							module.load(uuid);
-							taskbar.toggleNew(uuid, false);
-							app.alternatingTitle('');
+			self.taskbar.on('click', 'li', function() {
+				var	$btn = $(this),
+					module = $btn.attr('data-module'),
+					uuid = $btn.attr('data-uuid');
 
-							// Highlight the button
-							taskbar.tasklist.removeClass('active');
-							$btn.addClass('active');
-						} else {
-							module.minimize(uuid);
-						}
-					});
-					return false;
+				require([module], function(module) {
+					if (!$btn.hasClass('active')) {
+						minimizeAll();
+						module.load(uuid);
+						taskbar.toggleNew(uuid, false);
+						app.alternatingTitle('');
+
+						taskbar.tasklist.removeClass('active');
+						$btn.addClass('active');
+					} else {
+						module.minimize(uuid);
+					}
 				});
-
-				taskbar.initialized = true;
+				
+				return false;
 			});
-		},
+		});
+	};
 
-		update: function() {
-			var	tasks = taskbar.tasklist.find('li');
+	taskbar.discard = function(module, uuid) {
+		var btnEl = taskbar.tasklist.find('[data-module="' + module + '"][data-uuid="' + uuid + '"]');
+		btnEl.remove();
+		taskbar.update();
+	};
 
-			if (tasks.length > 0) {
-				taskbar.taskbar.attr('data-active', '1');
-			} else {
-				taskbar.taskbar.removeAttr('data-active');
-			}
-		},
+	taskbar.push = function(module, uuid, options) {
+		var element = taskbar.tasklist.find('li[data-uuid="' + uuid + '"]');
 
-		discard: function(module, uuid) {
-			var btnEl = taskbar.tasklist.find('[data-module="' + module + '"][data-uuid="' + uuid + '"]');
-			btnEl.remove();
-			taskbar.update();
-		},
-
-		push: function(module, uuid, options) {
-			var element = taskbar.tasklist.find('li[data-uuid="'+uuid+'"]');
-			if(element.length) {
-				return;
-			}
-			var title = $('<div></div>').text(options.title || 'NodeBB Task').html();
-
-			var	btnEl = $('<li />')
-				.html('<a href="#">' +
-					(options.icon ? '<i class="fa ' + options.icon + '"></i> ' : '') +
-					(options.image ? '<img src="' + options.image + '"/> ': '') +
-					'<span>' + title + '</span>' +
-					'</a>')
-				.attr({
-					'data-module': module,
-					'data-uuid': uuid
-				})
-				.addClass(options.state !== undefined ? options.state : 'active');
-
-			if (!options.state || options.state === 'active') {
-				taskbar.minimizeAll();
-			}
-
-			taskbar.tasklist.append(btnEl);
-			taskbar.update();
-		},
-
-		minimize: function(module, uuid) {
-			var btnEl = taskbar.tasklist.find('[data-module="' + module + '"][data-uuid="' + uuid + '"]');
-			btnEl.removeClass('active');
-		},
-
-		minimizeAll: function() {
-			taskbar.tasklist.find('.active').removeClass('active');
-		},
-
-		toggleNew: function(uuid, state) {
-			var btnEl = taskbar.tasklist.find('[data-uuid="' + uuid + '"]');
-			btnEl.toggleClass('new', state);
-		},
-
-		updateActive: function(uuid) {
-			var	tasks = taskbar.tasklist.find('li');
-			tasks.removeClass('active');
-			tasks.filter('[data-uuid="' + uuid + '"]').addClass('active');
-		},
-
-		isActive: function(uuid) {
-			var taskBtn = taskbar.tasklist.find('li[data-uuid="' + uuid + '"]');
-			return taskBtn.hasClass('active');
+		if (element.length) {
+			return;
 		}
+
+		var title = $('<div></div>').text(options.title || 'NodeBB Task').html();
+
+		var	btnEl = $('<li />')
+			.html('<a href="#">' +
+				(options.icon ? '<i class="fa ' + options.icon + '"></i> ' : '') +
+				(options.image ? '<img src="' + options.image + '"/> ': '') +
+				'<span>' + title + '</span>' +
+				'</a>')
+			.attr({
+				'data-module': module,
+				'data-uuid': uuid
+			})
+			.addClass(options.state !== undefined ? options.state : 'active');
+
+		if (!options.state || options.state === 'active') {
+			minimizeAll();
+		}
+
+		taskbar.tasklist.append(btnEl);
+		taskbar.update();
 	};
 
-	return {
-		push: taskbar.push,
-		discard: taskbar.discard,
-		minimize: taskbar.minimize,
-		toggleNew: taskbar.toggleNew,
-		updateActive: taskbar.updateActive,
-		isActive: taskbar.isActive,
-		init: taskbar.init
+	taskbar.minimize = function(module, uuid) {
+		var btnEl = taskbar.tasklist.find('[data-module="' + module + '"][data-uuid="' + uuid + '"]');
+		btnEl.removeClass('active');
 	};
+
+	taskbar.toggleNew = function(uuid, state) {
+		var btnEl = taskbar.tasklist.find('[data-uuid="' + uuid + '"]');
+		btnEl.toggleClass('new', state);
+	};
+
+	taskbar.updateActive = function(uuid) {
+		var	tasks = taskbar.tasklist.find('li');
+		tasks.removeClass('active');
+		tasks.filter('[data-uuid="' + uuid + '"]').addClass('active');
+	};
+
+	taskbar.isActive = function(uuid) {
+		var taskBtn = taskbar.tasklist.find('li[data-uuid="' + uuid + '"]');
+		return taskBtn.hasClass('active');
+	};
+
+	function update() {
+		var	tasks = taskbar.tasklist.find('li');
+
+		if (tasks.length > 0) {
+			taskbar.taskbar.attr('data-active', '1');
+		} else {
+			taskbar.taskbar.removeAttr('data-active');
+		}
+	}
+
+	function minimizeAll() {
+		taskbar.tasklist.find('.active').removeClass('active');
+	}
 });
