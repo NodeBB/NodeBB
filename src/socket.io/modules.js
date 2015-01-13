1adc3b4ea70c04b1edec4dccc0672e8634b47aaa
barisusakli <barisusakli@gmail.com>
2015-01-13 14:57:41 -0500

    removed console.log

--- src/socket.io/modules.js
+++ src/socket.io/modules.js
@@ -83,7 +83,6 @@ SocketModules.composer.editCheck = function(socket, pid, callback) {
 };
 
 SocketModules.composer.renderPreview = function(socket, content, callback) {
-	console.log(content);
 	plugins.fireHook('filter:parse.raw', content, callback);
 };
 
