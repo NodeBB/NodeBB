8256d2b997e57cb5c8bd537e61c51b7b2311fa8d
psychobunny <psycho.bunny@hotmail.com>
2015-02-25 14:15:50 -0500

    bugfix

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -8,17 +8,13 @@ var navigation = {},
 
 navigation.get = function(callback) {
 	db.getSortedSetRange('navigation:enabled', 0, -1, function(err, data) {
-
-		data = data
-			.filter(function(item) {
-				return item.enabled;
-			})
-			.map(function(item) {
-				return JSON.parse(item);
-			});
-
-		callback(err, data);
-	})
+		callback(err, data.map(function(item) {
+			return JSON.parse(item);
+		})
+		.filter(function(item) {
+			return item.enabled;
+		}));
+	});
 };
 
 
