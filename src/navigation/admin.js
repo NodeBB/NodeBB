f1adddb775754523f02b38702f9a00a2298a39a6
psychobunny <psycho.bunny@hotmail.com>
2015-02-25 13:45:49 -0500

    navigation - basic save function

--- src/navigation/admin.js
+++ src/navigation/admin.js
@@ -1,12 +1,31 @@
 "use strict";
 
 
-var admin = {};
+var admin = {},
+	async = require('async'),
+	plugins = require('../plugins'),
+	db = require('../database');
 
 
 admin.save = function(data, callback) {
+	var order = Object.keys(data),
+		items = data.map(function(item) {
+			return JSON.stringify(item);
+		});
 
+	async.waterfall([
+		function(next) {
+			db.delete('navigation:enabled', next);
+		},
+		function(next) {
+			db.sortedSetAdd('navigation:enabled', order, items, next);
+		}
+	], callback);
 };
 
+admin.getAvailable = function(data, callback) {
+	var core = require('../../install/data/navigation.json');
+	plugins.fireHook('filter:navigation.available', core, callback);
+};
 
 module.exports = admin;
