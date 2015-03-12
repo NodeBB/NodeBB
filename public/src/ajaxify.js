f89741636cdad244d38bf2c8dca52054f65c335c
barisusakli <barisusakli@gmail.com>
2015-03-11 21:56:33 -0400

    return false to prevent default

--- public/src/ajaxify.js
+++ public/src/ajaxify.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
 
 	ajaxify.go = function (url, callback, quiet) {
 		if (ajaxify.handleACPRedirect(url)) {
-			return;
+			return false;
 		}
 
 		app.enterRoom('');
