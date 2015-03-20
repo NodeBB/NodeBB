04ed79e587e6639340d8912ca4f982059b22b6bf
barisusakli <barisusakli@gmail.com>
2015-03-20 14:48:19 -0400

    fix profile image upload on subfolder install

--- src/user.js
+++ src/user.js
@@ -120,7 +120,7 @@ var	async = require('async'),
 
 			if (user.picture) {
 				if (user.picture === user.uploadedpicture) {
-					user.picture = user.uploadedpicture = user.picture.indexOf('http') === -1 ? nconf.get('relative_path') + user.picture : user.picture;
+					user.picture = user.uploadedpicture = user.picture.startsWith('http') ? user.picture : nconf.get('relative_path') + user.picture;
 				} else {
 					user.picture = User.createGravatarURLFromEmail(user.email);
 				}
