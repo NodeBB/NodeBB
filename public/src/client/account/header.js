7bc2eeff4a6bd42e6ef8f2e2123d2857072e6a6e
Barış Soner Uşaklı <barisusakli@gmail.com>
2015-04-15 14:58:37 -0400

    duh

--- public/src/client/account/header.js
+++ public/src/client/account/header.js
@@ -5,22 +5,20 @@ define('forum/account/header', function() {
 	var	AccountHeader = {};
 
 	AccountHeader.init = function() {
-		displayAccountMenus();
+		hidePrivateLinks();
 		selectActivePill();
 	};
 
-	function displayAccountMenus() {
+	function hidePrivateLinks() {
 		if (!app.user.uid || app.user.uid !== parseInt(ajaxify.variables.get('theirid'), 10)) {
-			$('.account-sub-links .plugin-link.private').each(function() {
-				$(this).addClass('hide');
-			});
+			$('.account-sub-links .plugin-link.private').addClass('hide');
 		}
 	}
 
 	function selectActivePill() {
 		$('.account-sub-links li').removeClass('active').each(function() {
 			var href = $(this).find('a').attr('href');
-			
+
 			if (href === window.location.pathname) {
 				$(this).addClass('active');
 				return false;
