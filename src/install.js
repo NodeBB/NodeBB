ee78281f3535ee634e9a03137907ac0e5de2bf37
psychobunny <psycho.bunny@hotmail.com>
2015-04-22 13:22:07 -0400

    console.log

--- src/install.js
+++ src/install.js
@@ -61,7 +61,6 @@ function checkSetupFlag(next) {
 		setupVal = undefined;
 	}
 
-	console.log(setupVal);
 	if (setupVal && setupVal instanceof Object) {
 		if (setupVal['admin:username'] && setupVal['admin:password'] && setupVal['admin:password:confirm'] && setupVal['admin:email']) {
 			install.values = setupVal;
