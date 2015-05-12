a88ad903af1f822464468291509119f17c5fe158
psychobunny <psycho.bunny@hotmail.com>
2015-05-12 14:19:30 -0400

    action:ajaxify.dataLoaded

--- public/src/ajaxify.js
+++ public/src/ajaxify.js
@@ -206,6 +206,7 @@ $(document).ready(function() {
 				}
 
 				data.relative_path = RELATIVE_PATH;
+				$(window).trigger('action:ajaxify.dataLoaded', {url: url, data: data});
 
 				if (callback) {
 					callback(null, data);
