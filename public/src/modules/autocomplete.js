8f7416d1cb074655c9ebcb848468ff687e69eed4
Julian Lam <julian@designcreateplay.com>
2015-05-26 12:35:27 -0400

    updated acp category privilege settings so that not all groups are shown in privilege table, closes #3036

--- public/src/modules/autocomplete.js
+++ public/src/modules/autocomplete.js
@@ -35,5 +35,36 @@ define('autocomplete', function() {
 		});
 	};
 
+	module.group = function(input) {
+		return input.autocomplete({
+			delay: 100,
+			source: function(request, response) {
+				socket.emit('groups.search', {
+					query: request.term,
+					options: {}
+				}, function(err, results) {
+					if (err) {
+						return app.alertError(err.message);
+					}
+
+					if (results && results.length) {
+						var names = results.map(function(group) {
+							return group && {
+								label: group.name,
+								value: group.name,
+								group: {
+									name: group.name,
+									slug: group.slug
+								}
+							};
+						});
+						response(names);
+					}
+					$('.ui-autocomplete a').attr('data-ajaxify', 'false');
+				});
+			}
+		});
+	};
+
 	return module;
 });
