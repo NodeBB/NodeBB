0feb11f205474c28c2a5be5c6cf680bc62a8202b
barisusakli <barisusakli@gmail.com>
2015-06-02 17:12:46 -0400

    test

--- tests/groups.js
+++ tests/groups.js
@@ -37,7 +37,7 @@ describe('Groups', function() {
 			Groups.getGroupsFromSet('groups:createtime', 0, 0, -1, function(err, groups) {
 				if (err) return done(err);
 
-				assert.equal(groups.length, 2);
+				assert.equal(groups.length, 3);
 				done();
 			});
 		});
