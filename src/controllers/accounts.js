2deeda553a79e73fa5fa120fdbe5b761ff30f7e1
barisusakli <barisusakli@gmail.com>
2015-06-21 01:58:11 -0400

    closes #3246

--- src/controllers/accounts.js
+++ src/controllers/accounts.js
@@ -88,6 +88,7 @@ function getUserDataByUserSlug(userslug, callerUID, callback) {
 			userData.profile_links = results.profile_links;
 			userData.status = require('../socket.io').isUserOnline(userData.uid) ? (userData.status || 'online') : 'offline';
 			userData.banned = parseInt(userData.banned, 10) === 1;
+			userData.website = validator.escape(userData.website);
 			userData.websiteName = userData.website.replace('http://', '').replace('https://', '');
 			userData.followingCount = parseInt(userData.followingCount, 10) || 0;
 			userData.followerCount = parseInt(userData.followerCount, 10) || 0;
