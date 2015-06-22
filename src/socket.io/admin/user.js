3a21329c5e691e3fd8c234c1253fe893ccc5a502
psychobunny <psycho.bunny@hotmail.com>
2015-06-22 16:03:32 -0400

    allow admins to remove themselves as admin
    but only if they are not the last admin left

--- src/socket.io/admin/user.js
+++ src/socket.io/admin/user.js
@@ -38,10 +38,6 @@ User.removeAdmins = function(socket, uids, callback) {
 		return callback(new Error('[[error:invalid-data]]'));
 	}
 
-	if (uids.indexOf(socket.uid.toString()) !== -1) {
-		return callback(new Error('[[error:cant-remove-self-as-admin]]'));
-	}
-
 	async.eachSeries(uids, function(uid, next) {
 		groups.getMemberCount('administrators', function(err, count) {
 			if (err) {
