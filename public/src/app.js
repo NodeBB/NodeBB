b19120f82298f485bf3999c6a6a451da4497bed3
psychobunny <rodrigues.andrew@gmail.com>
2015-08-27 12:20:45 -0400

    user/logout componen

--- public/src/app.js
+++ public/src/app.js
@@ -553,7 +553,7 @@ app.cacheBuster = null;
 
 			handleNewTopic();
 
-			$('#logout-link').on('click', app.logout);
+			components.get('user/logout').on('click', app.logout);
 
 			Visibility.change(function(e, state){
 				if (state === 'visible') {
