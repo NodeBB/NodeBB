95b709c8066b863ac8b0408572d86df5ff501a70
barisusakli <barisusakli@gmail.com>
2015-09-11 18:36:19 -0400

    dont crash if data.responseJSON doesnt exist

--- public/src/ajaxify.js
+++ public/src/ajaxify.js
@@ -110,13 +110,14 @@ $(document).ready(function() {
 			textStatus = err.textStatus;
 
 		if (data) {
-			data.responseJSON.config = config;
 			var status = parseInt(data.status, 10);
 			if (status === 403 || status === 404 || status === 500 || status === 502 || status === 503) {
 				if (status === 502) {
 					status = 500;
 				}
-
+				if (data.responseJSON) {
+					data.responseJSON.config = config;
+				}
 				$('#footer, #content').removeClass('hide').addClass('ajaxifying');
 				return renderTemplate(url, status.toString(), data.responseJSON, callback);
 			} else if (status === 401) {
