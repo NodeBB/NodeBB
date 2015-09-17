31ddb8a0e03e1af79b72e0089bce8c6d819da827
barisusakli <barisusakli@gmail.com>
2015-09-17 19:04:29 -0400

    uid can be group names :dull:

--- src/groups/membership.js
+++ src/groups/membership.js
@@ -44,7 +44,7 @@ module.exports = function(Groups) {
 
 		callback = callback || function() {};
 
-		if (!groupName || !parseInt(uid, 10)) {
+		if (!groupName) {
 			return callback(new Error('[[error:invalid-data]]'));
 		}
 
