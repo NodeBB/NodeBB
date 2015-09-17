28a4703a4bf3d0ec81d14bf20724c707ec6a4eef
psychobunny <rodrigues.andrew@gmail.com>
2015-09-17 19:04:30 -0400

    acp remove logo fix

--- public/src/admin/settings/general.js
+++ public/src/admin/settings/general.js
@@ -6,9 +6,7 @@ define('admin/settings/general', ['admin/settings'], function(Settings) {
 
 	Module.init = function() {
 		$('button[data-action="removeLogo"]').on('click', function() {
-			socket.emit('admin.settings.removeLogo', function() {
-				app.alertSuccess('Logo removed');
-			});
+			$('input[data-field="brand:logo"]').val('');
 		});
 	};
 
