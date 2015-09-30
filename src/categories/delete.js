a054353b6c9896693bfb9c64e7e389380bff1ea1
barisusakli <barisusakli@gmail.com>
2015-09-30 16:29:25 -0400

    category purge fix

--- src/categories/delete.js
+++ src/categories/delete.js
@@ -25,7 +25,7 @@ module.exports = function(Categories) {
 	};
 
 	function purgeCategory(cid, callback) {
-		async.parallel([
+		async.series([
 			function(next) {
 				db.sortedSetRemove('categories:cid', cid, next);
 			},
