f385e1353168228c1c62c01ec045c6e1e4b55232
psychobunny <rodrigues.andrew@gmail.com>
2015-10-29 15:16:49 -0400

    fix default cover on groups/list page

--- src/groups.js
+++ src/groups.js
@@ -403,10 +403,9 @@ var async = require('async'),
 					group.hidden = parseInt(group.hidden, 10) === 1;
 					group.system = parseInt(group.system, 10) === 1;
 					group.private = parseInt(group.private, 10) === 1;
-					if (!group['cover:url']) {
-						group['cover:url'] = nconf.get('relative_path') + '/images/cover-default.png';
-						group['cover:position'] = '50% 50%';
-					}
+
+					group['cover:url'] = group['cover:url'] || require('./coverPhoto').getDefaultGroupCover(group.name);
+					group['cover:position'] = group['cover:position'] || '50% 50%';
 				}
 			});
 
