12d2abff447c53cf95d40b7de835a05fdfc46e1b
psychobunny <rodrigues.andrew@gmail.com>
2015-10-29 16:05:07 -0400

    missed some other profile pages

--- src/controllers/accounts/helpers.js
+++ src/controllers/accounts/helpers.js
@@ -143,8 +143,8 @@ helpers.getBaseUser = function(userslug, callerUID, callback) {
 			results.user.showHidden = results.user.isSelf || results.isAdmin;
 			results.user.profile_links = results.profile_links;
 
-			results['cover:url'] = results['cover:url'] || require('../../coverPhoto').getDefaultProfileCover(results.user.uid);
-			results['cover:position'] = results['cover:position'] || '50% 50%';
+			results.user['cover:url'] = results.user['cover:url'] || require('../../coverPhoto').getDefaultProfileCover(results.user.uid);
+			results.user['cover:position'] = results.user['cover:position'] || '50% 50%';
 
 			next(null, results.user);
 		}
