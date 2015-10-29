c69778527044db00bf7083bd01582b7763ae096e
psychobunny <rodrigues.andrew@gmail.com>
2015-10-29 15:55:20 -0400

    console.log

--- src/controllers/accounts/profile.js
+++ src/controllers/accounts/profile.js
@@ -75,7 +75,6 @@ profileController.get = function(req, res, callback) {
 
 			userData['cover:url'] = userData['cover:url'] || require('../../coverPhoto').getDefaultProfileCover(userData.uid);
 			userData['cover:position'] = userData['cover:position'] || '50% 50%';
-			console.log(userData['cover:url']);
 
 			if (!userData.profileviews) {
 				userData.profileviews = 1;
