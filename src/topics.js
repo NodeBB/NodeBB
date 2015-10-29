0588a59c0cbac766833bc13020e09772302c1dc4
psychobunny <rodrigues.andrew@gmail.com>
2015-10-29 18:07:04 -0400

    normalize user data between topic.user and post.user

--- src/topics.js
+++ src/topics.js
@@ -122,7 +122,7 @@ var async = require('async'),
 
 				async.parallel({
 					users: function(next) {
-						user.getUsersFields(uids, ['uid', 'username', 'userslug', 'picture'], next);
+						user.getUsersFields(uids, ['uid', 'username', 'userslug', 'reputation', 'postcount', 'picture', 'signature', 'banned', 'status'], next);
 					},
 					categories: function(next) {
 						categories.getCategoriesFields(cids, ['cid', 'name', 'slug', 'icon', 'bgColor', 'color', 'disabled'], next);
