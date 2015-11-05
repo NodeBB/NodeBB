364260fa3795d696fb7fe20dc3feb011e7502245
barisusakli <barisusakli@gmail.com>
2015-11-05 12:54:15 -0500

    fix lastonline iso

--- src/controllers/accounts/helpers.js
+++ src/controllers/accounts/helpers.js
@@ -58,7 +58,7 @@ helpers.getUserDataByUserSlug = function(userslug, callerUID, callback) {
 			var self = parseInt(callerUID, 10) === parseInt(userData.uid, 10);
 
 			userData.joindate = utils.toISOString(userData.joindate);
-			userData.lastonline = userData.lastonline ? utils.toISOString(userData.lastonline) : userData.joindate;
+			userData.lastonlineISO = utils.toISOString(userData.lastonline || userData.joindate);
 			userData.age = userData.birthday ? Math.floor((new Date().getTime() - new Date(userData.birthday).getTime()) / 31536000000) : '';
 
 			if (!(isAdmin || self || (userData.email && userSettings.showemail))) {
