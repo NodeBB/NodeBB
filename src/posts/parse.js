6d9adac6e466e8476c8e4442dee7ef8731b6d355
Julian Lam <julian@designcreateplay.com>
2015-11-26 23:34:55 -0500

    fixed LRU cache problem

--- src/posts/parse.js
+++ src/posts/parse.js
@@ -9,9 +9,8 @@ module.exports = function(Posts) {
 	Posts.parsePost = function(postData, callback) {
 		postData.content = postData.content || '';
 
-		var cachedContent = cache.get(postData.pid);
-		if (cachedContent) {
-			postData.content = cachedContent;
+		if (postData.pid && cache.has(postData.pid)) {
+			postData.content = cache.get(postData.pid);
 			return callback(null, postData);
 		}
 
