530b0dc19e7431c4a2fac43c83af7dbfb5e61cb6
Barış Soner Uşaklı <baris@nodebb.org>
2015-12-14 22:03:06 +0200

    use .text()

--- public/src/client/topic/fork.js
+++ public/src/client/topic/fork.js
@@ -118,7 +118,7 @@ define('forum/topic/fork', ['components'], function(components) {
 
 			if (pids.length) {
 				pids.sort(function(a,b) { return a - b; });
-				forkModal.find('#fork-pids').html(pids.join(', '));
+				forkModal.find('#fork-pids').text(pids.join(', '));
 			} else {
 				showNoPostsSelected();
 			}
