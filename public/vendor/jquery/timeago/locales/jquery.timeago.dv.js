04879e6da2f50f6b5ef85ce3cea1c69b932bcc44
Julian Lam <julian@designcreateplay.com>
2015-12-18 14:13:47 -0500

    updated jquery-timeago locales

---
+++ public/vendor/jquery/timeago/locales/jquery.timeago.dv.js
@@ -0,0 +1,22 @@
+/** 
+ * Dhivehi time in Thaana for timeago.js
+ **/
+jQuery.timeago.settings.strings = {
+  prefixAgo: null,
+  prefixFromNow: null,
+  suffixAgo: "ކުރިން",
+  suffixFromNow: "ފަހުން",
+  seconds: "ސިކުންތުކޮޅެއް",
+  minute: "މިނިޓެއްވަރު",
+  minutes: "%d މިނިޓު",
+  hour: "ގަޑިއެއްވަރު",
+  hours: "ގާތްގަނޑަކަށް %d ގަޑިއިރު",
+  day: "އެއް ދުވަސް",
+  days: "މީގެ %d ދުވަސް",
+  month: "މަހެއްވަރު",
+  months: "މީގެ %d މަސް",
+  year: "އަހަރެއްވަރު",
+  years: "މީގެ %d އަހަރު",
+  wordSeparator: " ",
+  numbers: []
+};
