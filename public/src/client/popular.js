70bbf41dc344dfb5279c225ce0f93ac32730ae01
psychobunny <rodrigues.andrew@gmail.com>
2016-01-07 16:24:14 -0500

    popular/tab component

--- public/src/client/popular.js
+++ public/src/client/popular.js
@@ -2,13 +2,16 @@
 
 /* globals define, app, socket*/
 
-define('forum/popular', function() {
+define('forum/popular', ['components'], function(components) {
 	var Popular = {};
 
 	Popular.init = function() {
 		app.enterRoom('popular_topics');
 
-		$('.nav-pills li').removeClass('active').find('a[href="' + window.location.pathname + '"]').parent().addClass('active');
+		components.get('popular/tab')
+			.removeClass('active')
+			.find('a[href="' + window.location.pathname + '"]')
+			.parent().addClass('active');
 	};
 
 	return Popular;
