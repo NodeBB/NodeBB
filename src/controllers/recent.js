1c29722ac41564125c2be95e9a8b655f8a05fda3
barisusakli <barisusakli@gmail.com>
2016-01-26 18:13:18 +0200

    recent style

--- src/controllers/recent.js
+++ src/controllers/recent.js
@@ -1,11 +1,12 @@
 
 'use strict';
 
-var nconf = require('nconf'),
-	topics = require('../topics'),
-	meta = require('../meta'),
-	helpers = require('./helpers'),
-	plugins = require('../plugins');
+var nconf = require('nconf');
+var async = require('async');
+var topics = require('../topics');
+var meta = require('../meta');
+var helpers = require('./helpers');
+var plugins = require('../plugins');
 
 var recentController = {};
 
@@ -13,24 +14,25 @@ recentController.get = function(req, res, next) {
 
 	var stop = (parseInt(meta.config.topicsPerList, 10) || 20) - 1;
 
-	topics.getTopicsFromSet('topics:recent', req.uid, 0, stop, function(err, data) {
+	async.waterfall([
+		function (next) {
+			topics.getTopicsFromSet('topics:recent', req.uid, 0, stop, next);
+		},
+		function (data, next) {
+			data['feeds:disableRSS'] = parseInt(meta.config['feeds:disableRSS'], 10) === 1;
+			data.rssFeedUrl = nconf.get('relative_path') + '/recent.rss';
+			data.title = '[[pages:recent]]';
+			if (req.path.startsWith('/api/recent') || req.path.startsWith('/recent')) {
+				data.breadcrumbs = helpers.buildBreadcrumbs([{text: '[[recent:title]]'}]);
+			}
+
+			plugins.fireHook('filter:recent.build', {req: req, res: res, templateData: data}, next);
+		}
+	], function(err, data) {
 		if (err) {
 			return next(err);
 		}
-
-		data['feeds:disableRSS'] = parseInt(meta.config['feeds:disableRSS'], 10) === 1;
-		data.rssFeedUrl = nconf.get('relative_path') + '/recent.rss';
-		data.title = '[[pages:recent]]';
-		if (req.path.startsWith('/api/recent') || req.path.startsWith('/recent')) {
-			data.breadcrumbs = helpers.buildBreadcrumbs([{text: '[[recent:title]]'}]);
-		}
-
-		plugins.fireHook('filter:recent.build', {req: req, res: res, templateData: data}, function(err, data) {
-			if (err) {
-				return next(err);
-			}
-			res.render('recent', data.templateData);
-		});
+		res.render('recent', data.templateData);
 	});
 };
 
