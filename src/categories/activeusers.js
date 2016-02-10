93ae5e2a3fcaefa52a57780d842da47eea605b6b
barisusakli <barisusakli@gmail.com>
2016-02-10 13:05:02 +0200

    style change

--- src/categories/activeusers.js
+++ src/categories/activeusers.js
@@ -1,24 +1,24 @@
 'use strict';
 
-var async = require('async'),
-	posts = require('../posts'),
-	db = require('../database');
+var async = require('async');
+var posts = require('../posts');
+var db = require('../database');
 
 module.exports = function(Categories) {
 
 	Categories.getActiveUsers = function(cid, callback) {
 		async.waterfall([
-			function(next) {
+			function (next) {
 				db.getSortedSetRevRange('cid:' + cid + ':pids', 0, 24, next);
 			},
-			function(pids, next) {
+			function (pids, next) {
 				posts.getPostsFields(pids, ['uid'], next);
 			},
-			function(posts, next) {
+			function (posts, next) {
 				var uids = posts.map(function(post) {
 					return post.uid;
 				}).filter(function(uid, index, array) {
-					return parseInt(uid, 10) !== 0 && array.indexOf(uid) === index;
+					return parseInt(uid, 10) && array.indexOf(uid) === index;
 				});
 
 				next(null, uids);
