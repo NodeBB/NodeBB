944509406a1f918db1a073278cab42aa817779a3
Stephan <hondapreludeba4@gmail.com>
2016-03-11 22:10:25 +0100

    Create jquery.timeago.de-short.js

---
+++ public/vendor/jquery/timeago/locales/jquery.timeago.de-short.js
@@ -0,0 +1,20 @@
+// German shortened
+jQuery.timeago.settings.strings = {
+  prefixAgo: null,
+  prefixFromNow: null,
+  suffixAgo: "",
+  suffixFromNow: "",
+  seconds: "sec",
+  minute: "1min",
+  minutes: "%dmin",
+  hour: "1h",
+  hours: "%dh",
+  day: "1d",
+  days: "%dd",
+  month: "1Mon",
+  months: "%dMon",
+  year: "1Jhr",
+  years: "%dJhr",
+  wordSeparator: " ",
+  numbers: []
+};
