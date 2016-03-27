NodeBB/NodeBB

commit 3e2231d2cb8860c731649f00ef85364bfcdc8ed1

Fixing viewport shuffling due to image load
Introduced new method ".loadImages()" in posts client side lib
to handle viewport height changes when loading images. Requires
nodebb-plugin-markdown@5.0.0

@BenLubar @boomzillawtf

public/less/global.less
@@ -1,0 +1,21 @@
+/*
+	This stylesheet is applied to all themes and all pages.
+	They can be overridden by themes, though their presence (or initial settings) may be depended upon by
+	client-side logic in core.
+
+	==========
+*/
+
+/* Prevent viewport shuffling on image load by restricting image dimensions until viewed by the browser */
+[component="post/content"] img:not(.not-responsive) {
+	height: 1rem;
+	opacity: 0;
+	transition: width 500ms ease;
+	transition: height 500ms ease;
+	transition: opacity 500ms ease;
+
+	&[data-state="loaded"] {
+		height: auto;
+		opacity: 1;
+	}
+}

public/src/client/topic/posts.js
@@ -231,6 +231,66 @@ define('forum/topic/posts', [
 		hidePostToolsForDeletedPosts(posts);
 	};
 
+	Posts.loadImages = function(threshold) {
+		/*
+			If threshold is defined, images loaded above this threshold will modify
+			the user's scroll position so they are not scrolled away from content
+			they were reading. Images loaded below this threshold will push down content.
+
+			If no threshold is defined, loaded images will push down content, as per
+			default
+		*/
+
+		var images = components.get('post/content').find('img[data-state="unloaded"]'),
+			visible = images.filter(function() {
+				return utils.isElementInViewport(this);
+			}),
+			scrollTop = $(window).scrollTop(),
+			adjusting = false,
+			adjustQueue = [],
+			adjustPosition = function() {
+				adjusting = true;
+				oldHeight = document.body.clientHeight;
+
+				// Display the image
+				$(this).attr('data-state', 'loaded');
+				newHeight = document.body.clientHeight;
+
+				var imageRect = this.getBoundingClientRect();
+				if (imageRect.top < threshold) {
+					scrollTop = scrollTop + (newHeight - oldHeight);
+					$(window).scrollTop(scrollTop);
+				}
+
+				if (adjustQueue.length) {
+					adjustQueue.pop()();
+				} else {
+					adjusting = false;
+				}
+			},
+			oldHeight, newHeight;
+
+		// For each image, reset the source and adjust scrollTop when loaded
+		visible.attr('data-state', 'loading');
+		visible.each(function(index, image) {
+			image = $(image);
+
+			image.on('load', function() {
+				if (!adjusting) {
+					adjustPosition.call(this);
+				} else {
+					adjustQueue.push(adjustPosition.bind(this));
+				}
+			});
+
+			image.attr('src', image.attr('data-src'));
+			if (image.parent().attr('href')) {
+				image.parent().attr('href', image.attr('data-src'));
+			}
+			image.removeAttr('data-src');
+		});
+	};
+
 	Posts.wrapImagesInLinks = function(posts) {
 		posts.find('[component="post/content"] img:not(.emoji)').each(function() {
 			var $this = $(this);

public/src/client/topic.js
@@ -61,11 +61,11 @@ define('forum/topic', [
 
 		addParentHandler();
 
-		handleBookmark(tid);
-
 		handleKeys();
 
 		navigator.init('[component="post/anchor"]', ajaxify.data.postcount, Topic.toTop, Topic.toBottom, Topic.navigatorCallback, Topic.calculateIndex);
+
+		handleBookmark(tid);
 
 		$(window).on('scroll', updateTopicTitle);
 
@@ -141,6 +141,7 @@ define('forum/topic', [
 				return navigator.scrollToPostIndex(postIndex, true);
 			}
 		} else if (bookmark && (!config.usePagination || (config.usePagination && ajaxify.data.pagination.currentPage === 1)) && ajaxify.data.postcount > 5) {
+			navigator.update();
 			app.alert({
 				alert_id: 'bookmark',
 				message: '[[topic:bookmark_instructions]]',
@@ -156,6 +157,8 @@ define('forum/topic', [
 			setTimeout(function() {
 				app.removeAlert('bookmark');
 			}, 10000);
+		} else {
+			navigator.update();
 		}
 	}
 
@@ -233,7 +236,7 @@ define('forum/topic', [
 		return index;
 	};
 
-	Topic.navigatorCallback = function(index, elementCount) {
+	Topic.navigatorCallback = function(index, elementCount, threshold) {
 		var path = ajaxify.removeRelativePath(window.location.pathname.slice(1));
 		if (!path.startsWith('topic')) {
 			return 1;
@@ -248,13 +251,13 @@ define('forum/topic', [
 				newUrl += '/' + index;
 			}
 
+			posts.loadImages(threshold);
+
 			if (newUrl !== currentUrl) {
 				if (Topic.replaceURLTimeout) {
 					clearTimeout(Topic.replaceURLTimeout);
 				}
-
 				Topic.replaceURLTimeout = setTimeout(function() {
-
 					updateUserBookmark(index);
 
 					Topic.replaceURLTimeout = 0;

public/src/modules/navigator.js
@@ -1,7 +1,7 @@
 
 'use strict';
 
-/* globals app, define, ajaxify, utils, config */
+/* globals define, ajaxify, utils, config */
 
 
 define('navigator', ['forum/pagination', 'components'], function(pagination, components) {
@@ -56,7 +56,6 @@ define('navigator', ['forum/pagination', 'components'], function(pagination, com
 		});
 
 		navigator.setCount(count);
-		navigator.update();
 	};
 
 	function generateUrl(index) {
@@ -92,7 +91,13 @@ define('navigator', ['forum/pagination', 'components'], function(pagination, com
 		$('.pagination-block').toggleClass('ready', flag);
 	}
 
-	navigator.update = function() {
+	navigator.update = function(threshold) {
+		threshold = typeof threshold === 'number' ? threshold : undefined;
+
+		/*
+			The "threshold" is defined as the distance from the top of the page to
+			a spot where a user is expecting to begin reading.
+		*/
 		var els = $(navigator.selector);
 		if (els.length) {
 			index = parseInt(els.first().attr('data-index'), 10) + 1;
@@ -114,7 +119,7 @@ define('navigator', ['forum/pagination', 'components'], function(pagination, com
 		});
 
 		if (typeof navigator.callback === 'function') {
-			navigator.callback(index, count);
+			navigator.callback(index, count, threshold);
 		}
 
 		navigator.updateTextAndProgressBar();
@@ -202,6 +207,9 @@ define('navigator', ['forum/pagination', 'components'], function(pagination, com
 			return;
 		}
 
+		// Temporarily disable navigator update on scroll
+		$(window).off('scroll', navigator.update);
+
 		duration = duration !== undefined ? duration : 400;
 		navigator.scrollActive = true;
 		var done = false;
@@ -209,21 +217,24 @@ define('navigator', ['forum/pagination', 'components'], function(pagination, com
 		function animateScroll() {
 			var scrollTop = 0;
 			if (postHeight < viewportHeight) {
-				scrollTop = (scrollTo.offset().top - (viewportHeight / 2) + (postHeight / 2)) + 'px';
+				scrollTop = (scrollTo.offset().top - (viewportHeight / 2) + (postHeight / 2));
 			} else {
 				scrollTop = scrollTo.offset().top - navbarHeight;
 			}
 
 			$('html, body').animate({
-				scrollTop: scrollTop
+				scrollTop: scrollTop + 'px'
 			}, duration, function() {
 				if (done) {
+					// Re-enable onScroll behaviour
+					$(window).on('scroll', navigator.update);
+					var scrollToRect = scrollTo.get(0).getBoundingClientRect();
+					navigator.update(scrollToRect.top);
 					return;
 				}
 				done = true;
 
 				navigator.scrollActive = false;
-				navigator.update();
 				highlightPost();
 				$('body').scrollTop($('body').scrollTop() - 1);
 				$('html').scrollTop($('html').scrollTop() - 1);

public/src/utils.js
@@ -309,6 +309,23 @@
 			return labels;
 		},
 
+		/* Retrieved from http://stackoverflow.com/a/7557433 @ 27 Mar 2016 */
+		isElementInViewport: function(el) {
+			//special bonus for those using jQuery
+			if (typeof jQuery === "function" && el instanceof jQuery) {
+				el = el[0];
+			}
+
+			var rect = el.getBoundingClientRect();
+
+			return (
+				rect.top >= 0 &&
+				rect.left >= 0 &&
+				rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+				rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+			);
+		},
+
 		// get all the url params in a single key/value hash
 		params: function(options) {
 			var a, hash = {}, params;

src/meta/css.js
@@ -72,6 +72,7 @@ module.exports = function(Meta) {
 				source += '\n@import "..' + path.sep + '..' + path.sep + 'public/less/blacklist.less";';
 				source += '\n@import "..' + path.sep + '..' + path.sep + 'public/less/generics.less";';
 				source += '\n@import "..' + path.sep + '..' + path.sep + 'public/less/mixins.less";';
+				source += '\n@import "..' + path.sep + '..' + path.sep + 'public/less/global.less";';
 
 				var acpSource = '\n@import "..' + path.sep + 'public/less/admin/admin";\n' + source;
 				acpSource += '\n@import "..' + path.sep + 'public/less/generics.less";';

commit 28db642050932f83b0300e21d7763c4fb877eced

Fixing regression from 3e2231d2cb8860c731649f00ef85364bfcdc8ed1
@BenLubar

public/js-enabled.css
@@ -1,0 +1,7 @@
+/*
+    The following stylesheet is only included on pages that can execute javascript
+*/
+
+[component="post/content"] img:not(.not-responsive):not([data-state]) {
+	display: none !important;
+}

public/less/global.less
@@ -7,14 +7,19 @@
 */
 
 /* Prevent viewport shuffling on image load by restricting image dimensions until viewed by the browser */
-[component="post/content"] img:not(.not-responsive) {
-	height: 1rem;
-	opacity: 0;
+[component="post/content"] img {
 	transition: width 500ms ease;
 	transition: height 500ms ease;
 	transition: opacity 500ms ease;
 
+	&[data-state="unloaded"], &[data-state="loading"] {
+		display: inherit;
+		height: 1rem;
+		opacity: 0;
+	}
+
 	&[data-state="loaded"] {
+		display: inherit;
 		height: auto;
 		opacity: 1;
 	}

public/src/app.js
@@ -24,65 +24,65 @@ app.cacheBuster = null;
 	});
 
 	app.load = function() {
-		$('document').ready(function () {
-			var url = ajaxify.start(window.location.pathname.slice(1) + window.location.search + window.location.hash, true);
-			ajaxify.end(url, app.template);
+		app.loadProgressiveStylesheet();
 
-			handleStatusChange();
+		var url = ajaxify.start(window.location.pathname.slice(1) + window.location.search + window.location.hash, true);
+		ajaxify.end(url, app.template);
 
-			if (config.searchEnabled) {
-				app.handleSearch();
+		handleStatusChange();
+
+		if (config.searchEnabled) {
+			app.handleSearch();
+		}
+
+		$('#content').on('click', '#new_topic', function(){
+			app.newTopic();
+		});
+
+		require(['components'], function(components) {
+			components.get('user/logout').on('click', app.logout);
+		});
+
+		Visibility.change(function(e, state){
+			if (state === 'visible') {
+				app.isFocused = true;
+				app.alternatingTitle('');
+			} else if (state === 'hidden') {
+				app.isFocused = false;
 			}
+		});
 
-			$('#content').on('click', '#new_topic', function(){
-				app.newTopic();
-			});
+		overrides.overrideBootbox();
+		overrides.overrideTimeago();
+		createHeaderTooltips();
+		app.showEmailConfirmWarning();
 
-			require(['components'], function(components) {
-				components.get('user/logout').on('click', app.logout);
-			});
+		socket.removeAllListeners('event:nodebb.ready');
+		socket.on('event:nodebb.ready', function(data) {
+			if (!app.cacheBusters || app.cacheBusters['cache-buster'] !== data['cache-buster']) {
+				app.cacheBusters = data;
 
-			Visibility.change(function(e, state){
-				if (state === 'visible') {
-					app.isFocused = true;
-					app.alternatingTitle('');
-				} else if (state === 'hidden') {
-					app.isFocused = false;
-				}
-			});
+				app.alert({
+					alert_id: 'forum_updated',
+					title: '[[global:updated.title]]',
+					message: '[[global:updated.message]]',
+					clickfn: function() {
+						window.location.reload();
+					},
+					type: 'warning'
+				});
+			}
+		});
 
-			overrides.overrideBootbox();
-			overrides.overrideTimeago();
-			createHeaderTooltips();
-			app.showEmailConfirmWarning();
+		require(['taskbar', 'helpers', 'forum/pagination'], function(taskbar, helpers, pagination) {
+			taskbar.init();
 
-			socket.removeAllListeners('event:nodebb.ready');
-			socket.on('event:nodebb.ready', function(data) {
-				if (!app.cacheBusters || app.cacheBusters['cache-buster'] !== data['cache-buster']) {
-					app.cacheBusters = data;
+			// templates.js helpers
+			helpers.register();
 
-					app.alert({
-						alert_id: 'forum_updated',
-						title: '[[global:updated.title]]',
-						message: '[[global:updated.message]]',
-						clickfn: function() {
-							window.location.reload();
-						},
-						type: 'warning'
-					});
-				}
-			});
+			pagination.init();
 
-			require(['taskbar', 'helpers', 'forum/pagination'], function(taskbar, helpers, pagination) {
-				taskbar.init();
-
-				// templates.js helpers
-				helpers.register();
-
-				pagination.init();
-
-				$(window).trigger('action:app.load');
-			});
+			$(window).trigger('action:app.load');
 		});
 	};
 
@@ -539,4 +539,12 @@ app.cacheBuster = null;
 			}
 		});
 	};
+
+	app.loadProgressiveStylesheet = function() {
+		var linkEl = document.createElement('link');
+		linkEl.rel = 'stylesheet';
+		linkEl.href = config.relative_path + '/js-enabled.css';
+
+		document.head.appendChild(linkEl);
+	}
 }());

public/src/client/topic/posts.js
@@ -218,6 +218,7 @@ define('forum/topic/posts', [
 	};
 
 	Posts.processPage = function(posts) {
+		Posts.unloadImages();
 		Posts.showBottomPostBar();
 		posts.find('[component="post/content"] img:not(.not-responsive)').addClass('img-responsive');
 		app.createUserTooltips(posts);
@@ -229,6 +230,15 @@ define('forum/topic/posts', [
 
 		addBlockquoteEllipses(posts.find('[component="post/content"] > blockquote > blockquote'));
 		hidePostToolsForDeletedPosts(posts);
+	};
+
+	Posts.unloadImages = function() {
+		var images = components.get('post/content').find('img:not(.not-responsive)');
+		images.each(function() {
+			$(this).attr('data-src', $(this).attr('src'));
+			$(this).attr('data-state', 'unloaded');
+			$(this).attr('src', 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7');
+		});
 	};
 
 	Posts.loadImages = function(threshold) {