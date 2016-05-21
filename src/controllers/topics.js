86cab0787afd0577e57ce225bcb7307d94b17a26
Barış Soner Uşaklı <baris@nodebb.org>
2016-05-21 19:26:06 +0300

    closes #4668

--- src/controllers/topics.js
+++ src/controllers/topics.js
@@ -264,6 +264,7 @@ topicsController.get = function(req, res, callback) {
 		data['downvote:disabled'] = parseInt(meta.config['downvote:disabled'], 10) === 1;
 		data['feeds:disableRSS'] = parseInt(meta.config['feeds:disableRSS'], 10) === 1;
 		data.bookmarkThreshold = parseInt(meta.config.bookmarkThreshold, 10) || 5;
+		data.postEditDuration = parseInt(meta.config.postEditDuration, 10);
 		data.scrollToMyPost = settings.scrollToMyPost;
 		data.rssFeedUrl = nconf.get('relative_path') + '/topic/' + data.tid + '.rss';
 		data.pagination = pagination.create(currentPage, pageCount);
