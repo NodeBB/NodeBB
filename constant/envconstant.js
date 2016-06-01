ece5a82f4725f4d322644aa87aa444b1563c721c
xuzm <huahouye@gmail.com>
2016-06-01 16:53:09 +0800

    remove outer cfenv.

--- constant/envconstant.js
+++ constant/envconstant.js
@@ -12,7 +12,6 @@ try {
 	winston.info('This platform does not support cfenv.getAppEnv() method.');
 }
 
-var appEnv = cfenv.getAppEnv();
 // ipaddress openshift - Cloud Foundry(IBM Bluemix, Amazon EC2 etc.) - '127.0.0.1'
 env.IPADDRESS = process.env.OPENSHIFT_NODEJS_IP || appEnv.bind || '127.0.0.1';
 // port openshift - Cloud Foundry(IBM Bluemix, Amazon EC2 etc.) - 4567
