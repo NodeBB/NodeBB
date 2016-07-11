c8e64ae93b99535c5212f372f248b6bc18af4a1b
Julian Lam <julian@nodebb.org>
2016-07-11 18:26:04 -0400

    updating profile fields to allow plugins (e.g. the write-api) to change a user's profile picture
    @barisusakli please let me know if this is not ok.

--- src/user/profile.js
+++ src/user/profile.js
@@ -13,7 +13,7 @@ var plugins = require('../plugins');
 module.exports = function(User) {
 
 	User.updateProfile = function(uid, data, callback) {
-		var fields = ['username', 'email', 'fullname', 'website', 'location', 'groupTitle', 'birthday', 'signature', 'aboutme'];
+		var fields = ['username', 'email', 'fullname', 'website', 'location', 'groupTitle', 'birthday', 'signature', 'aboutme', 'picture', 'uploadedpicture'];
 
 		plugins.fireHook('filter:user.updateProfile', {uid: uid, data: data, fields: fields}, function(err, data) {
 			if (err) {
