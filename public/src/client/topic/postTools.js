c204eac93a0cd27c460455e086272f2985a91182
pichalite <pichalite.com@gmail.com>
2016-07-14 16:43:05 +0000

    translate language strings in votes modal

--- public/src/client/topic/postTools.js
+++ public/src/client/topic/postTools.js
@@ -377,15 +377,18 @@ define('forum/topic/postTools', ['share', 'navigator', 'components', 'translator
 			}
 
 			templates.parse('partials/modals/votes_modal', data, function(html) {
-				var dialog = bootbox.dialog({
-					title: 'Voters',
-					message: html,
-					className: 'vote-modal',
-					show: true
-				});
-
-				dialog.on('click', function() {
-					dialog.modal('hide');
+				translator.translate(html, function(translated) {
+					var dialog = bootbox.dialog({
+						title: 'Voters',
+						message: translated,
+						className: 'vote-modal',
+						show: true
+					});
+	
+					dialog.on('click', function() {
+						dialog.modal('hide');
+					});
+				    
 				});
 			});
 		});
