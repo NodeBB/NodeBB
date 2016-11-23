17f8afd0dcd2c41eb675595f6d1e6aaea9a74b15
barisusakli <barisusakli@gmail.com>
2016-11-23 16:09:46 +0300

    closes #5230

--- src/middleware/headers.js
+++ src/middleware/headers.js
@@ -26,7 +26,7 @@ module.exports = function (middleware) {
 
 		for (var key in headers) {
 			if (headers.hasOwnProperty(key)) {
-				res.setHeader(key, encodeURIComponent(headers[key]));
+				res.setHeader(key, encodeURI(headers[key]));
 			}
 		}
 
