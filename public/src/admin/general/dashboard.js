65cf2dbe5591753d2d32055a507212e57d0a691c
psychobunny <rodrigues.andrew@gmail.com>
2017-01-12 14:26:03 -0500

    move updateRoomUsage to after graph setup #5346

--- public/src/admin/general/dashboard.js
+++ public/src/admin/general/dashboard.js
@@ -37,7 +37,6 @@ define('admin/general/dashboard', ['semver', 'Chart', 'translator'], function (s
 
 	Admin.init = function () {
 		app.enterRoom('admin');
-		socket.emit('admin.rooms.getAll', Admin.updateRoomUsage);
 
 		isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
@@ -299,6 +298,7 @@ define('admin/general/dashboard', ['semver', 'Chart', 'translator'], function (s
 				updateTrafficGraph($(this).attr('data-units'), until);
 			});
 
+			socket.emit('admin.rooms.getAll', Admin.updateRoomUsage);
 			initiateDashboard();
 		});
 	}
