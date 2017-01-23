dbb71423a8a830f176fcd37cf44a53ee8c5f7690
Julian Lam <julian@nodebb.org>
2017-01-23 15:17:53 -0500

    executing core middlewares first before secondary or plugin middlewares

--- src/routes/helpers.js
+++ src/routes/helpers.js
@@ -3,7 +3,7 @@
 var helpers = {};
 
 helpers.setupPageRoute = function (router, name, middleware, middlewares, controller) {
-	middlewares = middlewares.concat([middleware.registrationComplete, middleware.pageView, middleware.pluginHooks]);
+	middlewares = [middleware.registrationComplete, middleware.pageView, middleware.pluginHooks].concat(middlewares);
 
 	router.get(name, middleware.busyCheck, middleware.buildHeader, middlewares, controller);
 	router.get('/api' + name, middlewares, controller);
