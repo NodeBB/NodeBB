b62a79c640d0b018475d9af4f76f19d49a9c3061
pichalite <pichalite.com@gmail.com>
2017-02-13 22:08:53 +0000

    Change HTML attributes used for widgets to data- attributes

--- public/src/widgets.js
+++ public/src/widgets.js
@@ -5,11 +5,11 @@
 	ajaxify.widgets = {};
 
 	ajaxify.widgets.reposition = function (location) {
-		$('body [has-widget-class]').each(function () {
+		$('body [data-has-widget-class]').each(function () {
 			var $this = $(this);
-			if ($this.attr('has-widget-target') === location) {
+			if ($this.attr('data-has-widget-target') === location) {
 				$this.removeClass();
-				$this.addClass($this.attr('has-widget-class'));
+				$this.addClass($this.attr('data-has-widget-class'));
 			}
 		});
 	};
@@ -22,8 +22,8 @@
 
 		var widgetLocations = ['sidebar', 'footer', 'header'];
 
-		$('#content [widget-area]').each(function () {
-			var location = $(this).attr('widget-area');
+		$('#content [data-widget-area]').each(function () {
+			var location = $(this).attr('data-widget-area');
 			if ($.inArray(location, widgetLocations) === -1) {
 				widgetLocations.push(location);
 			}
@@ -45,24 +45,24 @@
 					html += templates.parse(renderedWidgets[i].html, {});
 				}
 
-				var area = $('#content [widget-area="' + location + '"]');
+				var area = $('#content [data-widget-area="' + location + '"]');
 
 				if (!area.length && window.location.pathname.indexOf('/admin') === -1 && renderedWidgets.length) {
-					if (location === 'footer' && !$('#content [widget-area="footer"]').length) {
-						$('#content').append($('<div class="row"><div widget-area="footer" class="col-xs-12"></div></div>'));
-					} else if (location === 'sidebar' && !$('#content [widget-area="sidebar"]').length) {
+					if (location === 'footer' && !$('#content [data-widget-area="footer"]').length) {
+						$('#content').append($('<div class="row"><div data-widget-area="footer" class="col-xs-12"></div></div>'));
+					} else if (location === 'sidebar' && !$('#content [data-widget-area="sidebar"]').length) {
 						if ($('[component="account/cover"]').length) {
-							$('[component="account/cover"]').nextAll().wrapAll($('<div class="row"><div class="col-lg-9 col-xs-12"></div><div widget-area="sidebar" class="col-lg-3 col-xs-12"></div></div></div>'));
+							$('[component="account/cover"]').nextAll().wrapAll($('<div class="row"><div class="col-lg-9 col-xs-12"></div><div data-widget-area="sidebar" class="col-lg-3 col-xs-12"></div></div></div>'));
 						} else if ($('[component="groups/cover"]').length) {
-							$('[component="groups/cover"]').nextAll().wrapAll($('<div class="row"><div class="col-lg-9 col-xs-12"></div><div widget-area="sidebar" class="col-lg-3 col-xs-12"></div></div></div>'));
+							$('[component="groups/cover"]').nextAll().wrapAll($('<div class="row"><div class="col-lg-9 col-xs-12"></div><div data-widget-area="sidebar" class="col-lg-3 col-xs-12"></div></div></div>'));
 						} else {
-							$('#content > *').wrapAll($('<div class="row"><div class="col-lg-9 col-xs-12"></div><div widget-area="sidebar" class="col-lg-3 col-xs-12"></div></div></div>'));
+							$('#content > *').wrapAll($('<div class="row"><div class="col-lg-9 col-xs-12"></div><div data-widget-area="sidebar" class="col-lg-3 col-xs-12"></div></div></div>'));
 						}
-					} else if (location === 'header' && !$('#content [widget-area="header"]').length) {
-						$('#content').prepend($('<div class="row"><div widget-area="header" class="col-xs-12"></div></div>'));
+					} else if (location === 'header' && !$('#content [data-widget-area="header"]').length) {
+						$('#content').prepend($('<div class="row"><div data-widget-area="header" class="col-xs-12"></div></div>'));
 					}
 
-					area = $('#content [widget-area="' + location + '"]');
+					area = $('#content [data-widget-area="' + location + '"]');
 				}
 
 				area.html(html);
@@ -73,7 +73,7 @@
 				}
 			}
 
-			var widgetAreas = $('#content [widget-area]');
+			var widgetAreas = $('#content [data-widget-area]');
 			widgetAreas.find('img:not(.not-responsive)').addClass('img-responsive');
 			widgetAreas.find('.timeago').timeago();
 			widgetAreas.find('img[title].teaser-pic,img[title].user-img').each(function () {
