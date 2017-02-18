1b43faba1ab0680b6bc753dd596d0c2bf8c65e44
Barış Soner Uşaklı <baris@nodebb.org>
2017-02-19 02:44:38 +0300

    closes #5441

--- src/start.js
+++ src/start.js
@@ -99,6 +99,7 @@ function setupConfigs() {
 	nconf.set('use_port', !!urlObject.port);
 	nconf.set('relative_path', relativePath);
 	nconf.set('port', urlObject.port || nconf.get('port') || nconf.get('PORT') || (nconf.get('PORT_ENV_VAR') ? nconf.get(nconf.get('PORT_ENV_VAR')) : false) || 4567);
+	nconf.set('upload_url', '/assets/uploads');
 }
 
 function printStartupInfo() {
