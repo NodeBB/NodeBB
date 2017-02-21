89380824f892e8e2e96b148c0f7c6dd0228c7b52
Barış Soner Uşaklı <baris@nodebb.org>
2017-02-21 17:00:54 +0300

    navigation test

--- src/navigation/admin.js
+++ src/navigation/admin.js
@@ -1,14 +1,12 @@
 "use strict";
 
-
-
 var async = require('async');
 var plugins = require('../plugins');
 var db = require('../database');
 var translator = require('../../public/src/modules/translator');
 var pubsub = require('../pubsub');
 
-var admin = {};
+var admin = module.exports;
 admin.cache = null;
 
 pubsub.on('admin:navigation:save', function () {
@@ -71,5 +69,3 @@ function getAvailable(callback) {
 
 	plugins.fireHook('filter:navigation.available', core, callback);
 }
-
-module.exports = admin;
