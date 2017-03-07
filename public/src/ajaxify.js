8a123f7c1c78b94632f4df952591a2db4f37bb85
barisusakli <barisusakli@gmail.com>
2017-03-07 16:13:09 +0300

    closes #5509

--- public/src/ajaxify.js
+++ public/src/ajaxify.js
@@ -99,7 +99,7 @@ $(document).ready(function () {
 	};
 
 	ajaxify.handleRedirects = function (url) {
-		url = ajaxify.removeRelativePath(url.replace(/\/$/, '')).toLowerCase();
+		url = ajaxify.removeRelativePath(url.replace(/^\/|\/$/g, '')).toLowerCase();
 		var isClientToAdmin = url.startsWith('admin') && window.location.pathname.indexOf(RELATIVE_PATH + '/admin') !== 0;
 		var isAdminToClient = !url.startsWith('admin') && window.location.pathname.indexOf(RELATIVE_PATH + '/admin') === 0;
 		var uploadsOrApi = url.startsWith('assets/uploads') || url.startsWith('uploads') || url.startsWith('api');
