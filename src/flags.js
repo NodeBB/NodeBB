5a9b7b673a06e8e00c23ffef1570760ab6a3c890
Barış Soner Uşaklı <baris@nodebb.org>
2017-05-09 14:16:21 -0400

    closes #5659

--- src/flags.js
+++ src/flags.js
@@ -15,7 +15,7 @@ var utils = require('../public/src/utils');
 var _ = require('underscore');
 var S = require('string');
 
-var Flags = {};
+var Flags = module.exports;
 
 Flags.get = function (flagId, callback) {
 	async.waterfall([
@@ -26,6 +26,9 @@ Flags.get = function (flagId, callback) {
 			notes: async.apply(Flags.getNotes, flagId),
 		}),
 		function (data, next) {
+			if (!data.base) {
+				return callback();
+			}
 			// Second stage
 			async.parallel({
 				userObj: async.apply(user.getUserFields, data.base.uid, ['username', 'userslug', 'picture']),
