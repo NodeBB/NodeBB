d3011396443ac1d7205a23d69b716951fa6eb83d
Barış Soner Uşaklı <baris@nodebb.org>
2017-05-24 16:38:16 -0400

    logger test

---
+++ test/logger.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/* global require, after, before*/
+
+
+var async = require('async');
+var assert = require('assert');
+var path = require('path');
+
+var db = require('./mocks/databasemock');
+var logger = require('../src/logger');
+var index = require('../src/socket.io');
+var meta = require('../src/meta');
+
+describe('logger', function () {
+	before(function (done) {
+		done();
+	});
+
+	it('should enable logging', function (done) {
+		meta.config.loggerStatus = 1;
+		meta.config.loggerIOStatus = 1;
+		var loggerPath = path.join(__dirname, '..', 'logs', 'logger.log');
+		logger.monitorConfig({ io: index.server }, { loggerPath: loggerPath });
+		setTimeout(function () {
+			meta.config.loggerStatus = 0;
+			meta.config.loggerIOStatus = 0;
+			done();
+		}, 500);
+	});
+
+	after(function (done) {
+		db.emptydb(done);
+	});
+});
