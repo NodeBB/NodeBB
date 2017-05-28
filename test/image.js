932247f440339e99f2d2c6dec849eab700b845f5
Barış Soner Uşaklı <baris@nodebb.org>
2017-05-28 17:21:44 -0400

    up deps

--- test/image.js
+++ test/image.js
@@ -18,4 +18,21 @@ describe('image', function () {
 			});
 		});
 	});
+
+	it('should resize an image', function (done) {
+		image.resizeImage({
+			path: path.join(__dirname, 'files/normalise.jpg'),
+			target: path.join(__dirname, 'files/normalise-resized.jpg'),
+			width: 50,
+			height: 40,
+		}, function (err) {
+			assert.ifError(err);
+			image.size(path.join(__dirname, 'files/normalise-resized.jpg'), function (err, bitmap) {
+				assert.ifError(err);
+				assert.equal(bitmap.width, 50);
+				assert.equal(bitmap.height, 40);
+				done();
+			});
+		});
+	});
 });
