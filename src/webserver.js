d98b3e8056e8dd049292e1d816ca7b71139230de
Julian Lam <julian@nodebb.org>
2017-06-22 11:10:23 -0400

    automatically lowercasing env vars in nconf, fixes #2544

--- src/webserver.js
+++ src/webserver.js
@@ -231,6 +231,7 @@ function setupAutoLocale(app, callback) {
 
 function listen(callback) {
 	callback = callback || function () { };
+	console.log('derp', nconf.get('port'));
 	var port = parseInt(nconf.get('port'), 10);
 	var isSocket = isNaN(port);
 	var socketPath = isSocket ? nconf.get('port') : '';
