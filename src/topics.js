0f5be5ae255b47e6de4aeb02df061559ae17cf0c
Barış Soner Uşaklı <baris@nodebb.org>
2017-07-12 19:44:35 -0400

    remove plugin.hook check

--- src/topics.js
+++ src/topics.js
@@ -326,12 +326,8 @@ Topics.isLocked = function (tid, callback) {
 };
 
 Topics.search = function (tid, term, callback) {
-	if (plugins.hasListeners('filter:topic.search')) {
-		plugins.fireHook('filter:topic.search', {
-			tid: tid,
-			term: term,
-		}, callback);
-	} else {
-		callback(new Error('[[error:no-plugins-available]]'), []);
-	}
+	plugins.fireHook('filter:topic.search', {
+		tid: tid,
+		term: term,
+	}, callback);
 };
