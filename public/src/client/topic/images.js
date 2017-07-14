f55fecf73a78f5b6930060618c7afece52f0f3db
Stjepan Osojnički <sosojni@hotmail.com>
2017-07-14 14:53:16 +0200

    trying to fix Travis error

--- public/src/client/topic/images.js
+++ public/src/client/topic/images.js
@@ -121,7 +121,7 @@ define('forum/topic/images', [
 					+ (!srcExt && altExt ? ' download="' + altFilename + '" ' : '')
 					+ ' target="_blank" >');
 			}
-			
+
 			$(window).trigger('action:images.loaded');
 		});
 	};
