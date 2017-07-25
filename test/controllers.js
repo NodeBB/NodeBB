5ce9f898b2aa53ac94ea70b36287f117bfabb788
Baris Usakli <barisusakli@gmail.com>
2017-07-25 16:23:43 -0400

    fix test

--- test/controllers.js
+++ test/controllers.js
@@ -1776,8 +1776,7 @@ describe('Controllers', function () {
 		it('should redirect to login', function (done) {
 			request(nconf.get('url') + '//api/admin/advanced/database', { json: true }, function (err, res, body) {
 				assert.ifError(err);
-				assert.equal(res.statusCode, 200);
-				assert(body.indexOf('Login to your account') !== -1);
+				assert.equal(res.statusCode, 401);
 				done();
 			});
 		});
