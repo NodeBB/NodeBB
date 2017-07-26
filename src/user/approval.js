541b78c26de9fbd7dbb68a17c7b77cf2d301dc53
Baris Usakli <barisusakli@gmail.com>
2017-07-26 13:39:34 -0400

    fix ipMatch data

--- src/user/approval.js
+++ src/user/approval.js
@@ -178,7 +178,7 @@ module.exports = function (User) {
 
 					async.parallel([
 						function (next) {
-							getIPMatchedUsers(user.ip, next);
+							getIPMatchedUsers(user, next);
 						},
 						function (next) {
 							getSpamData(user, next);
@@ -197,14 +197,18 @@ module.exports = function (User) {
 		], callback);
 	};
 
-	function getIPMatchedUsers(ip, callback) {
+	function getIPMatchedUsers(user, callback) {
 		async.waterfall([
 			function (next) {
-				User.getUidsFromSet('ip:' + ip + ':uid', 0, -1, next);
+				User.getUidsFromSet('ip:' + user.ip + ':uid', 0, -1, next);
 			},
 			function (uids, next) {
 				User.getUsersFields(uids, ['uid', 'username', 'picture'], next);
 			},
+			function (data, next) {
+				user.ipMatch = data;
+				next();
+			}
 		], callback);
 	}
 
