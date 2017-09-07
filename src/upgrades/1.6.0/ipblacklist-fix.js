f5a9216909b89c06450773f1f7e069b49849e2eb
Baris Usakli <barisusakli@gmail.com>
2017-09-07 13:34:32 -0400

    closes #5917

---
+++ src/upgrades/1.6.0/ipblacklist-fix.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var async = require('async');
+
+var db = require('../../database');
+
+module.exports = {
+	name: 'Changing ip blacklist storage to object',
+	timestamp: Date.UTC(2017, 8, 7),
+	method: function (callback) {
+		var rules;
+		async.waterfall([
+			function (next) {
+				db.get('ip-blacklist-rules', next);
+			},
+			function (_rules, next) {
+				rules = _rules;
+				if (!rules) {
+					return next();
+				}
+				db.delete('ip-blacklist-rules', next);
+			},
+			function (next) {
+				db.setObject('ip-blacklist-rules', { rules: rules }, next);
+			},
+		], callback);
+	},
+};
