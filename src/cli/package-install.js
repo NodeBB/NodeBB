NodeBB/NodeBB

commit 9b5e0f9e95ca4d44c15ea388bea4abc715bac446

updated upgrade logic to not break ACP restart flow

src/cli/package-install.js
@@ -30,7 +30,7 @@ function updatePackageFile() {
 exports.updatePackageFile = updatePackageFile;
 
 function installAll() {
-	process.stdout.write('\n');
+	process.stdout.write('  started\n'.green);
 
 	var prod = global.env !== 'development';
 	var command = 'npm install';

src/cli/upgrade.js
@@ -16,6 +16,7 @@ var steps = {
 		handler: function (next) {
 			packageInstall.updatePackageFile();
 			packageInstall.preserveExtraneousPlugins();
+			process.stdout.write('  OK\n'.green);
 			next();
 		},
 	},
@@ -54,11 +55,8 @@ function runSteps(tasks) {
 	tasks = tasks.map(function (key, i) {
 		return function (next) {
 			process.stdout.write('\n' + ((i + 1) + '. ').bold + steps[key].message.yellow);
-			return steps[key].handler(function (err, inhibitOk) {
+			return steps[key].handler(function (err) {
 				if (err) { return next(err); }
-				if (!inhibitOk) {
-					process.stdout.write('  OK'.green + '\n'.reset);
-				}
 				next();
 			});
 		};

src/cli/upgrade-plugins.js
@@ -212,7 +212,7 @@ function upgradePlugins(callback) {
 				});
 			} else {
 				console.log('Package upgrades skipped'.yellow + '. Check for upgrades at any time by running "'.reset + './nodebb upgrade -p'.green + '".'.reset);
-				callback(null, true);
+				callback();
 			}
 		});
 	});

src/meta/build.js
@@ -212,7 +212,7 @@ function build(targets, callback) {
 		}
 
 		winston.info('[build] Asset compilation successful. Completed in ' + totalTime + 'sec.');
-		callback(null, true);
+		callback();
 	});
 }
 

commit d1e02c73502a2096018d78aa487b8a1dbea0356f

Fix auto-install regression
introduced in 9b5e0f9e95ca4d44c15ea388bea4abc715bac446

src/cli/package-install.js
@@ -30,8 +30,6 @@ function updatePackageFile() {
 exports.updatePackageFile = updatePackageFile;
 
 function installAll() {
-	process.stdout.write('  started\n'.green);
-
 	var prod = global.env !== 'development';
 	var command = 'npm install';
 	try {

src/cli/upgrade.js
@@ -23,6 +23,7 @@ var steps = {
 	install: {
 		message: 'Bringing base dependencies up to date...',
 		handler: function (next) {
+			process.stdout.write('  started\n'.green);
 			packageInstall.installAll();
 			next();
 		},