59573450c3def007c122ab2fe1532cacd8b82aa0
Ben Lubar <ben.lubar@gmail.com>
2018-01-07 16:18:54 -0600

    [pubsub] Delay messages sent before the database is ready until the database is ready.

--- src/pubsub.js
+++ src/pubsub.js
@@ -2,4 +2,65 @@
 
 var nconf = require('nconf');
 
-module.exports = nconf.get('redis') ? require('./database/redis').pubsub : require('./database').pubsub;
+var real;
+var fake = {
+	publishQueue: [],
+	publish: function (event, data) {
+		fake.publishQueue.push({ event: event, data: data });
+	},
+	listenQueue: {},
+	on: function (event, callback) {
+		if (!Object.prototype.hasOwnProperty.call(fake.listenQueue, event)) {
+			fake.listenQueue[event] = [];
+		}
+		fake.listenQueue[event].push(callback);
+	},
+	removeAllListeners: function (event) {
+		delete fake.listenQueue[event];
+	},
+};
+
+function get() {
+	if (real) {
+		return real;
+	}
+
+	var pubsub;
+
+	if (nconf.get('redis')) {
+		pubsub = require('./database/redis').pubsub;
+	} else {
+		pubsub = require('./database').pubsub;
+	}
+
+	if (!pubsub) {
+		return fake;
+	}
+
+	Object.keys(fake.listenQueue).forEach(function (event) {
+		fake.listenQueue[event].forEach(function (callback) {
+			pubsub.on(event, callback);
+		});
+	});
+
+	fake.publishQueue.forEach(function (msg) {
+		pubsub.publish(msg.event, msg.data);
+	});
+
+	real = pubsub;
+	fake = null;
+
+	return pubsub;
+}
+
+module.exports = {
+	publish: function (event, data) {
+		get().publish(event, data);
+	},
+	on: function (event, callback) {
+		get().on(event, callback);
+	},
+	removeAllListeners: function (event) {
+		get().removeAllListeners(event);
+	},
+};
