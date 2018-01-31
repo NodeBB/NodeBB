a5bae451f2b7eb6531dbfe14901b85170ff36f22
Andrew Rodrigues <rodrigues.andrew@gmail.com>
2018-01-30 19:53:38 -0500

    strip html tags from email subject

--- src/notifications.js
+++ src/notifications.js
@@ -220,7 +220,7 @@ function pushToUids(uids, notification, callback) {
 		async.eachLimit(uids, 3, function (uid, next) {
 			emailer.send('notification', uid, {
 				path: notification.path,
-				subject: notification.subject || '[[notifications:new_notification_from, ' + meta.config.title + ']]',
+				subject: utils.stripHTMLTags(notification.subject || '[[notifications:new_notification_from, ' + meta.config.title + ']]'),
 				intro: utils.stripHTMLTags(notification.bodyShort),
 				body: utils.stripHTMLTags(notification.bodyLong || ''),
 				showUnsubscribe: true,
