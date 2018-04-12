3bc0266aa4fd4fff89f2c7ea8c9b12901bc50f51
Julian Lam <julian@nodebb.org>
2018-04-12 19:58:21 -0400

    fixing tests

--- test/authentication.js
+++ test/authentication.js
@@ -169,6 +169,7 @@ describe('authentication', function () {
 				}, function (err, response, body) {
 					assert.ifError(err);
 					assert(body);
+					console.log(body);
 					assert.equal(body.username, 'admin');
 					assert.equal(body.email, 'admin@nodebb.org');
 					user.getSettings(body.uid, function (err, settings) {
