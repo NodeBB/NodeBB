7b93de2731c353b5272c134efc703a45014b2b7b
Barış Soner Uşaklı <baris@nodebb.org>
2018-04-13 18:16:47 -0400

    increase post cache size

--- src/posts/cache.js
+++ src/posts/cache.js
@@ -4,7 +4,7 @@ var LRU = require('lru-cache');
 var meta = require('../meta');
 
 var cache = LRU({
-	max: parseInt(meta.config.postCacheSize, 10) || 1048576,
+	max: parseInt(meta.config.postCacheSize, 10) || 5242880,
 	length: function (n) { return n.length; },
 	maxAge: 0,
 });
