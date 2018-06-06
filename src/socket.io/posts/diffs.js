779d03cf574a55483a77dc11c53a2777e5adfe20
Barış Soner Uşaklı <baris@nodebb.org>
2018-06-06 16:21:48 -0400

    move timestamp to server

--- src/socket.io/posts/diffs.js
+++ src/socket.io/posts/diffs.js
@@ -1,10 +1,19 @@
 'use strict';
 
+var async = require('async');
 var posts = require('../../posts');
 
 module.exports = function (SocketPosts) {
 	SocketPosts.getDiffs = function (socket, data, callback) {
-		posts.diffs.list(data.pid, callback);
+		async.waterfall([
+			function (next) {
+				posts.diffs.list(data.pid, next);
+			},
+			function (timestamps, next) {
+				timestamps.unshift(Date.now());
+				next(null, timestamps);
+			},
+		], callback);
 	};
 
 	SocketPosts.showPostAt = function (socket, data, callback) {
