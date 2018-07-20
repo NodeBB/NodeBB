f1a6537fc2dada85b539f00dd17545828f93fb49
Ben Lubar <ben.lubar+github@gmail.com>
2018-07-20 09:29:51 -0500

    Add notification frequencies from nodebb-plugin-upvote-notifications. (#6640)
    * Add notification frequencies from nodebb-plugin-upvote-notifications.
    
    https://github.com/boomzillawtf/nodebb-plugin-upvote-notifications
    
    * Fix editing settings clearing upvote notification preferences.

--- src/controllers/accounts/settings.js
+++ src/controllers/accounts/settings.js
@@ -148,7 +148,9 @@ settingsController.get = function (req, res, callback) {
 
 			var notifFreqOptions = [
 				'all',
+				'first',
 				'everyTen',
+				'threshold',
 				'logarithmic',
 				'disabled',
 			];
@@ -156,7 +158,7 @@ settingsController.get = function (req, res, callback) {
 			userData.upvoteNotifFreq = notifFreqOptions.map(function (name) {
 				return {
 					name: name,
-					selected: name === userData.notifFreqOptions,
+					selected: name === userData.settings.upvoteNotifFreq,
 				};
 			});
 
