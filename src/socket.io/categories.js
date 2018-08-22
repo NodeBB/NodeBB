cacdd9ff377015b285658313a54aca03a8acfefd
André Zanghelini <An-dz@users.noreply.github.com>
2018-08-21 22:24:32 -0300

    Fix passing undefined to pushUnreadCount

--- src/socket.io/categories.js
+++ src/socket.io/categories.js
@@ -174,6 +174,8 @@ SocketCategories.ignore = function (socket, cid, callback) {
 };
 
 function ignoreOrWatch(fn, socket, cid, callback) {
+	var cids = [parseInt(cid, 10)];
+
 	async.waterfall([
 		function (next) {
 			db.getSortedSetRange('categories:cid', 0, -1, next);
@@ -187,10 +189,7 @@ function ignoreOrWatch(fn, socket, cid, callback) {
 				c.parentCid = parseInt(c.parentCid, 10);
 			});
 
-			var cids = [parseInt(cid, 10)];
-
 			// filter to subcategories of cid
-
 			var cat;
 			do {
 				cat = categoryData.find(function (c) {
@@ -203,10 +202,13 @@ function ignoreOrWatch(fn, socket, cid, callback) {
 
 			async.each(cids, function (cid, next) {
 				fn(socket.uid, cid, next);
-			}, next(null, cids));
+			}, next);
 		},
-		function (cids, next) {
-			topics.pushUnreadCount(socket.uid, next(null, cids));
+		function (next) {
+			topics.pushUnreadCount(socket.uid, next);
+		},
+		function (next) {
+			next(null, cids);
 		},
 	], callback);
 }
