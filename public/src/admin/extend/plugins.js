ebaea9b6c11be8099b8664eb89337747dbc37672
Barış Soner Uşaklı <baris@nodebb.org>
2018-09-08 14:01:15 -0400

    closes #6743

--- public/src/admin/extend/plugins.js
+++ public/src/admin/extend/plugins.js
@@ -20,7 +20,7 @@ define('admin/extend/plugins', ['jqueryui', 'translator', 'benchpress'], functio
 		pluginsList.on('click', 'button[data-action="toggleActive"]', function () {
 			var pluginEl = $(this).parents('li');
 			pluginID = pluginEl.attr('data-plugin-id');
-			var btn = $('#' + pluginID + ' [data-action="toggleActive"]');
+			var btn = $('[id="' + pluginID + '"] [data-action="toggleActive"]');
 
 			var pluginData = ajaxify.data.installed[pluginEl.attr('data-plugin-index')];
 
@@ -34,7 +34,7 @@ define('admin/extend/plugins', ['jqueryui', 'translator', 'benchpress'], functio
 						btn.toggleClass('btn-warning', status.active).toggleClass('btn-success', !status.active);
 
 						// clone it to active plugins tab
-						if (status.active && !$('#active #' + pluginID).length) {
+						if (status.active && !$('#active [id="' + pluginID + '"]').length) {
 							$('#active ul').prepend(pluginEl.clone(true));
 						}
 
