d975c3f41100d19810622c52542ac9bcc1055b7b
Baris Usakli <barisusakli@gmail.com>
2018-10-12 15:54:21 -0400

    allow groups.join to take an array of group names

--- src/groups/membership.js
+++ src/groups/membership.js
@@ -21,73 +21,93 @@ var cache = LRU({
 module.exports = function (Groups) {
 	Groups.cache = cache;
 
-	Groups.join = function (groupName, uid, callback) {
+	Groups.join = function (groupNames, uid, callback) {
 		callback = callback || function () {};
 
-		if (!groupName) {
+		if (!groupNames) {
 			return callback(new Error('[[error:invalid-data]]'));
+		}
+
+		if (!Array.isArray(groupNames)) {
+			groupNames = [groupNames];
 		}
 
 		if (!uid) {
 			return callback(new Error('[[error:invalid-uid]]'));
 		}
-
+		var isAdmin;
 		async.waterfall([
 			function (next) {
-				Groups.isMember(uid, groupName, next);
-			},
-			function (isMember, next) {
-				if (isMember) {
-					return callback();
-				}
-				Groups.exists(groupName, next);
-			},
-			function (exists, next) {
-				if (exists) {
-					return next();
-				}
-				Groups.create({
-					name: groupName,
-					description: '',
-					hidden: 1,
-				}, function (err) {
-					if (err && err.message !== '[[error:group-already-exists]]') {
-						winston.error('[groups.join] Could not create new hidden group', err);
-						return callback(err);
-					}
-					next();
-				});
-			},
-			function (next) {
 				async.parallel({
-					isAdmin: function (next) {
-						user.isAdministrator(uid, next);
-					},
-					isHidden: function (next) {
-						Groups.isHidden(groupName, next);
-					},
+					isMembers: async.apply(Groups.isMemberOfGroups, uid, groupNames),
+					exists: async.apply(Groups.exists, groupNames),
+					isAdmin: async.apply(user.isAdministrator, uid),
 				}, next);
 			},
 			function (results, next) {
+				isAdmin = results.isAdmin;
+
+				var groupsToCreate = groupNames.filter(function (groupName, index) {
+					return groupName && !results.exists[index];
+				});
+
+				groupNames = groupNames.filter(function (groupName, index) {
+					return !results.isMembers[index];
+				});
+
+				if (!groupNames.length) {
+					return callback();
+				}
+
+				if (!groupsToCreate.length) {
+					return next();
+				}
+				async.eachSeries(groupsToCreate, function (groupName, next) {
+					Groups.create({
+						name: groupName,
+						hidden: 1,
+					}, function (err) {
+						if (err && err.message !== '[[error:group-already-exists]]') {
+							winston.error('[groups.join] Could not create new hidden group', err);
+							return callback(err);
+						}
+						next();
+					});
+				}, next);
+			},
+			function (next) {
 				var tasks = [
-					async.apply(db.sortedSetAdd, 'group:' + groupName + ':members', Date.now(), uid),
-					async.apply(db.incrObjectField, 'group:' + groupName, 'memberCount'),
+					async.apply(db.sortedSetsAdd, groupNames.map(groupName => 'group:' + groupName + ':members'), Date.now(), uid),
+					async.apply(db.incrObjectField, groupNames.map(groupName => 'group:' + groupName), 'memberCount'),
 				];
-				if (results.isAdmin) {
-					tasks.push(async.apply(db.setAdd, 'group:' + groupName + ':owners', uid));
+				if (isAdmin) {
+					tasks.push(async.apply(db.setsAdd, groupNames.map(groupName => 'group:' + groupName + ':owners'), uid));
 				}
-				if (!results.isHidden) {
-					tasks.push(async.apply(db.sortedSetIncrBy, 'groups:visible:memberCount', 1, groupName));
-				}
+
 				async.parallel(tasks, next);
 			},
 			function (results, next) {
-				clearCache(uid, groupName);
-				setGroupTitleIfNotSet(groupName, uid, next);
+				clearCache(uid, groupNames);
+				Groups.getGroupsFields(groupNames, ['name', 'hidden', 'memberCount'], next);
+			},
+			function (groupData, next) {
+				var visibleGroups = groupData.filter(function (groupData) {
+					return groupData && parseInt(groupData.hidden, 10) !== 1;
+				});
+
+				if (visibleGroups.length) {
+					db.sortedSetAdd('groups:visible:memberCount', visibleGroups.map(groupData => groupData.memberCount), visibleGroups.map(groupData => groupData.name), next);
+				} else {
+					next();
+				}
+			},
+			function (next) {
+				setGroupTitleIfNotSet(groupNames, uid, next);
 			},
 			function (next) {
 				plugins.fireHook('action:group.join', {
-					groupName: groupName,
+					groupName: groupNames[0],
+					groupNames: groupNames,
 					uid: uid,
 				});
 				next();
@@ -95,17 +115,20 @@ module.exports = function (Groups) {
 		], callback);
 	};
 
-	function setGroupTitleIfNotSet(groupName, uid, callback) {
-		if (groupName === 'registered-users' || Groups.isPrivilegeGroup(groupName)) {
+	function setGroupTitleIfNotSet(groupNames, uid, callback) {
+		groupNames = groupNames.filter(function (groupName) {
+			return groupName !== 'registered-users' && !Groups.isPrivilegeGroup(groupName);
+		});
+		if (!groupNames.length) {
 			return callback();
 		}
 
 		db.getObjectField('user:' + uid, 'groupTitle', function (err, currentTitle) {
-			if (err || (currentTitle || currentTitle === '')) {
+			if (err || currentTitle || currentTitle === '') {
 				return callback(err);
 			}
 
-			user.setUserField(uid, 'groupTitle', JSON.stringify([groupName]), callback);
+			user.setUserField(uid, 'groupTitle', JSON.stringify(groupNames), callback);
 		});
 	}
 
