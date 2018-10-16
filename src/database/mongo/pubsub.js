aa567a5f2dded13c24e12f3e88123be7883338dd
Barış Soner Uşaklı <baris@nodebb.org>
2018-10-16 19:12:33 -0400

    print out connString and opts

--- src/database/mongo/pubsub.js
+++ src/database/mongo/pubsub.js
@@ -2,9 +2,10 @@
 
 var mubsub = require('mubsub-nbb');
 var db = require('../mongo');
-
-var client = mubsub(db.getConnectionString(), db.getConnectionOptions());
+const connString = db.getConnectionString();
+const connOptions = db.getConnectionOptions();
+var client = mubsub(connString, connOptions);
 client.on('error', function (err) {
-	console.error('mongo pubsub error', err);
+	console.error('mongo pubsub error ' + connString, connOptions, err);
 });
 module.exports = client.channel('pubsub');
