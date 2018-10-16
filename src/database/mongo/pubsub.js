154d7076d9b61d3e0cb08e6ae6497be7db2d93bb
Barış Soner Uşaklı <baris@nodebb.org>
2018-10-16 19:57:24 -0400

    dont pass options

--- src/database/mongo/pubsub.js
+++ src/database/mongo/pubsub.js
@@ -3,9 +3,9 @@
 var mubsub = require('mubsub-nbb');
 var db = require('../mongo');
 const connString = db.getConnectionString();
-const connOptions = db.getConnectionOptions();
-var client = mubsub(connString, connOptions);
+
+var client = mubsub(connString);
 client.on('error', function (err) {
-	console.error('mongo pubsub error ' + connString, connOptions, err);
+	console.error('mongo pubsub error ' + connString, err);
 });
 module.exports = client.channel('pubsub');
