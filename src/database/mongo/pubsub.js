1d15dc089b10ac5d8b41606a3b34a084ad43605f
Barış Soner Uşaklı <baris@nodebb.org>
2018-10-16 20:34:49 -0400

    revert isCluster

--- src/database/mongo/pubsub.js
+++ src/database/mongo/pubsub.js
@@ -2,10 +2,6 @@
 
 var mubsub = require('mubsub-nbb');
 var db = require('../mongo');
-const connString = db.getConnectionString();
+var client = mubsub(db.getConnectionString());
 
-var client = mubsub(connString);
-client.on('error', function (err) {
-	console.error('mongo pubsub error ' + connString, err);
-});
 module.exports = client.channel('pubsub');
