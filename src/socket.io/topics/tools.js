541997a75589619cbcc044a961aa5ce476067123
Barış Soner Uşaklı <baris@nodebb.org>
2018-10-24 19:25:40 -0400

    fix lint

--- src/socket.io/topics/tools.js
+++ src/socket.io/topics/tools.js
@@ -30,7 +30,7 @@ module.exports = function (SocketTopics) {
 			},
 			function (results, next) {
 				if (!results.topic) {
-					return next(new Error('[[error:no-topic]]'))
+					return next(new Error('[[error:no-topic]]'));
 				}
 				topic = results.topic;
 				topic.privileges = results.privileges;
