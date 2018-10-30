0de60cfdca87aa38656bde93f580f39bb496232c
Barış Soner Uşaklı <baris@nodebb.org>
2018-10-30 18:58:02 -0400

    fix mongodb tests

--- src/database/mongo.js
+++ src/database/mongo.js
@@ -64,7 +64,7 @@ mongoModule.helpers.mongo = require('./mongo/helpers');
 
 mongoModule.getConnectionString = function () {
 	var usernamePassword = '';
-	var uri = nconf.get('mongo:uri');
+	var uri = nconf.get('mongo:uri') || '';
 	if (nconf.get('mongo:username') && nconf.get('mongo:password')) {
 		usernamePassword = nconf.get('mongo:username') + ':' + encodeURIComponent(nconf.get('mongo:password')) + '@';
 	} else if (!uri.includes('@') || !uri.slice(uri.indexOf('://') + 3, uri.indexOf('@'))) {
