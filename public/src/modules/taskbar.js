af4fd5979f01ee9f4f40fd54c064f04422d1f1ae
Julian Lam <julian@nodebb.org>
2019-01-11 10:29:08 -0500

    Revert "fix: #7216, hide taskbar on chat modal invocation on mobile"
    This reverts commit a70db885cf796de6b53b4deca297dd32e51c8184.

--- public/src/modules/taskbar.js
+++ public/src/modules/taskbar.js
@@ -182,9 +182,5 @@ define('taskbar', ['benchpress', 'translator'], function (Benchpress, translator
 		taskbar.tasklist.find('[data-module="' + module + '"][data-uuid="' + uuid + '"] [component="taskbar/title"]').text(newTitle);
 	};
 
-	taskbar.toggleVisibility = function (visible) {
-		taskbar.taskbar.toggleClass('hidden', !visible);
-	};
-
 	return taskbar;
 });
