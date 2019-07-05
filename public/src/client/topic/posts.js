49e3a368f8a30b65c4574ee9e254438aef26233b
Julian Lam <julian@nodebb.org>
2019-07-05 14:58:34 -0400

    fix: #7423 private uploads are linked to login page, for guests

--- public/src/client/topic/posts.js
+++ public/src/client/topic/posts.js
@@ -254,9 +254,10 @@ define('forum/topic/posts', [
 		Posts.showBottomPostBar();
 		posts.find('[component="post/content"] img:not(.not-responsive)').addClass('img-responsive');
 		Posts.addBlockquoteEllipses(posts);
-		hidePostToolsForDeletedPosts(posts);
 
+		hidePostToolsForDeletedPosts(posts);
 		addNecroPostMessage();
+		handlePrivateUploads(posts);
 	};
 
 	function addNecroPostMessage() {
@@ -279,6 +280,27 @@ define('forum/topic/posts', [
 
 				$.timeago.settings.strings.suffixAgo = ago;
 			}
+		});
+	}
+
+	function handlePrivateUploads(posts) {
+		if (app.user.uid) {
+			return;
+		}
+
+		// Replace all requests for uploaded images/files with a login link
+		var loginEl = document.createElement('a');
+		loginEl.className = 'login-required';
+		loginEl.href = config.relative_path + '/login';
+		loginEl.appendChild(document.createTextNode('🔒 Log in to view'));
+
+		posts.each(function (idx, postEl) {
+			$(postEl).find('[component="post/content"] img').each(function (idx, imgEl) {
+				imgEl = $(imgEl);
+				if (imgEl.attr('src').startsWith(config.relative_path + config.upload_url)) {
+					imgEl.replaceWith(loginEl.cloneNode(true));
+				}
+			});
 		});
 	}
 
