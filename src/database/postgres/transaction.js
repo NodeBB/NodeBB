350dc81a47e5df9f7cb19000c75563fa493d7d05
Barış Soner Uşaklı <barisusakli@gmail.com>
2019-07-04 21:21:03 -0400

    feat: :dog:

--- src/database/postgres/transaction.js
+++ src/database/postgres/transaction.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = function (db, dbNamespace, module) {
+	const util = require('util');
 	module.transaction = async function (perform) {
 		if (dbNamespace.active && dbNamespace.get('db')) {
 			const client = dbNamespace.get('db');
@@ -13,19 +14,52 @@ module.exports = function (db, dbNamespace, module) {
 			}
 			return await client.query(`RELEASE SAVEPOINT nodebb_subtx`);
 		}
+		const testFn = util.promisify(test);
+		await testFn(perform);
+		// const client = await db.connect();
+		// await dbNamespace.run(function () {
+		// 	dbNamespace.set('db', client);
+		// });
 
-		const client = await db.connect();
-		await dbNamespace.run();
-		dbNamespace.set('db', client);
-		try {
-			await client.query(`BEGIN`);
-			await perform(module);
-			await client.query('COMMIT');
-			dbNamespace.set('db', null);
-		} catch (err) {
-			await client.query('ROLLBACK');
-			dbNamespace.set('db', null);
-			throw err;
-		}
+		// try {
+		// 	await client.query(`BEGIN`);
+		// 	await perform(module);
+		// 	await client.query('COMMIT');
+		// 	dbNamespace.set('db', null);
+		// } catch (err) {
+		// 	await client.query('ROLLBACK');
+		// 	dbNamespace.set('db', null);
+		// 	throw err;
+		// }
 	};
+
+	function test(perform, callback) {
+		db.connect(function (err, client, done) {
+			if (err) {
+				return callback(err);
+			}
+
+			dbNamespace.run(function () {
+				dbNamespace.set('db', client);
+
+				client.query(`BEGIN`, function (err) {
+					if (err) {
+						done();
+						dbNamespace.set('db', null);
+						return callback(err);
+					}
+
+					perform(module, function (err) {
+						var args = Array.prototype.slice.call(arguments, 1);
+
+						client.query(err ? `ROLLBACK` : `COMMIT`, function (err1) {
+							done();
+							dbNamespace.set('db', null);
+							callback.apply(this, [err || err1].concat(args));
+						});
+					});
+				});
+			});
+		});
+	}
 };
