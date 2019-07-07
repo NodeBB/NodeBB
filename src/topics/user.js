97414e18e475d935f1bd9c1d7927e77aef80217b
Barış Soner Uşaklı <barisusakli@gmail.com>
2019-07-07 18:13:22 -0400

    feat: topics/user.js

--- src/topics/user.js
+++ src/topics/user.js
@@ -3,17 +3,16 @@
 var db = require('../database');
 
 module.exports = function (Topics) {
-	Topics.isOwner = function (tid, uid, callback) {
+	Topics.isOwner = async function (tid, uid) {
 		uid = parseInt(uid, 10);
 		if (uid <= 0) {
-			return setImmediate(callback, null, false);
+			return false;
 		}
-		Topics.getTopicField(tid, 'uid', function (err, author) {
-			callback(err, author === uid);
-		});
+		const author = await Topics.getTopicField(tid, 'uid');
+		return author === uid;
 	};
 
-	Topics.getUids = function (tid, callback) {
-		db.getSortedSetRevRangeByScore('tid:' + tid + ':posters', 0, -1, '+inf', 1, callback);
+	Topics.getUids = async function (tid) {
+		return await db.getSortedSetRevRangeByScore('tid:' + tid + ':posters', 0, -1, '+inf', 1);
 	};
 };
