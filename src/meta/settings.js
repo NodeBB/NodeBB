80d7074ec7337b835d5133f6dbe77af5e898852f
Andrew Rodrigues <psycho.bunny@hotmail.com>
2019-07-18 22:00:55 -0400

    feat: server-side support for sorted lists in settings obj + tests

--- src/meta/settings.js
+++ src/meta/settings.js
@@ -1,24 +1,81 @@
 'use strict';
 
-var db = require('../database');
-var plugins = require('../plugins');
-var Meta = require('../meta');
-var pubsub = require('../pubsub');
+const db = require('../database');
+const plugins = require('../plugins');
+const Meta = require('../meta');
+const pubsub = require('../pubsub');
 
-var Settings = module.exports;
+const Settings = module.exports;
 
 Settings.get = async function (hash) {
-	return await db.getObject('settings:' + hash) || {};
+	const data = await db.getObject('settings:' + hash) || {};
+	const sortedLists = await db.getSetMembers('settings:' + hash + ':sorted-lists');
+
+
+	await Promise.all(sortedLists.map(async function (list) {
+		const members = await db.getSortedSetRange('settings:' + hash + ':sorted-list:' + list, 0, -1) || [];
+		const keys = [];
+
+		data[list] = [];
+		for (const member of members) {
+			keys.push('settings:' + hash + ':sorted-list:' + list + ':' + member);
+		}
+
+		const objects = await db.getObjects(keys);
+		objects.forEach(function (obj) {
+			data[list].push(obj);
+		});
+	}));
+
+	return data;
 };
 
 Settings.getOne = async function (hash, field) {
-	return await db.getObjectField('settings:' + hash, field);
+	const data = await Settings.get(hash);
+	return data[field];
 };
 
 Settings.set = async function (hash, values, quiet) {
 	quiet = quiet || false;
 
-	await db.setObject('settings:' + hash, values);
+	const sortedLists = [];
+
+	for (const key in values) {
+		if (values.hasOwnProperty(key)) {
+			if (Array.isArray(values[key]) && typeof values[key][0] !== 'string') {
+				sortedLists.push(key);
+			}
+		}
+	}
+
+	if (sortedLists.length) {
+		await db.delete('settings:' + hash + ':sorted-lists');
+		await db.setAdd('settings:' + hash + ':sorted-lists', sortedLists);
+
+		await Promise.all(sortedLists.map(async function (list) {
+			await db.delete('settings:' + hash + ':sorted-list:' + list);
+			await Promise.all(values[list].map(async function (data, order) {
+				await db.delete('settings:' + hash + ':sorted-list:' + list + ':' + order);
+			}));
+		}));
+
+		const ops = [];
+		sortedLists.forEach(function (list) {
+			const arr = values[list];
+			delete values[list];
+
+			arr.forEach(function (data, order) {
+				ops.push(db.sortedSetAdd('settings:' + hash + ':sorted-list:' + list, order, order));
+				ops.push(db.setObject('settings:' + hash + ':sorted-list:' + list + ':' + order, data));
+			});
+		});
+
+		await Promise.all(ops);
+	}
+
+	if (Object.keys(values).length) {
+		await db.setObject('settings:' + hash, values);
+	}
 
 	plugins.fireHook('action:settings.set', {
 		plugin: hash,
@@ -32,12 +89,12 @@ Settings.set = async function (hash, values, quiet) {
 Settings.setOne = async function (hash, field, value) {
 	const data = {};
 	data[field] = value;
-	return await Settings.set(hash, data);
+	await Settings.set(hash, data);
 };
 
 Settings.setOnEmpty = async function (hash, values) {
-	const settings = await db.getObject('settings:' + hash) || {};
-	var empty = {};
+	const settings = await Settings.get(hash) || {};
+	const empty = {};
 
 	Object.keys(values).forEach(function (key) {
 		if (!settings.hasOwnProperty(key)) {
@@ -45,7 +102,8 @@ Settings.setOnEmpty = async function (hash, values) {
 		}
 	});
 
+
 	if (Object.keys(empty).length) {
-		return await Settings.set(hash, empty);
+		await Settings.set(hash, empty);
 	}
 };
