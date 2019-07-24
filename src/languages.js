c02686bfc80810af6baf8d52996b0558546a2730
Barış Soner Uşaklı <barisusakli@gmail.com>
2019-07-24 17:44:23 -0400

    feat: #7743, meta/languages and languages

--- src/languages.js
+++ src/languages.js
@@ -1,99 +1,73 @@
 'use strict';
 
-var fs = require('fs');
-var path = require('path');
-var async = require('async');
+const fs = require('fs');
+const path = require('path');
 
-var Languages = module.exports;
-var languagesPath = path.join(__dirname, '../build/public/language');
+const util = require('util');
+const readFileAsync = util.promisify(fs.readFile);
+
+const Languages = module.exports;
+const languagesPath = path.join(__dirname, '../build/public/language');
 
 const files = fs.readdirSync(path.join(__dirname, '../public/vendor/jquery/timeago/locales'));
 Languages.timeagoCodes = files.filter(f => f.startsWith('jquery.timeago')).map(f => f.split('.')[2]);
 
-Languages.get = function (language, namespace, callback) {
-	fs.readFile(path.join(languagesPath, language, namespace + '.json'), { encoding: 'utf-8' }, function (err, data) {
-		if (err) {
-			return callback(err);
-		}
-
-		try {
-			data = JSON.parse(data) || {};
-		} catch (e) {
-			return callback(e);
-		}
-
-		callback(null, data);
-	});
+Languages.get = async function (language, namespace) {
+	let data = await readFileAsync(path.join(languagesPath, language, namespace + '.json'), 'utf8');
+	try {
+		data = JSON.parse(data) || {};
+	} catch (err) {
+		throw err;
+	}
+	return data;
 };
 
-var codeCache = null;
-Languages.listCodes = function (callback) {
+let codeCache = null;
+Languages.listCodes = async function () {
 	if (codeCache && codeCache.length) {
-		return callback(null, codeCache);
+		return codeCache;
 	}
+	try {
+		const file = await readFileAsync(path.join(languagesPath, 'metadata.json'), 'utf8');
+		const parsed = JSON.parse(file);
 
-	fs.readFile(path.join(languagesPath, 'metadata.json'), 'utf8', function (err, file) {
-		if (err && err.code === 'ENOENT') {
-			return callback(null, []);
+		codeCache = parsed.languages;
+		return parsed.languages;
+	} catch (err) {
+		if (err.code === 'ENOENT') {
+			return [];
 		}
-		if (err) {
-			return callback(err);
-		}
-
-		var parsed;
-		try {
-			parsed = JSON.parse(file);
-		} catch (e) {
-			return callback(e);
-		}
-
-		var langs = parsed.languages;
-		codeCache = langs;
-		callback(null, langs);
-	});
+		throw err;
+	}
 };
 
-var listCache = null;
-Languages.list = function (callback) {
+let listCache = null;
+Languages.list = async function () {
 	if (listCache && listCache.length) {
-		return callback(null, listCache);
+		return listCache;
 	}
 
-	Languages.listCodes(function (err, codes) {
-		if (err) {
-			return callback(err);
-		}
+	const codes = await Languages.listCodes();
 
-		async.map(codes, function (folder, next) {
-			var configPath = path.join(languagesPath, folder, 'language.json');
-
-			fs.readFile(configPath, 'utf8', function (err, file) {
-				if (err && err.code === 'ENOENT') {
-					return next();
-				}
-				if (err) {
-					return next(err);
-				}
-				var lang;
-				try {
-					lang = JSON.parse(file);
-				} catch (e) {
-					return next(e);
-				}
-				next(null, lang);
-			});
-		}, function (err, languages) {
-			if (err) {
-				return callback(err);
+	let languages = await Promise.all(codes.map(async function (folder) {
+		try {
+			const configPath = path.join(languagesPath, folder, 'language.json');
+			const file = await readFileAsync(configPath, 'utf8');
+			const lang = JSON.parse(file);
+			return lang;
+		} catch (err) {
+			if (err.code === 'ENOENT') {
+				return;
 			}
+			throw err;
+		}
+	}));
 
-			// filter out invalid ones
-			languages = languages.filter(function (lang) {
-				return lang && lang.code && lang.name && lang.dir;
-			});
+	// filter out invalid ones
+	languages = languages.filter(lang => lang && lang.code && lang.name && lang.dir);
 
-			listCache = languages;
-			callback(null, languages);
-		});
-	});
+	listCache = languages;
+	return languages;
 };
+
+require('./promisify')(Languages);
