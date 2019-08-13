d2937f446a21131c070ae5d0ff33d67cfe465b8c
Barış Soner Uşaklı <barisusakli@gmail.com>
2019-08-13 10:36:48 -0400

    feat: async/await admin/controllers

--- src/controllers/admin/categories.js
+++ src/controllers/admin/categories.js
@@ -1,84 +1,66 @@
 'use strict';
 
-var async = require('async');
+const categories = require('../../categories');
+const analytics = require('../../analytics');
+const plugins = require('../../plugins');
+const translator = require('../../translator');
 
-var categories = require('../../categories');
-var analytics = require('../../analytics');
-var plugins = require('../../plugins');
-var translator = require('../../translator');
+const categoriesController = module.exports;
 
-var categoriesController = module.exports;
+categoriesController.get = async function (req, res, next) {
+	const [categoryData, parent, allCategories] = await Promise.all([
+		categories.getCategories([req.params.category_id], req.uid),
+		categories.getParents([req.params.category_id]),
+		getAllCategories(req.uid),
+	]);
 
-categoriesController.get = function (req, res, callback) {
-	async.waterfall([
-		function (next) {
-			async.parallel({
-				category: async.apply(categories.getCategories, [req.params.category_id], req.uid),
-				parent: async.apply(categories.getParents, [req.params.category_id]),
-				allCategories: function (next) {
-					async.waterfall([
-						function (next) {
-							categories.getAllCidsFromSet('categories:cid', next);
-						},
-						function (cids, next) {
-							categories.getCategories(cids, req.uid, next);
-						},
-						function (categoryData, next) {
-							categoryData = categories.getTree(categoryData);
-							categories.buildForSelectCategories(categoryData, next);
-						},
-					], next);
-				},
-			}, next);
-		},
-		function (data, next) {
-			var category = data.category[0];
+	const category = categoryData[0];
+	if (!category) {
+		return next();
+	}
 
-			if (!category) {
-				return callback();
-			}
-			category.parent = data.parent[0];
-			data.allCategories.forEach(function (category) {
-				if (category) {
-					category.selected = parseInt(category.cid, 10) === parseInt(req.params.category_id, 10);
-				}
-			});
+	category.parent = parent[0];
+	allCategories.forEach(function (category) {
+		if (category) {
+			category.selected = parseInt(category.cid, 10) === parseInt(req.params.category_id, 10);
+		}
+	});
 
-			plugins.fireHook('filter:admin.category.get', {
-				req: req,
-				res: res,
-				category: category,
-				customClasses: [],
-				allCategories: data.allCategories,
-			}, next);
-		},
-		function (data) {
-			data.category.name = translator.escape(String(data.category.name));
+	const data = await plugins.fireHook('filter:admin.category.get', {
+		req: req,
+		res: res,
+		category: category,
+		customClasses: [],
+		allCategories: allCategories,
+	});
+	data.category.name = translator.escape(String(data.category.name));
 
-			res.render('admin/manage/category', {
-				category: data.category,
-				allCategories: data.allCategories,
-				customClasses: data.customClasses,
-			});
-		},
-	], callback);
+	res.render('admin/manage/category', {
+		category: data.category,
+		allCategories: data.allCategories,
+		customClasses: data.customClasses,
+	});
 };
+
+async function getAllCategories(uid) {
+	const cids = await categories.getAllCidsFromSet('categories:cid');
+	const categoryData = await categories.getCategories(cids, uid);
+	const tree = categories.getTree(categoryData);
+	return await categories.buildForSelectCategories(tree);
+}
 
 categoriesController.getAll = function (req, res) {
 	// Categories list will be rendered on client side with recursion, etc.
 	res.render('admin/manage/categories', {});
 };
 
-categoriesController.getAnalytics = function (req, res, next) {
-	async.waterfall([
-		function (next) {
-			async.parallel({
-				name: async.apply(categories.getCategoryField, req.params.category_id, 'name'),
-				analytics: async.apply(analytics.getCategoryAnalytics, req.params.category_id),
-			}, next);
-		},
-		function (data) {
-			res.render('admin/manage/category-analytics', data);
-		},
-	], next);
+categoriesController.getAnalytics = async function (req, res) {
+	const [name, analyticsData] = await Promise.all([
+		categories.getCategoryField(req.params.category_id, 'name'),
+		analytics.getCategoryAnalytics(req.params.category_id),
+	]);
+	res.render('admin/manage/category-analytics', {
+		name: name,
+		analytics: analyticsData,
+	});
 };
