80bd52fcfae3b86160b6d8bb06d1580d0ea55eda
Barış Soner Uşaklı <barisusakli@gmail.com>
2019-08-20 20:05:07 -0400

    feat: add image and imageClass to post summaries

--- src/posts/summary.js
+++ src/posts/summary.js
@@ -76,7 +76,7 @@ module.exports = function (Posts) {
 	async function getTopicAndCategories(tids) {
 		const topicsData = await topics.getTopicsFields(tids, ['uid', 'tid', 'title', 'cid', 'slug', 'deleted', 'postcount', 'mainPid']);
 		const cids = _.uniq(topicsData.map(topic => topic && topic.cid));
-		const categoriesData = await categories.getCategoriesFields(cids, ['cid', 'name', 'icon', 'slug', 'parentCid', 'bgColor', 'color']);
+		const categoriesData = await categories.getCategoriesFields(cids, ['cid', 'name', 'icon', 'slug', 'parentCid', 'bgColor', 'color', 'image', 'imageClass']);
 		return { topics: topicsData, categories: categoriesData };
 	}
 
