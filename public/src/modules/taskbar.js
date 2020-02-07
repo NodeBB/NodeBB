2e794801799b93f13abf2556706e96f8a371ee47
Baris Usakli <barisusakli@gmail.com>
2020-02-07 12:04:37 -0500

    fix: #8151, don't crash if taskbar doesn't have element

--- public/src/modules/taskbar.js
+++ public/src/modules/taskbar.js
@@ -200,6 +200,9 @@ define('taskbar', ['benchpress', 'translator'], function (Benchpress, translator
 
 	taskbar.update = function (module, uuid, options) {
 		var element = taskbar.tasklist.find('[data-module="' + module + '"][data-uuid="' + uuid + '"]');
+		if (!element.length) {
+			return;
+		}
 		var data = element.data();
 
 		Object.keys(options).forEach(function (key) {
