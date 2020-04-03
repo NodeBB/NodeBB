1136a369f3ba938358dd6182e71b0d8c1cb05a1f
Julian Lam <julian@nodebb.org>
2020-04-03 14:10:13 -0400

    feat: local redoc view on development mode only

--- src/routes/debug.js
+++ src/routes/debug.js
@@ -3,11 +3,26 @@
 var express = require('express');
 var nconf = require('nconf');
 
+const fs = require('fs').promises;
+const path = require('path');
+
 module.exports = function (app) {
 	var router = express.Router();
 
 	router.get('/test', function (req, res) {
 		res.redirect(404);
+	});
+
+	// Redoc
+	router.get('/spec/read', async (req, res) => {
+		const handle = await fs.open(path.resolve(__dirname, '../../public/vendor/redoc/index.html'), 'r');
+		let html = await handle.readFile({
+			encoding: 'utf-8',
+		});
+		await handle.close();
+
+		html = html.replace('apiUrl', nconf.get('relative_path') + '/assets/openapi/read.yaml');
+		res.status(200).type('text/html').send(html);
 	});
 
 	app.use(nconf.get('relative_path') + '/debug', router);
