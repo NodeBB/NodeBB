883a4aea37170859f4d7c2dab641409d1a6f9b9a
Barış Soner Uşaklı <barisusakli@gmail.com>
2020-05-28 17:44:34 -0400

    feat: wrap middlewares

--- src/routes/helpers.js
+++ src/routes/helpers.js
@@ -4,14 +4,21 @@ var helpers = module.exports;
 
 helpers.setupPageRoute = function (router, name, middleware, middlewares, controller) {
 	middlewares = [middleware.maintenanceMode, middleware.registrationComplete, middleware.pageView, middleware.pluginHooks].concat(middlewares);
+	middlewares = middlewares.map(m => helpers.tryRoute(m));
 
-	router.get(name, middleware.busyCheck, middleware.buildHeader, middlewares, helpers.tryRoute(controller));
+	router.get(name, helpers.tryRoute(middleware.busyCheck), helpers.tryRoute(middleware.buildHeader), middlewares, helpers.tryRoute(controller));
 	router.get('/api' + name, middlewares, helpers.tryRoute(controller));
 };
 
 helpers.setupAdminPageRoute = function (router, name, middleware, middlewares, controller) {
-	router.get(name, middleware.admin.buildHeader, middlewares, helpers.tryRoute(controller));
+	middlewares = middlewares.map(m => helpers.tryRoute(m));
+	router.get(name, helpers.tryRoute(middleware.admin.buildHeader), middlewares, helpers.tryRoute(controller));
 	router.get('/api' + name, middlewares, helpers.tryRoute(controller));
+};
+
+helpers.setupAPIRoute = function (router, name, middlewares, controller) {
+	middlewares = middlewares.map(m => helpers.tryRoute(m));
+	router.get(name, middlewares, helpers.tryRoute(controller));
 };
 
 helpers.tryRoute = function (controller) {
