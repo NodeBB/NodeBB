5e984d10f00600429d2e15daec1abcd498aa386e
Barış Soner Uşaklı <barisusakli@gmail.com>
2020-07-13 16:02:32 -0400

    fix: #8500

--- public/src/client/unread.js
+++ public/src/client/unread.js
@@ -14,7 +14,6 @@ define('forum/unread', ['topicSelect', 'components', 'topicList'], function (top
 		app.enterRoom('unread_topics');
 
 		topicList.init('unread');
-		topicSelect.init();
 
 		updateUnreadTopicCount('/' + ajaxify.data.selectedFilter.url, ajaxify.data.topicCount);
 
