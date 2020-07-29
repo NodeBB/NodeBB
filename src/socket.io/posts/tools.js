c705e08a76056fe3ce91a6adf5ec606b6396af11
Julian Lam <julian@nodebb.org>
2020-07-29 16:55:14 -0400

    feat: allow direct link to flag from post tools, #8531

--- src/socket.io/posts/tools.js
+++ src/socket.io/posts/tools.js
@@ -19,7 +19,7 @@ module.exports = function (SocketPosts) {
 		}
 
 		const results = await utils.promiseParallel({
-			posts: posts.getPostFields(data.pid, ['deleted', 'bookmarks', 'uid', 'ip']),
+			posts: posts.getPostFields(data.pid, ['deleted', 'bookmarks', 'uid', 'ip', 'flagId']),
 			isAdmin: user.isAdministrator(socket.uid),
 			isGlobalMod: user.isGlobalModerator(socket.uid),
 			isModerator: user.isModerator(socket.uid, data.cid),
