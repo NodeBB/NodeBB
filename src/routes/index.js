762b0be4b4744abef16873abd08cca2133bb159b
Barış Soner Uşaklı <barisusakli@gmail.com>
2020-07-29 08:20:41 -0400

    feat: remove /assets/stylesheet.css

--- src/routes/index.js
+++ src/routes/index.js
@@ -167,19 +167,6 @@ function addCoreRoutes(app, router, middleware) {
 		app.use(relativePath + '/assets/client-' + skin + '.css', middleware.buildSkinAsset);
 	});
 
-	// only warn once
-	var warned = new Set();
-
-	// DEPRECATED (v1.12.0)
-	app.use(relativePath + '/assets/stylesheet.css', function (req, res) {
-		if (!warned.has(req.path)) {
-			winston.warn('[deprecated] Accessing `/assets/stylesheet.css` is deprecated to be REMOVED in NodeBB v1.12.0. ' +
-			'Use `/assets/client.css` to access this file');
-			warned.add(req.path);
-		}
-		res.redirect(relativePath + '/assets/client.css?' + meta.config['cache-buster']);
-	});
-
 	app.use(controllers['404'].handle404);
 	app.use(controllers.errors.handleURIErrors);
 	app.use(controllers.errors.handleErrors);
