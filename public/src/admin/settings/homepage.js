8a144e2b227ba64c6c1296b7453fa298273a4b85
Peter Jaszkowiak <p.jaszkow@gmail.com>
2020-09-03 11:44:41 -0600

    fix(bug): #8611, custom route logic corrected

--- public/src/admin/settings/homepage.js
+++ public/src/admin/settings/homepage.js
@@ -3,10 +3,10 @@
 
 define('admin/settings/homepage', ['admin/settings'], function () {
 	function toggleCustomRoute() {
-		if ($('[data-field="homePageRoute"]').val()) {
-			$('#homePageCustom').hide();
-		} else {
+		if ($('[data-field="homePageRoute"]').val() === 'custom') {
 			$('#homePageCustom').show();
+		} else {
+			$('#homePageCustom').hide();
 		}
 	}
 
