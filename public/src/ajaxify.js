3cd3b7a1723bdec27ac6a6c6150b27c42c136864
Barış Soner Uşaklı <barisusakli@gmail.com>
2020-09-06 22:07:39 -0400

    feat: remove app.template

--- public/src/ajaxify.js
+++ public/src/ajaxify.js
@@ -76,7 +76,6 @@ ajaxify = window.ajaxify || {};
 			}
 
 			retry = true;
-			app.template = data.template.name;
 
 			renderTemplate(url, data.templateToRender || data.template.name, data, callback);
 		});
@@ -88,7 +87,7 @@ ajaxify = window.ajaxify || {};
 	ajaxify.coldLoad = function () {
 		var url = ajaxify.start(window.location.pathname.slice(1) + window.location.search + window.location.hash);
 		ajaxify.updateHistory(url, true);
-		ajaxify.end(url, app.template);
+		ajaxify.end(url, ajaxify.data.template.name);
 		$(window).trigger('action:ajaxify.coldLoad');
 	};
 
