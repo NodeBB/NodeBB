61a8c6f03738969e26925d416d0064805b954543
Julian Lam <julian@nodebb.org>
2020-09-08 11:38:10 -0400

    chore: update commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,25 @@
 'use strict';
 
-module.exports = { extends: ['@commitlint/config-angular'] };
+module.exports = {
+	extends: ['@commitlint/config-angular'],
+	rules: {
+		'type-enum': [
+			2,
+			'always',
+			[
+				'breaking',
+				'build',
+				'chore',
+				'ci',
+				'docs',
+				'feat',
+				'fix',
+				'perf',
+				'refactor',
+				'revert',
+				'style',
+				'test',
+			],
+		],
+	},
+};
