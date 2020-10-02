a905d7f0224c76c5d2c3e4eed0490d0ef95be26a
Julian Lam <julian@nodebb.org>
2020-10-02 10:53:18 -0400

    refactor: rewrite modules/api.js in AMD style

--- public/src/modules/api.js
+++ public/src/modules/api.js
@@ -1,45 +1,44 @@
-const baseUrl = config.relative_path + '/api/v1';
+'use strict';
 
-function call(options, onSuccess, onError) {
-	$.ajax(options)
-		.done((res) => {
-			if (onSuccess) {
-				onSuccess(res.response);
-			}
-		})
-		.fail((ev) => {
-			if (onError) {
-				onError(ev.responseJSON);
-			}
-		});
-}
+define('api', () => {
+	const api = {};
+	const baseUrl = config.relative_path + '/api/v1';
 
-export function get(route, payload, onSuccess, onError) {
-	return call({
+	function call(options, onSuccess, onError) {
+		$.ajax(options)
+			.done((res) => {
+				if (onSuccess) {
+					onSuccess(res.response);
+				}
+			})
+			.fail((ev) => {
+				if (onError) {
+					onError(ev.responseJSON);
+				}
+			});
+	}
+
+	api.get = (route, payload, onSuccess, onError) => call({
 		url: baseUrl + route + '?' + $.param(payload),
 	}, onSuccess, onError);
-}
 
-export function post(route, payload, onSuccess, onError) {
-	return call({
+	api.post = (route, payload, onSuccess, onError) => call({
 		url: baseUrl + route,
 		method: 'post',
 		data: payload,
 	}, onSuccess, onError);
-}
 
-export function put(route, payload, onSuccess, onError) {
-	return call({
+	api.put = (route, payload, onSuccess, onError) => call({
 		url: baseUrl + route,
 		method: 'put',
 		data: payload,
 	}, onSuccess, onError);
-}
 
-export function del(route, payload, onSuccess, onError) {
-	return call({
+	api.del = (route, payload, onSuccess, onError) => call({
 		url: baseUrl + route,
 		method: 'delete',
 		data: payload,
 	}, onSuccess, onError);
-}
+
+	return api;
+});
