6a0b08ac27d48f5364344d02628b8b99b8798254
Julian Lam <julian@nodebb.org>
2020-10-07 11:42:59 -0400

    refactor: pull out sorted list getter and setter into separate methods

--- src/meta/settings.js
+++ src/meta/settings.js
@@ -8,23 +8,8 @@ const pubsub = require('../pubsub');
 const Settings = module.exports;
 
 Settings.get = async function (hash) {
-	const data = await db.getObject('settings:' + hash) || {};
-	const sortedLists = await db.getSetMembers('settings:' + hash + ':sorted-lists');
-
-	await Promise.all(sortedLists.map(async function (list) {
-		const members = await db.getSortedSetRange('settings:' + hash + ':sorted-list:' + list, 0, -1) || [];
-		const keys = [];
-
-		data[list] = [];
-		for (const order of members) {
-			keys.push('settings:' + hash + ':sorted-list:' + list + ':' + order);
-		}
-
-		const objects = await db.getObjects(keys);
-		objects.forEach(function (obj) {
-			data[list].push(obj);
-		});
-	}));
+	let data = await db.getObject('settings:' + hash) || {};
+	data = { ...data, ...await Settings.sortedLists.get(hash) };
 
 	return data;
 };
@@ -48,28 +33,12 @@ Settings.set = async function (hash, values, quiet) {
 	}
 
 	if (sortedLists.length) {
-		await db.delete('settings:' + hash + ':sorted-lists');
-		await db.setAdd('settings:' + hash + ':sorted-lists', sortedLists);
-
-		await Promise.all(sortedLists.map(async function (list) {
-			await db.delete('settings:' + hash + ':sorted-list:' + list);
-			await Promise.all(values[list].map(async function (data, order) {
-				await db.delete('settings:' + hash + ':sorted-list:' + list + ':' + order);
-			}));
-		}));
-
-		const ops = [];
-		sortedLists.forEach(function (list) {
-			const arr = values[list];
-			delete values[list];
-
-			arr.forEach(function (data, order) {
-				ops.push(db.sortedSetAdd('settings:' + hash + ':sorted-list:' + list, order, order));
-				ops.push(db.setObject('settings:' + hash + ':sorted-list:' + list + ':' + order, data));
-			});
-		});
-
-		await Promise.all(ops);
+		const sortedListValues = sortedLists.reduce((memo, cur) => {
+			memo[cur] = values[cur];
+			delete values[cur];
+			return memo;
+		}, {});
+		Settings.sortedLists.set(hash, sortedLists, sortedListValues);
 	}
 
 	if (Object.keys(values).length) {
@@ -105,4 +74,52 @@ Settings.setOnEmpty = async function (hash, values) {
 	if (Object.keys(empty).length) {
 		await Settings.set(hash, empty);
 	}
+};
+
+Settings.sortedLists = {};
+
+Settings.sortedLists.get = async (hash) => {
+	const data = {};
+	const sortedLists = await db.getSetMembers('settings:' + hash + ':sorted-lists');
+
+	await Promise.all(sortedLists.map(async function (list) {
+		const members = await db.getSortedSetRange('settings:' + hash + ':sorted-list:' + list, 0, -1) || [];
+		const keys = [];
+
+		data[list] = [];
+		for (const order of members) {
+			keys.push('settings:' + hash + ':sorted-list:' + list + ':' + order);
+		}
+
+		const objects = await db.getObjects(keys);
+		objects.forEach(function (obj) {
+			data[list].push(obj);
+		});
+	}));
+
+	return data;
+};
+
+Settings.sortedLists.set = async (hash, sortedLists, values) => {
+	await db.delete('settings:' + hash + ':sorted-lists');
+	await db.setAdd('settings:' + hash + ':sorted-lists', sortedLists);
+
+	await Promise.all(sortedLists.map(async function (list) {
+		await db.delete('settings:' + hash + ':sorted-list:' + list);
+		await Promise.all(values[list].map(async function (data, order) {
+			await db.delete('settings:' + hash + ':sorted-list:' + list + ':' + order);
+		}));
+	}));
+
+	const ops = [];
+	sortedLists.forEach(function (list) {
+		const arr = values[list];
+
+		arr.forEach(function (data, order) {
+			ops.push(db.sortedSetAdd('settings:' + hash + ':sorted-list:' + list, order, order));
+			ops.push(db.setObject('settings:' + hash + ':sorted-list:' + list + ':' + order, data));
+		});
+	});
+
+	await Promise.all(ops);
 };
