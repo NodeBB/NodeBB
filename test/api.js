7b2301ff9cd9fd101be97c088b3044c51a6b9078
psychobunny <psycho.bunny@hotmail.com>
2020-10-13 13:10:49 -0400

    fix: enable tests

--- test/api.js
+++ test/api.js
@@ -236,18 +236,15 @@ describe('Read API', async () => {
 });
 
 describe('Write API', async () => {
+	const apiPath = path.resolve(__dirname, '../public/openapi/write.yaml');
 
-	// let writeApi;
-	// const apiPath = path.resolve(__dirname, '../public/openapi/write.yaml');
+	it('should pass OpenAPI v3 validation', async () => {
+		try {
+			await SwaggerParser.validate(apiPath);
+		} catch (e) {
+			assert.ifError(e);
+		}
+	});
 
-	// it('should pass OpenAPI v3 validation', async () => {
-	// 	try {
-	// 		await SwaggerParser.validate(apiPath);
-	// 	} catch (e) {
-	// 		assert.ifError(e);
-	// 	}
-	// });
-
-	// writeApi = await SwaggerParser.dereference(apiPath);
-	// console.log(writeApi);
+	console.log(await SwaggerParser.dereference(apiPath));
 });
