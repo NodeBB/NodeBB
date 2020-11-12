584110d076a41be11a76e699844d8dba21eee3a6
Barış Soner Uşaklı <barisusakli@gmail.com>
2020-11-12 14:38:04 -0500

    fix: typo

--- test/socket.io.js
+++ test/socket.io.js
@@ -395,7 +395,7 @@ describe('socket.io', function () {
 				setTimeout(function () {
 					socketAdmin.rooms.getAll({ uid: 10 }, {}, function (err, data) {
 						assert.ifError(err);
-						assert.equal(data.users.category, 1, JSON.strigify(data, null, 4));
+						assert.equal(data.users.category, 1, JSON.stringify(data, null, 4));
 						done();
 					});
 				}, 1000);
