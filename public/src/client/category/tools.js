9b4e3e40b5281d0d281957fe833ce29dde42d922
Julian Lam <julian@nodebb.org>
2020-11-19 11:28:43 -0500

    refactor: use categoryCommand local method for main actions in category tools

--- public/src/client/category/tools.js
+++ public/src/client/category/tools.js
@@ -6,7 +6,9 @@ define('forum/category/tools', [
 	'topicSelect',
 	'components',
 	'translator',
-], function (topicSelect, components, translator) {
+	'api',
+	'bootbox',
+], function (topicSelect, components, translator, api, bootbox) {
 	var CategoryTools = {};
 
 	CategoryTools.init = function () {
@@ -15,53 +17,37 @@ define('forum/category/tools', [
 		handlePinnedTopicSort();
 
 		components.get('topic/delete').on('click', function () {
-			categoryCommand('delete', topicSelect.getSelectedTids());
+			categoryCommand('del', '/state', 'delete', true, onDeletePurgeComplete);
 			return false;
 		});
 
 		components.get('topic/restore').on('click', function () {
-			categoryCommand('restore', topicSelect.getSelectedTids());
+			categoryCommand('put', '/state', 'restore', true, onDeletePurgeComplete);
 			return false;
 		});
 
 		components.get('topic/purge').on('click', function () {
-			categoryCommand('purge', topicSelect.getSelectedTids());
+			categoryCommand('del', '', 'purge', true, onDeletePurgeComplete);
 			return false;
 		});
 
 		components.get('topic/lock').on('click', function () {
-			var tids = topicSelect.getSelectedTids();
-			if (!tids.length) {
-				return app.alertError('[[error:no-topics-selected]]');
-			}
-			socket.emit('topics.lock', { tids: tids }, onCommandComplete);
+			categoryCommand('put', '/lock', 'lock', false, onCommandComplete);
 			return false;
 		});
 
 		components.get('topic/unlock').on('click', function () {
-			var tids = topicSelect.getSelectedTids();
-			if (!tids.length) {
-				return app.alertError('[[error:no-topics-selected]]');
-			}
-			socket.emit('topics.unlock', { tids: tids }, onCommandComplete);
+			categoryCommand('del', '/lock', 'lock', false, onCommandComplete);
 			return false;
 		});
 
 		components.get('topic/pin').on('click', function () {
-			var tids = topicSelect.getSelectedTids();
-			if (!tids.length) {
-				return app.alertError('[[error:no-topics-selected]]');
-			}
-			socket.emit('topics.pin', { tids: tids }, onCommandComplete);
+			categoryCommand('put', '/pin', 'pin', false, onCommandComplete);
 			return false;
 		});
 
 		components.get('topic/unpin').on('click', function () {
-			var tids = topicSelect.getSelectedTids();
-			if (!tids.length) {
-				return app.alertError('[[error:no-topics-selected]]');
-			}
-			socket.emit('topics.unpin', { tids: tids }, onCommandComplete);
+			categoryCommand('del', '/pin', 'pin', false, onCommandComplete);
 			return false;
 		});
 
@@ -136,20 +122,30 @@ define('forum/category/tools', [
 		socket.on('event:topic_moved', onTopicMoved);
 	};
 
-	function categoryCommand(command, tids) {
+	function categoryCommand(method, path, command, confirm, onComplete) {
+		if (!onComplete) {
+			onComplete = function () {};
+		}
+		const tids = topicSelect.getSelectedTids();
+		const execute = function (ok) {
+			if (ok) {
+				Promise.all(tids.map(tid => api[method](`/topics/${tid}${path}`)))
+					.then(onComplete)
+					.catch(app.alertError);
+			}
+		};
+
 		if (!tids.length) {
 			return app.alertError('[[error:no-topics-selected]]');
 		}
 
-		translator.translate('[[topic:thread_tools.' + command + '_confirm]]', function (msg) {
-			bootbox.confirm(msg, function (confirm) {
-				if (!confirm) {
-					return;
-				}
-
-				socket.emit('topics.' + command, { tids: tids }, onDeletePurgeComplete);
+		if (confirm) {
+			translator.translate('[[topic:thread_tools.' + command + '_confirm]]', function (msg) {
+				bootbox.confirm(msg, execute);
 			});
-		});
+		} else {
+			execute(true);
+		}
 	}
 
 	CategoryTools.removeListeners = function () {
@@ -167,18 +163,12 @@ define('forum/category/tools', [
 		$('.thread-tools.open').find('.dropdown-toggle').trigger('click');
 	}
 
-	function onCommandComplete(err) {
-		if (err) {
-			return app.alertError(err.message);
-		}
+	function onCommandComplete() {
 		closeDropDown();
 		topicSelect.unselectAll();
 	}
 
-	function onDeletePurgeComplete(err) {
-		if (err) {
-			return app.alertError(err.message);
-		}
+	function onDeletePurgeComplete() {
 		closeDropDown();
 		updateDropdownOptions();
 	}
