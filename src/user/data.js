1be08b2e8b9de82ba0eae4361e4122f145f34ec2
Barış Soner Uşaklı <barisusakli@gmail.com>
2020-11-19 22:08:03 -0500

    fix: guest displayname

--- src/user/data.js
+++ src/user/data.js
@@ -173,6 +173,7 @@ module.exports = function (User) {
 			if (!parseInt(user.uid, 10)) {
 				user.uid = 0;
 				user.username = (user.hasOwnProperty('oldUid') && parseInt(user.oldUid, 10)) ? '[[global:former_user]]' : '[[global:guest]]';
+				user.displayname = user.username;
 				user.userslug = '';
 				user.picture = User.getDefaultAvatar();
 				user['icon:text'] = '?';
