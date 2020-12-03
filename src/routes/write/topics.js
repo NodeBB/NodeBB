b5d910f53b32fb2626faa2cb162c2d24c3ffee6f
Julian Lam <julian@nodebb.org>
2020-12-03 16:48:57 -0500

    feat: core work for #9042, thumb deletion now accepts uuids
    + common data validation for thumb addition and deletion

--- src/routes/write/topics.js
+++ src/routes/write/topics.js
@@ -36,7 +36,7 @@ module.exports = function () {
 
 	setupApiRoute(router, 'get', '/:tid/thumbs', [], controllers.write.topics.getThumbs);
 	setupApiRoute(router, 'post', '/:tid/thumbs', [multipartMiddleware, middleware.validateFiles, ...middlewares], controllers.write.topics.addThumb);
-	setupApiRoute(router, 'delete', '/:tid/thumbs', [...middlewares, middleware.assert.topic, middleware.assert.path], controllers.write.topics.deleteThumb);
+	setupApiRoute(router, 'delete', '/:tid/thumbs', [...middlewares, middleware.assert.path], controllers.write.topics.deleteThumb);
 
 	return router;
 };
