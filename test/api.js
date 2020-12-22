501a7b777467e9ad8b5790f1bc5e3c43a3575c1e
Julian Lam <julian@nodebb.org>
2020-12-22 10:26:02 -0500

    fix: bug in api path existence test
    It was only checking for the path, but not ensuring that the method was also defined

--- test/api.js
+++ test/api.js
@@ -235,7 +235,8 @@ describe('API', async () => {
 					}
 
 					const normalizedPath = pathObj.path.replace(/\/:([^\\/]+)/g, '/{$1}').replace(/\?/g, '');
-					assert(schema.paths.hasOwnProperty(normalizedPath));
+					assert(schema.paths.hasOwnProperty(normalizedPath), `${pathObj.path} is not defined in schema docs`);
+					assert(schema.paths[normalizedPath].hasOwnProperty(pathObj.method), `${pathObj.path} was found in schema docs, but ${pathObj.method.toUpperCase()} method is not defined`);
 				});
 			});
 		});
