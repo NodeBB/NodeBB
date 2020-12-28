20c1b684ed9a4522d6c5ee69a0151be5b68c947f
Barış Soner Uşaklı <barisusakli@gmail.com>
2020-12-27 21:18:41 -0500

    fix: #9151, dont use service worker for posts requests

--- public/src/service-worker.js
+++ public/src/service-worker.js
@@ -1,6 +1,14 @@
 'use strict';
 
 self.addEventListener('fetch', function (event) {
+	// This is the code that ignores post requests
+	// https://github.com/NodeBB/NodeBB/issues/9151
+	// https://github.com/w3c/ServiceWorker/issues/1141
+	// https://stackoverflow.com/questions/54448367/ajax-xmlhttprequest-progress-monitoring-doesnt-work-with-service-workers
+	if (event.request.method === 'POST') {
+		return;
+	}
+
 	event.respondWith(caches.match(event.request).then(function (response) {
 		if (!response) {
 			return fetch(event.request);
