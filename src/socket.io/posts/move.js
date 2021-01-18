d85ad10d345ff4d2236aaff9c8aa111209409a4f
Julian Lam <julian@nodebb.org>
2021-01-18 15:47:15 -0500

    fix: restored sanity checks for post move socket calls

--- src/socket.io/posts/move.js
+++ src/socket.io/posts/move.js
@@ -4,13 +4,27 @@ const api = require('../../api');
 const sockets = require('..');
 
 module.exports = function (SocketPosts) {
+	function moveChecks(socket, typeCheck, data) {
+		if (!socket.uid) {
+			throw new Error('[[error:not-logged-in]]');
+		}
+
+		if (!data || !typeCheck || !data.tid) {
+			throw new Error('[[error:invalid-data]]');
+		}
+	}
+
 	SocketPosts.movePost = async function (socket, data) {
 		sockets.warnDeprecated(socket, 'PUT /api/v3/posts/:pid/move');
+
+		moveChecks(socket, isFinite(data.pid), data);
 		await api.posts.move(socket, data);
 	};
 
 	SocketPosts.movePosts = async function (socket, data) {
 		sockets.warnDeprecated(socket, 'PUT /api/v3/posts/:pid/move');
+
+		moveChecks(socket, !Array.isArray(data.pids), data);
 		await Promise.all(data.pids.map(async pid => api.posts.move(socket, {
 			tid: data.tid,
 			pid,
