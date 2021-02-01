d74f74e55cda6250a2d7a34b436e59bfdd42beec
Barış Soner Uşaklı <barisusakli@gmail.com>
2021-02-01 14:21:46 -0500

    fix: tests

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const nconf = require('nconf');
+const _ = require('lodash');
 
 const categories = require('../categories');
 const meta = require('../meta');
@@ -27,7 +28,7 @@ categoriesController.list = async function (req, res) {
 	const stop = start + meta.config.categoriesPerPage - 1;
 	const pageCids = rootCids.slice(start, stop + 1);
 
-	const allChildCids = (await Promise.all(pageCids.map(cid => categories.getChildrenCids(cid)))).flat();
+	const allChildCids = _.flatten(await Promise.all(pageCids.map(cid => categories.getChildrenCids(cid))));
 	const childCids = await privileges.categories.filterCids('find', allChildCids, req.uid);
 	const categoryData = await categories.getCategories(pageCids.concat(childCids), req.uid);
 	const tree = categories.getTree(categoryData, 0);
