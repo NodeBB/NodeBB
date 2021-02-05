8167db94dfeb851ca5571566e9922e844932eef6
Barış Soner Uşaklı <barisusakli@gmail.com>
2021-02-04 22:11:45 -0500

    feat: add pagination to /user/:userslug/categories

--- src/controllers/accounts/categories.js
+++ src/controllers/accounts/categories.js
@@ -4,6 +4,8 @@ const user = require('../../user');
 const categories = require('../../categories');
 const accountHelpers = require('./helpers');
 const helpers = require('../helpers');
+const pagination = require('../../pagination');
+const meta = require('../../meta');
 
 const categoriesController = module.exports;
 
@@ -12,10 +14,17 @@ categoriesController.get = async function (req, res, next) {
 	if (!userData) {
 		return next();
 	}
-	const [states, categoriesData] = await Promise.all([
+	const [states, allCategoriesData] = await Promise.all([
 		user.getCategoryWatchState(userData.uid),
 		categories.buildForSelect(userData.uid, 'find', ['descriptionParsed', 'depth', 'slug']),
 	]);
+
+	const pageCount = Math.max(1, Math.ceil(allCategoriesData.length / meta.config.categoriesPerPage));
+	const page = Math.min(parseInt(req.query.page, 10) || 1, pageCount);
+	const start = Math.max(0, (page - 1) * meta.config.categoriesPerPage);
+	const stop = start + meta.config.categoriesPerPage - 1;
+	const categoriesData = allCategoriesData.slice(start, stop + 1);
+
 
 	categoriesData.forEach(function (category) {
 		if (category) {
@@ -30,5 +39,6 @@ categoriesController.get = async function (req, res, next) {
 		{ text: userData.username, url: '/user/' + userData.userslug },
 		{ text: '[[pages:categories]]' },
 	]);
+	userData.pagination = pagination.create(page, pageCount, req.query);
 	res.render('account/categories', userData);
 };
