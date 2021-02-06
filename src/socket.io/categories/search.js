f8e189d734a63049954c6edfb7b3775666e8e484
Barış Soner Uşaklı <barisusakli@gmail.com>
2021-02-06 17:15:34 -0500

    fix: tests and search filter

--- src/socket.io/categories/search.js
+++ src/socket.io/categories/search.js
@@ -13,18 +13,18 @@ module.exports = function (SocketCategories) {
 		let cids = [];
 		let matchedCids = [];
 		const privilege = data.privilege || 'topics:read';
+		data.states = (data.states || ['watching', 'notwatching', 'ignoring']).map(
+			state => categories.watchStates[state]
+		);
+
 		if (data.query) {
-			({ cids, matchedCids } = await findMatchedCids(data));
+			({ cids, matchedCids } = await findMatchedCids(socket.uid, data));
 		} else {
 			cids = await loadCids(socket.uid, data.parentCid);
 		}
 
-		const states = (data.states || ['watching', 'notwatching', 'ignoring']).map(
-			state => categories.watchStates[state]
-		);
-
 		const visibleCategories = await controllersHelpers.getVisibleCategories({
-			cids, uid: socket.uid, states, privilege, showLinks: data.showLinks, parentCid: data.parentCid,
+			cids, uid: socket.uid, states: data.states, privilege, showLinks: data.showLinks, parentCid: data.parentCid,
 		});
 
 		if (Array.isArray(data.selectedCids)) {
@@ -43,7 +43,7 @@ module.exports = function (SocketCategories) {
 		return categoriesData;
 	};
 
-	async function findMatchedCids(data) {
+	async function findMatchedCids(uid, data) {
 		const result = await categories.search({
 			query: data.query,
 			paginate: false,
@@ -61,7 +61,16 @@ module.exports = function (SocketCategories) {
 			return toParent;
 		}
 
-		const matchedCids = result.categories.map(c => c.cid);
+		let matchedCids = result.categories.map(c => c.cid);
+		// no need to filter if all 3 states are used
+		const filterByWatchState = !Object.values(categories.watchStates)
+			.every(state => data.states.includes(state));
+
+		if (filterByWatchState) {
+			const states = await categories.getWatchState(matchedCids, uid);
+			matchedCids = matchedCids.filter((cid, index) => data.states.includes(states[index]));
+		}
+
 		const rootCids = _.uniq(_.flatten(await Promise.all(matchedCids.map(addParentsToRoot))));
 		const allChildCids = _.uniq(_.flatten(await Promise.all(matchedCids.map(cid => categories.getChildrenCids(cid)))));
 
