5d21be5674335b4053d9ec1d8c15568cff93facc
Barış Soner Uşaklı <barisusakli@gmail.com>
2021-02-14 10:26:09 -0500

    feat: mongo/psql

--- src/database/mongo/list.js
+++ src/database/mongo/list.js
@@ -7,12 +7,23 @@ module.exports = function (module) {
 		if (!key) {
 			return;
 		}
-
-		value = helpers.valueToString(value);
-
+		value = Array.isArray(value) ? value : [value];
+		value = value.map(helpers.valueToString);
+		value.reverse();
 		const exists = await module.isObjectField(key, 'array');
 		if (exists) {
-			await module.client.collection('objects').updateOne({ _key: key }, { $push: { array: { $each: [value], $position: 0 } } }, { upsert: true });
+			await module.client.collection('objects').updateOne({
+				_key: key,
+			}, {
+				$push: {
+					array: {
+						$each: value,
+						$position: 0,
+					},
+				},
+			}, {
+				upsert: true,
+			});
 		} else {
 			await module.listAppend(key, value);
 		}
@@ -22,8 +33,19 @@ module.exports = function (module) {
 		if (!key) {
 			return;
 		}
-		value = helpers.valueToString(value);
-		await module.client.collection('objects').updateOne({ _key: key }, { $push: { array: value } }, { upsert: true });
+		value = Array.isArray(value) ? value : [value];
+		value = value.map(helpers.valueToString);
+		await module.client.collection('objects').updateOne({
+			_key: key,
+		}, {
+			$push: {
+				array: {
+					$each: value,
+				},
+			},
+		}, {
+			upsert: true,
+		});
 	};
 
 	module.listRemoveLast = async function (key) {
