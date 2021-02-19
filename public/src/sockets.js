beb14273876406b69bb202ab2805926862e27879
Barış Soner Uşaklı <barisusakli@gmail.com>
2021-02-19 11:51:28 -0500

    feat: add logout to invalid session

--- public/src/sockets.js
+++ public/src/sockets.js
@@ -75,6 +75,7 @@ socket = window.socket;
 		socket.on('checkSession', function (uid) {
 			if (parseInt(uid, 10) !== parseInt(app.user.uid, 10)) {
 				app.handleInvalidSession();
+				app.logout(false);
 			}
 		});
 
