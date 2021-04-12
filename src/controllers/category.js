49583fe48b1947bc549ce25f8fe0ef52411888b9
Barış Soner Uşaklı <barisusakli@gmail.com>
2021-04-12 17:12:26 -0400

    feat: allow slugs

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -49,8 +49,8 @@ categoryController.get = async function (req, res, next) {
 		return helpers.notAllowed(req, res);
 	}
 
-	if (!res.locals.isAPI && (!req.params.slug || categoryFields.slug !== cid + '/' + req.params.slug) && (categoryFields.slug && categoryFields.slug !== cid + '/')) {
-		return helpers.redirect(res, '/category/' + categoryFields.slug, true);
+	if (!res.locals.isAPI && !req.params.slug && (categoryFields.slug && categoryFields.slug !== `${cid}/`)) {
+		return helpers.redirect(res, `/category/${categoryFields.slug}`, true);
 	}
 
 	if (categoryFields.link) {
