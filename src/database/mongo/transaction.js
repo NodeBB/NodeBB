08ecf14419f2936d0a9349f9547017a3c82421d8
Opliko <opliko.reg@protonmail.com>
2021-10-16 22:02:21 +0000

    chore: remove TODO from mongo transaction

--- src/database/mongo/transaction.js
+++ src/database/mongo/transaction.js
@@ -1,15 +1,12 @@
 'use strict';
 
 module.exports = function (module) {
-	// TODO
 	module.transaction = async function (perform) {
-		let res;
 		const session = module.mongoClient.startSession();
 		try {
-			res = await session.withTransaction(perform);
+			return await session.withTransaction(perform);
 		} finally {
 			await session.endSession();
 		}
-		return res;
 	};
 };
