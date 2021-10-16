eeb435f0e37916426a5c485098668052a7dcacbc
Opliko <opliko.reg@protonmail.com>
2021-10-16 22:38:03 +0000

    fix: don't run transactions on single instances

--- src/database/mongo/transaction.js
+++ src/database/mongo/transaction.js
@@ -2,6 +2,9 @@
 
 module.exports = function (module) {
 	module.transaction = async function (perform) {
+		if (module.mongoClient.topology.description.type === 'Single' || !module.mongoClient.topology.hasSessionSupport()) {
+			return await perform();
+		}
 		const session = module.mongoClient.startSession();
 		try {
 			return await session.withTransaction(perform);
