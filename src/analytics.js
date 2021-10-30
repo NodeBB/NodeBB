f05c605013a21bc2e7784b7b33a8210ac8db5820
Stanislaw Adaszewski <s.adaszewski@gmail.com>
2021-10-30 13:07:55 +0200

    incr by bulk.

--- src/analytics.js
+++ src/analytics.js
@@ -85,41 +85,51 @@ Analytics.pageView = async function (payload) {
 	}
 };
 
+function sortedSetIncrByBulk(data) {
+	if ('sortedSetIncrByBulk' in db) {
+		return db.sortedSetIncrByBulk(data);
+	} else {
+		const queue = data.map(item => db.sortedSetIncrBy(item[0], item[1], item[2]));
+		return Promise.all(queue);
+	}
+}
+
 Analytics.writeData = async function () {
 	const today = new Date();
 	const month = new Date();
 	const dbQueue = [];
+	const incrByBulk = [];
 
 	today.setHours(today.getHours(), 0, 0, 0);
 	month.setMonth(month.getMonth(), 1);
 	month.setHours(0, 0, 0, 0);
 
 	if (pageViews > 0) {
-		dbQueue.push(db.sortedSetIncrBy('analytics:pageviews', pageViews, today.getTime()));
-		dbQueue.push(db.sortedSetIncrBy('analytics:pageviews:month', pageViews, month.getTime()));
+		incrByBulk.push([ 'analytics:pageviews', pageViews, today.getTime() ]);
+		incrByBulk.push([ 'analytics:pageviews:month', pageViews, month.getTime() ]);
 		pageViews = 0;
 	}
 
 	if (pageViewsRegistered > 0) {
-		dbQueue.push(db.sortedSetIncrBy('analytics:pageviews:registered', pageViewsRegistered, today.getTime()));
-		dbQueue.push(db.sortedSetIncrBy('analytics:pageviews:month:registered', pageViewsRegistered, month.getTime()));
+		incrByBulk.push([ 'analytics:pageviews:registered', pageViewsRegistered, today.getTime() ]);
+		incrByBulk.push([ 'analytics:pageviews:month:registered', pageViewsRegistered, month.getTime() ]);
 		pageViewsRegistered = 0;
 	}
 
 	if (pageViewsGuest > 0) {
-		dbQueue.push(db.sortedSetIncrBy('analytics:pageviews:guest', pageViewsGuest, today.getTime()));
-		dbQueue.push(db.sortedSetIncrBy('analytics:pageviews:month:guest', pageViewsGuest, month.getTime()));
+		incrByBulk.push([ 'analytics:pageviews:guest', pageViewsGuest, today.getTime() ]);
+		incrByBulk.push([ 'analytics:pageviews:month:guest', pageViewsGuest, month.getTime() ]);
 		pageViewsGuest = 0;
 	}
 
 	if (pageViewsBot > 0) {
-		dbQueue.push(db.sortedSetIncrBy('analytics:pageviews:bot', pageViewsBot, today.getTime()));
-		dbQueue.push(db.sortedSetIncrBy('analytics:pageviews:month:bot', pageViewsBot, month.getTime()));
+		incrByBulk.push([ 'analytics:pageviews:bot', pageViewsBot, today.getTime() ]);
+		incrByBulk.push([ 'analytics:pageviews:month:bot', pageViewsBot, month.getTime() ]);
 		pageViewsBot = 0;
 	}
 
 	if (uniquevisitors > 0) {
-		dbQueue.push(db.sortedSetIncrBy('analytics:uniquevisitors', uniquevisitors, today.getTime()));
+		incrByBulk.push([ 'analytics:uniquevisitors', uniquevisitors, today.getTime() ]);
 		uniquevisitors = 0;
 	}
 
@@ -129,9 +139,14 @@ Analytics.writeData = async function () {
 	}
 
 	for (const [key, value] of Object.entries(counters)) {
-		dbQueue.push(db.sortedSetIncrBy(`analytics:${key}`, value, today.getTime()));
+		incrByBulk.push([ `analytics:${key}`, value, today.getTime() ]);
 		delete counters[key];
 	}
+
+	if (incrByBulk.length) {
+		dbQueue.push(sortedSetIncrByBulk(incrByBulk));
+	}
+
 	try {
 		await Promise.all(dbQueue);
 	} catch (err) {
