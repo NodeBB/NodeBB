80f9963bed22b495c5e3ecac5b31a2930ccc72cf
Barış Soner Uşaklı <barisusakli@gmail.com>
2021-11-10 20:19:13 -0500

    refactor: remove jshint, remove async.parallel

--- src/socket.io/admin/analytics.js
+++ src/socket.io/admin/analytics.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const async = require('async');
 const analytics = require('../../analytics');
+const utils = require('../../utils');
 
 const Analytics = module.exports;
 
-Analytics.get = function (socket, data, callback) {
+Analytics.get = async function (socket, data) {
 	if (!data || !data.graph || !data.units) {
-		return callback(new Error('[[error:invalid-data]]'));
+		throw new Error('[[error:invalid-data]]');
 	}
 
 	// Default returns views from past 24 hours, by hour
@@ -20,30 +20,17 @@ Analytics.get = function (socket, data, callback) {
 	}
 	const getStats = data.units === 'days' ? analytics.getDailyStatsForSet : analytics.getHourlyStatsForSet;
 	if (data.graph === 'traffic') {
-		async.parallel({
-			uniqueVisitors: function (next) {
-				getStats('analytics:uniquevisitors', data.until || Date.now(), data.amount, next);
-			},
-			pageviews: function (next) {
-				getStats('analytics:pageviews', data.until || Date.now(), data.amount, next);
-			},
-			pageviewsRegistered: function (next) {
-				getStats('analytics:pageviews:registered', data.until || Date.now(), data.amount, next);
-			},
-			pageviewsGuest: function (next) {
-				getStats('analytics:pageviews:guest', data.until || Date.now(), data.amount, next);
-			},
-			pageviewsBot: function (next) {
-				getStats('analytics:pageviews:bot', data.until || Date.now(), data.amount, next);
-			},
-			summary: function (next) {
-				analytics.getSummary(next);
-			},
-		}, (err, data) => {
-			data.pastDay = data.pageviews.reduce((a, b) => parseInt(a, 10) + parseInt(b, 10));
-			const last = data.pageviews.length - 1;
-			data.pageviews[last] = parseInt(data.pageviews[last], 10) + analytics.getUnwrittenPageviews();
-			callback(err, data);
+		const result = await utils.promiseParallel({
+			uniqueVisitors: getStats('analytics:uniquevisitors', data.until || Date.now(), data.amount),
+			pageviews: getStats('analytics:pageviews', data.until || Date.now(), data.amount),
+			pageviewsRegistered: getStats('analytics:pageviews:registered', data.until || Date.now(), data.amount),
+			pageviewsGuest: getStats('analytics:pageviews:guest', data.until || Date.now(), data.amount),
+			pageviewsBot: getStats('analytics:pageviews:bot', data.until || Date.now(), data.amount),
+			summary: analytics.getSummary(),
 		});
+		result.pastDay = result.pageviews.reduce((a, b) => parseInt(a, 10) + parseInt(b, 10));
+		const last = result.pageviews.length - 1;
+		result.pageviews[last] = parseInt(result.pageviews[last], 10) + analytics.getUnwrittenPageviews();
+		return result;
 	}
 };
