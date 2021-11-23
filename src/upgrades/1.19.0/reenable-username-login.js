f37c879b08720e675d0b18ab3a7ee872124feec0
Julian Lam <julian@nodebb.org>
2021-11-23 14:21:26 -0500

    feat: remove email-only login, upgrade script to fix config

---
+++ src/upgrades/1.19.0/reenable-username-login.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const db = require('../../database');
+const meta = require('../../meta');
+
+module.exports = {
+	name: 'Re-enable username login',
+	timestamp: Date.UTC(2021, 10, 23),
+	method: async () => {
+		const setting = await meta.config.allowLoginWith;
+
+		if (setting === 'email') {
+			await meta.configs.set('allowLoginWith', 'username-email');
+		}
+	},
+};
