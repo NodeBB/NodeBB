2f64d63369b8f56f1f1e4f2e2ea97abd71058516
Barış Soner Uşaklı <barisusakli@gmail.com>
2022-02-15 20:22:22 -0500

    fix: doggy.gif

--- src/upgrades/1.19.3/rename_post_upload_hashes.js
+++ src/upgrades/1.19.3/rename_post_upload_hashes.js
@@ -3,7 +3,6 @@
 'use strict';
 
 const crypto = require('crypto');
-const _ = require('lodash');
 
 const db = require('../../database');
 const batch = require('../../batch');
@@ -28,7 +27,7 @@ module.exports = {
 				let uploads = await db.getSortedSetRangeWithScores(key, 0, -1);
 
 				// Don't process those that have already the right format
-				uploads = _.uniq(uploads.filter(upload => !upload.value.startsWith('files/')));
+				uploads = uploads.filter(upload => upload && upload.value && !upload.value.startsWith('files/'));
 
 				// Rename the zset members
 				await db.sortedSetRemove(key, uploads.map(upload => upload.value));
