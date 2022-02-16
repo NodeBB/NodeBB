cbf090a15e7e4f649a1d36ead68a33d7ef93a179
sadaszewski <s.adaszewski@gmail.com>
2022-02-16 18:19:35 +0100

    Update webserver.js

--- src/webserver.js
+++ src/webserver.js
@@ -174,11 +174,13 @@ function setupExpressApp(app) {
 	auth.initialize(app, middleware);
 	const als = require('./als');
 	app.use((req, res, next) => {
-		als.run({ uid: req.uid,
+		als.run({
+			uid: req.uid,
 			url: req.url,
 			baseUrl: req.baseUrl,
 			originalUrl: req.originalUrl,
-			path: req.path }, next);
+			path: req.path
+		}, next);
 	});
 	app.use(middleware.autoLocale); // must be added after auth middlewares are added
 
