935704a83caab7e53d46e713f3b40378c9b99b63
Julian Lam <julian@nodebb.org>
2022-03-15 16:30:05 -0400

    feat: collect hook logs in order to reduce console noise, flush on ajaxify loadScript completion

--- public/src/ajaxify.js
+++ public/src/ajaxify.js
@@ -122,6 +122,7 @@ ajaxify = window.ajaxify || {};
 			url: url,
 		};
 
+		hooks.logs.collect();
 		hooks.fire('action:ajaxify.start', payload);
 
 		ajaxify.count += 1;
@@ -304,6 +305,7 @@ ajaxify = window.ajaxify || {};
 		}
 		ajaxify.loadScript(tpl_url, function done() {
 			hooks.fire('action:ajaxify.end', { url: url, tpl_url: tpl_url, title: ajaxify.data.title });
+			hooks.logs.flush();
 		});
 		ajaxify.widgets.render(tpl_url);
 
