c0a13d7e1d1f2b123e192bb769e643086d5bc751
Julian Lam <julian@nodebb.org>
2022-04-04 13:15:39 -0400

    refactor: introduce a generic autocomplete.init() method that can be called to add nodebb-style autocompletion but using different data sources (e.g. not user/groups/tags)

--- public/src/modules/autocomplete.js
+++ public/src/modules/autocomplete.js
@@ -1,121 +1,116 @@
-
 'use strict';
-
 
 define('autocomplete', ['api', 'alerts'], function (api, alerts) {
 	const module = {};
 
-	module.user = function (input, params, onselect) {
+	module.init = ({ input, source, onSelect, delay = 200 }) => {
+		app.loadJQueryUI(function () {
+			input.autocomplete({
+				delay,
+				open: function () {
+					$(this).autocomplete('widget').css('z-index', 100005);
+				},
+				select: function (event, ui) {
+					console.log(onSelect);
+					handleOnSelect(input, onSelect, event, ui);
+				},
+				source,
+			});
+		});
+	};
+
+	module.user = function (input, params, onSelect) {
 		if (typeof params === 'function') {
-			onselect = params;
+			onSelect = params;
 			params = {};
 		}
 		params = params || {};
-		app.loadJQueryUI(function () {
-			input.autocomplete({
-				delay: 200,
-				open: function () {
-					$(this).autocomplete('widget').css('z-index', 100005);
-				},
-				select: function (event, ui) {
-					handleOnSelect(input, onselect, event, ui);
-				},
-				source: function (request, response) {
-					params.query = request.term;
 
-					api.get('/api/users', params, function (err, result) {
-						if (err) {
-							return alerts.error(err);
-						}
+		module.init({
+			input,
+			onSelect,
+			source: (request, response) => {
+				params.query = request.term;
 
-						if (result && result.users) {
-							const names = result.users.map(function (user) {
-								const username = $('<div></div>').html(user.username).text();
-								return user && {
-									label: username,
-									value: username,
-									user: {
-										uid: user.uid,
-										name: user.username,
-										slug: user.userslug,
-										username: user.username,
-										userslug: user.userslug,
-										picture: user.picture,
-										banned: user.banned,
-										'icon:text': user['icon:text'],
-										'icon:bgColor': user['icon:bgColor'],
-									},
-								};
-							});
-							response(names);
-						}
+				api.get('/api/users', params, function (err, result) {
+					if (err) {
+						return alerts.error(err);
+					}
 
-						$('.ui-autocomplete a').attr('data-ajaxify', 'false');
-					});
-				},
-			});
+					if (result && result.users) {
+						const names = result.users.map(function (user) {
+							const username = $('<div></div>').html(user.username).text();
+							return user && {
+								label: username,
+								value: username,
+								user: {
+									uid: user.uid,
+									name: user.username,
+									slug: user.userslug,
+									username: user.username,
+									userslug: user.userslug,
+									picture: user.picture,
+									banned: user.banned,
+									'icon:text': user['icon:text'],
+									'icon:bgColor': user['icon:bgColor'],
+								},
+							};
+						});
+						response(names);
+					}
+
+					$('.ui-autocomplete a').attr('data-ajaxify', 'false');
+				});
+			},
 		});
 	};
 
-	module.group = function (input, onselect) {
-		app.loadJQueryUI(function () {
-			input.autocomplete({
-				delay: 200,
-				open: function () {
-					$(this).autocomplete('widget').css('z-index', 100005);
-				},
-				select: function (event, ui) {
-					handleOnSelect(input, onselect, event, ui);
-				},
-				source: function (request, response) {
-					socket.emit('groups.search', {
-						query: request.term,
-					}, function (err, results) {
-						if (err) {
-							return alerts.error(err);
-						}
-						if (results && results.length) {
-							const names = results.map(function (group) {
-								return group && {
-									label: group.name,
-									value: group.name,
-									group: group,
-								};
-							});
-							response(names);
-						}
-						$('.ui-autocomplete a').attr('data-ajaxify', 'false');
-					});
-				},
-			});
+	module.group = function (input, onSelect) {
+		module.init({
+			input,
+			onSelect,
+			source: (request, response) => {
+				socket.emit('groups.search', {
+					query: request.term,
+				}, function (err, results) {
+					if (err) {
+						return alerts.error(err);
+					}
+					if (results && results.length) {
+						const names = results.map(function (group) {
+							return group && {
+								label: group.name,
+								value: group.name,
+								group: group,
+							};
+						});
+						response(names);
+					}
+					$('.ui-autocomplete a').attr('data-ajaxify', 'false');
+				});
+			},
 		});
 	};
 
-	module.tag = function (input, onselect) {
-		app.loadJQueryUI(function () {
-			input.autocomplete({
-				delay: 100,
-				open: function () {
-					$(this).autocomplete('widget').css('z-index', 20000);
-				},
-				select: function (event, ui) {
-					handleOnSelect(input, onselect, event, ui);
-				},
-				source: function (request, response) {
-					socket.emit('topics.autocompleteTags', {
-						query: request.term,
-						cid: ajaxify.data.cid || 0,
-					}, function (err, tags) {
-						if (err) {
-							return alerts.error(err);
-						}
-						if (tags) {
-							response(tags);
-						}
-						$('.ui-autocomplete a').attr('data-ajaxify', 'false');
-					});
-				},
-			});
+	module.tag = function (input, onSelect) {
+		module.init({
+			input,
+			onSelect,
+			delay: 100,
+			source: (request, response) => {
+				socket.emit('topics.autocompleteTags', {
+					query: request.term,
+					cid: ajaxify.data.cid || 0,
+				}, function (err, tags) {
+					if (err) {
+						return alerts.error(err);
+					}
+					if (tags) {
+						response(tags);
+					}
+					$('.ui-autocomplete a').attr('data-ajaxify', 'false');
+				});
+			},
 		});
 	};
 
