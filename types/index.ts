2b6f661f190c9d6a688b90aa16921826a59a1652
steve <29133953+stevefan1999-personal@users.noreply.github.com>
2022-07-14 00:21:03 +0800

    types: make INodeBBDatabaseBackend implement the five major interface set
    Signed-off-by: steve <29133953+stevefan1999-personal@users.noreply.github.com>

--- types/index.ts
+++ types/index.ts
@@ -1,35 +1,36 @@
-import { Store } from 'express-session'
+import {Store} from 'express-session'
+import {StringQueryable} from "./string";
+import {ListQueryable} from "./list";
+import {HashSetQueryable} from "./set";
+import {SortedSetQueryable} from "./zset";
+import {HashQueryable} from "./hash";
 
-export { HashQueryable } from './hash'
-export { ListQueryable } from './list'
-export { HashSetQueryable } from './set'
-export { StringQueryable } from './string'
-export {
+
+export interface INodeBBDatabaseBackend extends
+  StringQueryable,
+  ListQueryable,
+  HashSetQueryable,
   SortedSetQueryable,
-  SortedSetTheoryOperation,
-  SortedSetScanBaseParameters,
-} from './zset'
-
-export interface INodeBBDatabaseBackend {
+  HashQueryable {
   checkCompatibility(callback: () => void): Promise<void>
-
+  
   checkCompatibilityVersion(
     version: string,
     callback: () => void,
   ): Promise<void>
-
+  
   close(): Promise<void>
-
+  
   createIndices(callback: () => void): Promise<void>
-
+  
   createSessionStore(options: any): Promise<Store>
-
+  
   emptydb(): Promise<void>
-
+  
   flushdb(): Promise<void>
-
+  
   info(db: any): Promise<any>
-
+  
   init(): Promise<void>
 }
 
@@ -56,3 +57,13 @@ export type ValueAndScore = { value: string; score: number }
 export type RedisStyleAggregate = 'SUM' | 'MIN' | 'MAX'
 export type NumberTowardsMinima = number | '-inf'
 export type NumberTowardsMaxima = number | '+inf'
+
+export {HashQueryable} from './hash'
+export {ListQueryable} from './list'
+export {HashSetQueryable} from './set'
+export {StringQueryable} from './string'
+export {
+  SortedSetQueryable,
+  SortedSetTheoryOperation,
+  SortedSetScanBaseParameters,
+} from './zset'
