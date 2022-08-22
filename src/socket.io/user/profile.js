ea403211e339b361a24fafea06a0bcfa9cf3b4f5
Julian Lam <julian@nodebb.org>
2022-08-22 15:50:02 -0400

    refactor: move export generation logic to v3 controller, GET/HEAD routes for exports
    re: #10384

--- src/socket.io/user/profile.js
+++ src/socket.io/user/profile.js
@@ -1,13 +1,11 @@
 'use strict';
 
-const winston = require('winston');
-
 const user = require('../../user');
-const events = require('../../events');
-const notifications = require('../../notifications');
 const privileges = require('../../privileges');
-const db = require('../../database');
 const plugins = require('../../plugins');
+
+const sockets = require('..');
+const api = require('../../api');
 
 module.exports = function (SocketUser) {
 	SocketUser.updateCover = async function (socket, data) {
@@ -64,6 +62,8 @@ module.exports = function (SocketUser) {
 	};
 
 	async function doExport(socket, data, type) {
+		sockets.warnDeprecated(socket, 'POST /api/v3/users/:uid/exports/:type');
+
 		if (!socket.uid) {
 			throw new Error('[[error:invalid-uid]]');
 		}
@@ -74,36 +74,6 @@ module.exports = function (SocketUser) {
 
 		await user.isAdminOrSelf(socket.uid, data.uid);
 
-		const count = await db.incrObjectField('locks', `export:${data.uid}${type}`);
-		if (count > 1) {
-			throw new Error('[[error:already-exporting]]');
-		}
-
-		const child = require('child_process').fork(`./src/user/jobs/export-${type}.js`, [], {
-			env: process.env,
-		});
-		child.send({ uid: data.uid });
-		child.on('error', async (err) => {
-			winston.error(err.stack);
-			await db.deleteObjectField('locks', `export:${data.uid}${type}`);
-		});
-		child.on('exit', async () => {
-			await db.deleteObjectField('locks', `export:${data.uid}${type}`);
-			const userData = await user.getUserFields(data.uid, ['username', 'userslug']);
-			const { displayname } = userData;
-			const n = await notifications.create({
-				bodyShort: `[[notifications:${type}-exported, ${displayname}]]`,
-				path: `/api/user/${userData.userslug}/export/${type}`,
-				nid: `${type}:export:${data.uid}`,
-				from: data.uid,
-			});
-			await notifications.push(n, [socket.uid]);
-			await events.log({
-				type: `export:${type}`,
-				uid: socket.uid,
-				targetUid: data.uid,
-				ip: socket.ip,
-			});
-		});
+		api.users.generateExport(socket, { type, ...data });
 	}
 };
