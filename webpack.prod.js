02ef3e974c72ae1aa8a934960d10beec98cc84e4
Barış Soner Uşaklı <barisusakli@gmail.com>
2022-10-06 07:51:13 -0400

    Esbuild (#10940)
    * ci: add minimum GitHub token permissions for workflows
    Signed-off-by: Ashish Kurmi <akurmi@stepsecurity.io>
    
    * feat: use esbuild for minification
    
    Co-authored-by: Ashish Kurmi <akurmi@stepsecurity.io>

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,19 @@
 'use strict';
 
 const { merge } = require('webpack-merge');
+const TerserPlugin = require('terser-webpack-plugin');
+
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
 	mode: 'production',
+	optimization: {
+		minimize: true,
+		minimizer: [
+			new TerserPlugin({
+				minify: TerserPlugin.esbuildMinify,
+				terserOptions: {},
+			}),
+		],
+	},
 });
