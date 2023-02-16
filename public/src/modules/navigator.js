5f4f45c44e8cd2e4d715213f2645d42f6f3c229f
Barış Soner Uşaklı <barisusakli@gmail.com>
2023-02-15 20:36:34 -0500

    fix: navigator not showing up on first load

--- public/src/modules/navigator.js
+++ public/src/modules/navigator.js
@@ -361,7 +361,7 @@ define('navigator', ['forum/pagination', 'components', 'hooks', 'alerts', 'trans
 		unreadEl.style.height = `${trackHeight * percentage}px`;
 
 		const thumbEl = trackEl.querySelector('.scroller-thumb');
-		const thumbBottom = parseInt(thumbEl.style.top, 10) + parseInt(thumbEl.style.height, 10);
+		const thumbBottom = parseInt(thumbEl.style.top || 0, 10) + parseInt(thumbEl.style.height, 10);
 		const anchorEl = unreadEl.querySelector('.meta a');
 		const remaining = ajaxify.data.postcount - index;
 		if (remaining > 0 && (trackHeight - thumbBottom) > 50) {
