175ee0c2dcfba16a08121078518d8031ddc8c513
Julian Lam <julian@nodebb.org>
2023-03-01 12:41:38 -0500

    refactor: audit flags v3 write api routes as per #11321

--- src/controllers/write/flags.js
+++ src/controllers/write/flags.js
@@ -1,53 +1,48 @@
 'use strict';
 
 const user = require('../../user');
-const flags = require('../../flags');
 const api = require('../../api');
 const helpers = require('../helpers');
 
 const Flags = module.exports;
 
 Flags.create = async (req, res) => {
-	const flagObj = await api.flags.create(req, { ...req.body });
+	const { type, id, reason } = req.body;
+	const flagObj = await api.flags.create(req, { type, id, reason });
 	helpers.formatApiResponse(200, res, await user.isPrivileged(req.uid) ? flagObj : undefined);
 };
 
 Flags.get = async (req, res) => {
-	const isPrivileged = await user.isPrivileged(req.uid);
-	if (!isPrivileged) {
-		return helpers.formatApiResponse(403, res);
-	}
-
-	helpers.formatApiResponse(200, res, await flags.get(req.params.flagId));
+	helpers.formatApiResponse(200, res, await api.flags.get(req, req.params.flagId));
 };
 
 Flags.update = async (req, res) => {
+	const { state, assignee } = req.body;
 	const history = await api.flags.update(req, {
 		flagId: req.params.flagId,
-		...req.body,
+		state,
+		assignee,
 	});
 
 	helpers.formatApiResponse(200, res, { history });
 };
 
 Flags.delete = async (req, res) => {
-	await flags.purge([req.params.flagId]);
+	await api.flags.delete(req, { flagId: req.params.flagId });
 	helpers.formatApiResponse(200, res);
 };
 
 Flags.appendNote = async (req, res) => {
+	const { note, datetime } = req.body;
 	const payload = await api.flags.appendNote(req, {
 		flagId: req.params.flagId,
-		...req.body,
+		note,
+		datetime,
 	});
 
 	helpers.formatApiResponse(200, res, payload);
 };
 
 Flags.deleteNote = async (req, res) => {
-	const payload = await api.flags.deleteNote(req, {
-		...req.params,
-	});
-
-	helpers.formatApiResponse(200, res, payload);
+	helpers.formatApiResponse(200, res, await api.flags.deleteNote(req, req.params));
 };
