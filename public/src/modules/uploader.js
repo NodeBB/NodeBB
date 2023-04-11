dd8719a10e615bf2597992928555818555b38a91
Julian Lam <julian@nodebb.org>
2023-04-11 10:10:05 -0400

    fix: upload modal error handler
    The underlying API call was updated to v3 style (with standard status/response object in response body), so the existing code always falls back to the default error instead of showing the more specific error sent back by the backend.
    
    Also, the fallback wasn't internationalized, so that's done to.

--- public/src/modules/uploader.js
+++ public/src/modules/uploader.js
@@ -72,7 +72,7 @@ define('uploader', ['jquery-form'], function () {
 			},
 			error: function (xhr) {
 				xhr = maybeParse(xhr);
-				showAlert(uploadModal, 'error', xhr.responseJSON ? (xhr.responseJSON.error || xhr.statusText) : 'error uploading, code : ' + xhr.status);
+				showAlert(uploadModal, 'error', xhr.responseJSON?.status?.message || `[[error:upload-error-fallback, ${xhr.status} ${xhr.statusText}]]`);
 			},
 			uploadProgress: function (event, position, total, percent) {
 				uploadModal.find('#upload-progress-bar').css('width', percent + '%');
