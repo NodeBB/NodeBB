165b0f856581b0404ad1680e9f1a9e01c9677cb9
Barış Soner Uşaklı <barisusakli@gmail.com>
2023-07-15 21:19:07 -0400

    fix: closes #11806, fix code blocks

--- public/src/client/topic.js
+++ public/src/client/topic.js
@@ -242,6 +242,15 @@ define('forum/topic', [
 			function scrollbarVisible(element) {
 				return element.scrollHeight > element.clientHeight;
 			}
+			function offsetCodeBtn(codeEl) {
+				if (!codeEl.length) { return; }
+				if (!codeEl[0].scrollHeight) {
+					return setTimeout(offsetCodeBtn, 100, codeEl);
+				}
+				if (scrollbarVisible(codeEl.get(0))) {
+					codeEl.parent().parent().find('[component="copy/code/btn"]').css({ margin: '0.5rem 1.5rem 0 0' });
+				}
+			}
 			let codeBlocks = $('[component="topic"] [component="post/content"] code:not([data-button-added])');
 			codeBlocks = codeBlocks.filter((i, el) => $(el).text().includes('\n'));
 			const container = $('<div class="hover-parent position-relative"></div>');
@@ -250,9 +259,7 @@ define('forum/topic', [
 			preEls.wrap(container).parent().append(buttonDiv);
 			preEls.parent().find('[component="copy/code/btn"]').translateAttr('title', '[[topic:copy-code]]');
 			preEls.each((index, el) => {
-				if (scrollbarVisible(el)) {
-					$(el).parent().find('[component="copy/code/btn"]').css({ margin: '0.5rem 1.5rem 0 0' });
-				}
+				offsetCodeBtn($(el).find('code'));
 			});
 			codeBlocks.attr('data-button-added', 1);
 		}
