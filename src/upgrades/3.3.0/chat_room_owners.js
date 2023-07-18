77550a50dbfe0c4699521bf5cea844ba75873129
Barış Soner Uşaklı <barisusakli@gmail.com>
2023-07-18 17:19:57 -0400

    fix: fallback for room timestamp

--- src/upgrades/3.3.0/chat_room_owners.js
+++ src/upgrades/3.3.0/chat_room_owners.js
@@ -12,6 +12,8 @@ module.exports = {
 		const { progress } = this;
 
 		progress.total = await db.sortedSetCard('chat:rooms');
+		const users = await db.getSortedSetRangeWithScores(`users:joindate`, 0, 0);
+		const timestamp = users.length ? users[0].score : Date.now();
 
 		await batch.processSortedSet('chat:rooms', async (roomIds) => {
 			progress.incr(roomIds.length);
@@ -19,9 +21,17 @@ module.exports = {
 				roomIds.map(id => `chat:room:${id}`)
 			);
 
+			const arrayOfUids = await Promise.all(
+				roomIds.map(roomId => db.getSortedSetRangeWithScores(`chat:room:${roomId}:uids`, 0, 0))
+			);
+
 			const bulkAdd = [];
-			roomData.forEach((room) => {
-				if (room && room.roomId && room.owner && room.timestamp) {
+			roomData.forEach((room, idx) => {
+				if (room && room.roomId && room.owner) {
+					// if room doesn't have timestamp for some reason use the first user timestamp
+					room.timestamp = room.timestamp || (
+						arrayOfUids[idx].length ? (arrayOfUids[idx][0].score || timestamp) : timestamp
+					);
 					bulkAdd.push([`chat:room:${room.roomId}:owners`, room.timestamp, room.owner]);
 				}
 			});
