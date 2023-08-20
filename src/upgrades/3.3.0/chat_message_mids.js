78a7cfe9c25d08a3f2d40505807421eb791f3d15
Barış Soner Uşaklı <barisusakli@gmail.com>
2023-08-19 21:53:44 -0400

    fix: upgrade script, get rid of nested processSortedSet

--- src/upgrades/3.3.0/chat_message_mids.js
+++ src/upgrades/3.3.0/chat_message_mids.js
@@ -11,36 +11,36 @@ module.exports = {
 	method: async function () {
 		const { progress } = this;
 
-		progress.total = await db.sortedSetCard(`chat:rooms`);
-		await batch.processSortedSet(`chat:rooms`, async (roomIds) => {
-			progress.incr(roomIds.length);
-			for (const roomId of roomIds) {
-				await batch.processSortedSet(`chat:room:${roomId}:mids`, async (mids) => {
-					let messageData = await db.getObjects(mids.map(mid => `message:${mid}`));
-					messageData.forEach((m, idx) => {
-						if (m) {
-							m.mid = parseInt(mids[idx], 10);
-						}
-					});
-					messageData = messageData.filter(Boolean);
+		const allRoomIds = await db.getSortedSetRange(`chat:rooms`, 0, -1);
 
-					const bulkSet = messageData.map(
-						msg => [`message:${msg.mid}`, { mid: msg.mid }]
-					);
+		progress.total = allRoomIds.length;
 
-					await db.setObjectBulk(bulkSet);
-					await db.sortedSetAdd(
-						'messages:mid',
-						messageData.map(msg => msg.timestamp),
-						messageData.map(msg => msg.mid)
-					);
-				}, {
-					batch: 500,
+		for (const roomId of allRoomIds) {
+			await batch.processSortedSet(`chat:room:${roomId}:mids`, async (mids) => {
+				let messageData = await db.getObjects(mids.map(mid => `message:${mid}`));
+				messageData.forEach((m, idx) => {
+					if (m) {
+						m.mid = parseInt(mids[idx], 10);
+					}
 				});
-			}
-		}, {
-			batch: 500,
-		});
+				messageData = messageData.filter(Boolean);
+
+				const bulkSet = messageData.map(
+					msg => [`message:${msg.mid}`, { mid: msg.mid }]
+				);
+
+				await db.setObjectBulk(bulkSet);
+				await db.sortedSetAdd(
+					'messages:mid',
+					messageData.map(msg => msg.timestamp),
+					messageData.map(msg => msg.mid)
+				);
+			}, {
+				batch: 500,
+			});
+			progress.incr(1);
+		}
+
 		const count = await db.sortedSetCard(`messages:mid`);
 		await db.setObjectField('global', 'messageCount', count);
 	},
