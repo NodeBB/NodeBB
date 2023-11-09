24f218206f5758a40a892d00569ff8b800b46836
Julian Lam <julian@nodebb.org>
2023-11-09 11:39:26 -0500

    chore: v4 note for deprecations

--- src/socket.io/groups.js
+++ src/socket.io/groups.js
@@ -1,5 +1,10 @@
 'use strict';
 
+/**
+ * v4 note — all socket methods here have been deprecated and can be removed for v4
+ *           EXCEPT socketGroups.cover.*
+ */
+
 const groups = require('../groups');
 const user = require('../user');
 const utils = require('../utils');
