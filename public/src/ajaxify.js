NodeBB/NodeBB

commit a75a043b61ad21cdede136eb1e0dda64d06c4072

feat: allow % in tags, #10135

public/src/ajaxify.js
@@ -140,7 +140,7 @@ ajaxify = window.ajaxify || {};
 
 		if (data) {
 			let status = parseInt(data.status, 10);
-			if (status === 403 || status === 404 || status === 500 || status === 502 || status === 503) {
+			if ([400, 403, 404, 500, 502, 504].includes(status)) {
 				if (status === 502 && retry) {
 					retry = false;
 					ajaxifyTimer = undefined;

public/src/utils.js
@@ -334,7 +334,7 @@
 			tag = tag.trim().toLowerCase();
 			// see https://github.com/NodeBB/NodeBB/issues/4378
 			tag = tag.replace(/\u202E/gi, '');
-			tag = tag.replace(/[,/#!$%^*;:{}=_`<>'"~()?|]/g, '');
+			tag = tag.replace(/[,/#!$^*;:{}=_`<>'"~()?|]/g, '');
 			tag = tag.substr(0, maxLength || 15).trim();
 			const matches = tag.match(/^[.-]*(.+?)[.-]*$/);
 			if (matches && matches.length > 1) {

src/topics/data.js
@@ -134,6 +134,7 @@ function modifyTopic(topic, fields) {
 			return {
 				value: tag,
 				valueEscaped: escaped,
+				valueEncoded: encodeURIComponent(escaped),
 				class: escaped.replace(/\s/g, '-'),
 			};
 		});

src/topics/tags.js
@@ -285,6 +285,7 @@ module.exports = function (Topics) {
 		}
 		tags.forEach((tag) => {
 			tag.valueEscaped = validator.escape(String(tag.value));
+			tag.valueEncoded = encodeURIComponent(tag.valueEscaped);
 			tag.class = tag.valueEscaped.replace(/\s/g, '-');
 		});
 		return tags;

commit f7e0fd0bd7fd217cbe3574e06cf0d4de4b7e39e3

fix: 503 rendering on ajaxify
regression from https://github.com/NodeBB/NodeBB/commit/a75a043b61ad21cdede136eb1e0dda64d06c4072

public/src/ajaxify.js
@@ -150,7 +150,7 @@ ajaxify.widgets = { render: render };
 
 		if (data) {
 			let status = parseInt(data.status, 10);
-			if ([400, 403, 404, 500, 502, 504].includes(status)) {
+			if ([400, 403, 404, 500, 502, 503].includes(status)) {
 				if (status === 502 && retry) {
 					retry = false;
 					ajaxifyTimer = undefined;