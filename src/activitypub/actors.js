338c568c9919b5209da14472da87a0856e864f89
Julian Lam <julian@nodebb.org>
2024-03-13 10:56:00 -0400

    fix: handle actor assertion failure if mocked profile does not have a fullname

--- src/activitypub/actors.js
+++ src/activitypub/actors.js
@@ -114,7 +114,7 @@ Actors.assert = async (ids, options = {}) => {
 			memo.handleAdd[profile.username.toLowerCase()] = profile.uid;
 		}
 
-		if (fullname !== profile.fullname) {
+		if (profile.fullname && fullname !== profile.fullname) {
 			if (uidsForCurrent[idx] !== 0) {
 				memo.searchRemove.push(['ap.name:sorted', `${fullname.toLowerCase()}:${profile.uid}`]);
 			}
