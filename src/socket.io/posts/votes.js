8410cec797a5126a002097783b446c099de2febb
Barış Soner Uşaklı <barisusakli@gmail.com>
2024-04-17 09:15:35 -0400

    fix: index should be cid

--- src/socket.io/posts/votes.js
+++ src/socket.io/posts/votes.js
@@ -91,7 +91,7 @@ module.exports = function (SocketPosts) {
 		]);
 		const cidToAllowed = _.zip(uniqCids, canRead);
 		const checks = cids.map(
-			(cid, index) => isAdmin || isMod[index] || (cidToAllowed[index] && !!meta.config.votesArePublic)
+			(cid, index) => isAdmin || isMod[index] || (cidToAllowed[cid] && !!meta.config.votesArePublic)
 		);
 		return isArray ? checks : checks[0];
 	}
