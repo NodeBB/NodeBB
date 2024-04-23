c73f759cf1bef9fe90a9a516c84d271778468949
Barış Soner Uşaklı <barisusakli@gmail.com>
2024-04-23 13:23:20 -0400

    test: add back timestamp

--- src/user/info.js
+++ src/user/info.js
@@ -75,6 +75,7 @@ module.exports = function (User) {
 
 		flags.forEach((flag, idx) => {
 			if (flag) {
+				flag.timestamp = flag.datetime;
 				flag.timestampISO = new Date(flag.datetime).toISOString();
 				flag.reports = reports[idx];
 			}
