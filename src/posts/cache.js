236ac32303c7ead1ed52cc5746a9fc076de40c56
Barış Soner Uşaklı <barisusakli@gmail.com>
2024-06-10 17:10:43 -0400

    refactor: posts cache to get rid of require in functions

--- src/posts/cache.js
+++ src/posts/cache.js
@@ -3,10 +3,30 @@
 const cacheCreate = require('../cache/lru');
 const meta = require('../meta');
 
-module.exports = cacheCreate({
-	name: 'post',
-	maxSize: meta.config.postCacheSize,
-	sizeCalculation: function (n) { return n.length || 1; },
-	ttl: 0,
-	enabled: global.env === 'production',
-});
+let cache = null;
+
+exports.getOrCreate = function () {
+	if (!cache) {
+		cache = cacheCreate({
+			name: 'post',
+			maxSize: meta.config.postCacheSize,
+			sizeCalculation: function (n) { return n.length || 1; },
+			ttl: 0,
+			enabled: global.env === 'production',
+		});
+	}
+
+	return cache;
+};
+
+exports.del = function (pid) {
+	if (cache) {
+		cache.del(pid);
+	}
+}
+
+exports.reset = function () {
+	if (cache) {
+		cache.reset();
+	}
+}
