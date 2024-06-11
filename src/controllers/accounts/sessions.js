5c6a853e94cfe30651881e5b54d4b7fa75b49583
Barış Soner Uşaklı <barisusakli@gmail.com>
2024-06-11 16:07:10 -0400

    perf: get rid of more calls, fix other profile pages #12632

--- src/controllers/accounts/sessions.js
+++ src/controllers/accounts/sessions.js
@@ -6,13 +6,15 @@ const helpers = require('../helpers');
 const sessionController = module.exports;
 
 sessionController.get = async function (req, res) {
-	const { username, userslug } = await user.getUserFields(res.locals.uid, ['username', 'userslug']);
+	const payload = res.locals.userData;
+	const { username, userslug } = payload;
 
-	const payload = {
-		sessions: await user.auth.getSessions(res.locals.uid, req.sessionID),
-		title: '[[pages:account/sessions]]',
-		breadcrumbs: helpers.buildBreadcrumbs([{ text: username, url: `/user/${userslug}` }, { text: '[[pages:account/sessions]]' }]),
-	};
+	payload.sessions = await user.auth.getSessions(res.locals.uid, req.sessionID);
+	payload.title = '[[pages:account/sessions]]';
+	payload.breadcrumbs = helpers.buildBreadcrumbs([
+		{ text: username, url: `/user/${userslug}` },
+		{ text: '[[pages:account/sessions]]' },
+	]);
 
 	res.render('account/sessions', payload);
 };
