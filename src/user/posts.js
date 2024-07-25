3e494dc757b759ded1eafa4d8793d5fb1041f807
Julian Lam <julian@nodebb.org>
2024-07-25 14:30:57 -0400

    fix: updating of post counts for remote users

--- src/user/posts.js
+++ src/user/posts.js
@@ -111,7 +111,7 @@ module.exports = function (User) {
 		if (uids.length) {
 			const counts = await db.sortedSetsCard(uids.map(uid => `uid:${uid}:posts`));
 			await Promise.all([
-				db.setObjectBulk(uids.map((uid, index) => ([`user:${uid}`, { postcount: counts[index] }]))),
+				db.setObjectBulk(uids.map((uid, index) => ([`user${activitypub.helpers.isUri(uid) ? 'Remote' : ''}:${uid}`, { postcount: counts[index] }]))),
 				db.sortedSetAdd('users:postcount', counts, uids),
 			]);
 		}
