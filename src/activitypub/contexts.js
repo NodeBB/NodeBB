NodeBB/NodeBB

commit 903d03891941902ddac80b380552530e1853c306

fix: fall back to inReplyTo traversal on context resolution failure

src/activitypub/contexts.js
@@ -79,6 +79,11 @@ Contexts.getItems = async (uid, id, options) => {
 		next = first;
 	}
 
+	// Early breakout on empty collection
+	if (!chain.size) {
+		return [];
+	}
+
 	if (next) {
 		winston.verbose('[activitypub/context] Fetching next page...');
 		Array

src/activitypub/notes.js
@@ -49,10 +49,14 @@ Notes.assert = async (uid, input, options = { skipChecks: false }) => {
 		return { tid, count: 0 };
 	} else if (context.context) {
 		chain = Array.from(await activitypub.contexts.getItems(uid, context.context, { input }));
-	} else {
-		// Fall back to inReplyTo traversal
+	}
+
+	if (!chain) {
+		// Fall back to inReplyTo traversal on context retrieval failure
 		chain = Array.from(await Notes.getParentChain(uid, input));
 	}
+
+	// Can't resolve — give up.
 	if (!chain.length) {
 		unlock(id);
 		return null;

commit d451adafc4edf832216fb2df2d6a99982b5bb501

fix: regression introduced by 903d03891941902ddac80b380552530e1853c306

src/activitypub/contexts.js
@@ -80,7 +80,7 @@ Contexts.getItems = async (uid, id, options) => {
 	}
 
 	// Early breakout on empty collection
-	if (!chain.size) {
+	if (!next && !chain.size) {
 		return [];
 	}
 