679fcb71a988310bd8ef6061c3366eb04d288109
Barış Soner Uşaklı <barisusakli@gmail.com>
2025-01-22 13:16:40 -0500

    fix: #13067, add sourceContent to teasers
    required in posts.parse

--- src/topics/teaser.js
+++ src/topics/teaser.js
@@ -43,7 +43,7 @@ module.exports = function (Topics) {
 		});
 
 		const [allPostData, callerSettings] = await Promise.all([
-			posts.getPostsFields(teaserPids, ['pid', 'uid', 'timestamp', 'tid', 'content']),
+			posts.getPostsFields(teaserPids, ['pid', 'uid', 'timestamp', 'tid', 'content', 'sourceContent']),
 			user.getSettings(uid),
 		]);
 		let postData = allPostData.filter(post => post && post.pid);
