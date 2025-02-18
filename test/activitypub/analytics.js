6431824216c1871ea112064575575261254404d6
Barış Soner Uşaklı <barisusakli@gmail.com>
2025-02-18 10:53:25 -0500

    test: wait after post request

--- test/activitypub/analytics.js
+++ test/activitypub/analytics.js
@@ -128,7 +128,7 @@ describe('Analytics', () => {
 		let counters;
 		({ counters } = analytics.peek());
 		const before = { ...counters };
-
+		const { setTimeout } = require('timers/promises');
 		const id = `https://example.org/activity/${utils.generateUUID()}`;
 		await controllers.activitypub.postInbox({
 			body: {
@@ -141,7 +141,7 @@ describe('Analytics', () => {
 				},
 			},
 		}, { sendStatus: () => {} });
-
+		await setTimeout(2000);
 		({ counters } = analytics.peek());
 		const after = { ...counters };
 
