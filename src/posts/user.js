655d0c080181e2767b48dc35cf112a40332ef77e
Michael Banucu <michael.banucu@googlemail.com>
2025-04-20 21:42:53 +0200

    fix little bug with special case

--- src/posts/user.js
+++ src/posts/user.js
@@ -25,7 +25,15 @@ module.exports = function (Posts) {
 
 		userData.forEach((userData, index) => {
 			userData.signature = validator.escape(String(userData.signature || ''));
-			userData.fullname = userSettings[index].showfullname ? validator.escape(String(userData.fullname || '')) : undefined;
+			userData.fullname = userSettings[index]
+				?
+				userSettings[index].showfullname
+					?
+					validator.escape(String(userData.fullname || ''))
+					:
+					undefined
+				:
+				undefined;
 			userData.selectedGroups = [];
 
 			if (meta.config.hideFullname) {
