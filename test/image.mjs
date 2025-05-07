ef1abb14111e905fde7ca20d0f9f2c6085b9ff18
Michael Banucu <michael.banucu@googlemail.com>
2025-05-07 19:15:50 +0200

    translate test `image` to ESM using Grok 3 and fix

--- test/image.mjs
+++ test/image.mjs
@@ -1,38 +1,30 @@
-'use strict';
+import assert from 'assert';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const assert = require('assert');
-const path = require('path');
+import './mocks/databasemock.mjs';
+import image from '../src/image.js';
+import file from '../src/file.js';
 
-const db = require('./mocks/databasemock.mjs');
-const image = require('../src/image');
-const file = require('../src/file');
+// ESM equivalent of __dirname
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 describe('image', () => {
-	it('should normalise image', (done) => {
-		image.normalise(path.join(__dirname, 'files/normalise.jpg'), '.jpg', (err) => {
-			assert.ifError(err);
-			file.exists(path.join(__dirname, 'files/normalise.jpg.png'), (err, exists) => {
-				assert.ifError(err);
-				assert(exists);
-				done();
-			});
-		});
+	it('should normalise image', async () => {
+		await image.normalise(path.join(__dirname, 'files/normalise.jpg'), '.jpg');
+		const exists = await file.exists(path.join(__dirname, 'files/normalise.jpg.png'));
+		assert(exists);
 	});
 
-	it('should resize an image', (done) => {
-		image.resizeImage({
+	it('should resize an image', async () => {
+		await image.resizeImage({
 			path: path.join(__dirname, 'files/normalise.jpg'),
 			target: path.join(__dirname, 'files/normalise-resized.jpg'),
 			width: 50,
 			height: 40,
-		}, (err) => {
-			assert.ifError(err);
-			image.size(path.join(__dirname, 'files/normalise-resized.jpg'), (err, bitmap) => {
-				assert.ifError(err);
-				assert.equal(bitmap.width, 50);
-				assert.equal(bitmap.height, 40);
-				done();
-			});
 		});
+		const bitmap = await image.size(path.join(__dirname, 'files/normalise-resized.jpg'));
+		assert.equal(bitmap.width, 50);
+		assert.equal(bitmap.height, 40);
 	});
 });
