6bfe4e627d8bd396131442b59ee8c1fade8162b2
Julian Lam <julian@nodebb.org>
2025-05-08 16:34:13 -0400

    fix: another case

--- src/controllers/activitypub/index.js
+++ src/controllers/activitypub/index.js
@@ -71,13 +71,15 @@ Controller.getFollowing = async (req, res) => {
 		url: `${nconf.get('url')}/uid/${req.params.uid}/following`,
 	});
 
-	collection.orderedItems = collection.orderedItems.map(({ uid }) => {
-		if (utils.isNumber(uid)) {
-			return `${nconf.get('url')}/uid/${uid}`;
-		}
+	if (collection.hasOwnProperty('orderedItems')) {
+		collection.orderedItems = collection.orderedItems.map(({ uid }) => {
+			if (utils.isNumber(uid)) {
+				return `${nconf.get('url')}/uid/${uid}`;
+			}
 
-		return uid;
-	});
+			return uid;
+		});
+	}
 
 	res.status(200).json(collection);
 };
