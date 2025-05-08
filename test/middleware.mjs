6d0183c34a2b06d46a01bb032b0ccf093808cb23
Michael Banucu <michael.banucu@googlemail.com>
2025-05-08 20:55:32 +0200

    translate test `middleware` to ESM using Grok 3

--- test/middleware.mjs
+++ test/middleware.mjs
@@ -1,15 +1,12 @@
-'use strict';
-
-const assert = require('assert');
-const nconf = require('nconf');
-
-const db = require('./mocks/databasemock.mjs');
-
-const user = require('../src/user');
-const groups = require('../src/groups');
-const utils = require('../src/utils');
-const request = require('../src/request');
-const helpers = require('./helpers');
+import assert from 'node:assert';
+import nconf from 'nconf';
+import db from './mocks/databasemock.mjs';
+import * as user from '../src/user/index.js';
+import * as groups from '../src/groups/index.js';
+import * as utils from '../src/utils.js';
+import * as request from '../src/request.js';
+import * as helpers from './helpers.js';
+import * as middleware from '../src/middleware/index.js';
 
 describe('Middlewares', () => {
 	describe('expose', () => {
@@ -20,88 +17,98 @@ describe('Middlewares', () => {
 			await groups.join('administrators', adminUid);
 		});
 
-		it('should expose res.locals.isAdmin = false', (done) => {
-			const middleware = require('../src/middleware');
+		it('should expose res.locals.isAdmin = false', async () => {
 			const resMock = { locals: {} };
-			middleware.exposeAdmin({}, resMock, () => {
-				assert.strictEqual(resMock.locals.isAdmin, false);
-				done();
+			await new Promise((resolve, reject) => {
+				middleware.exposeAdmin({}, resMock, (err) => {
+					if (err) reject(err);
+					else resolve();
+				});
 			});
+			assert.strictEqual(resMock.locals.isAdmin, false);
 		});
 
-		it('should expose res.locals.isAdmin = true', (done) => {
-			const middleware = require('../src/middleware');
+		it('should expose res.locals.isAdmin = true', async () => {
 			const reqMock = { user: { uid: adminUid } };
 			const resMock = { locals: {} };
-			middleware.exposeAdmin(reqMock, resMock, () => {
-				assert.strictEqual(resMock.locals.isAdmin, true);
-				done();
+			await new Promise((resolve, reject) => {
+				middleware.exposeAdmin(reqMock, resMock, (err) => {
+					if (err) reject(err);
+					else resolve();
+				});
 			});
+			assert.strictEqual(resMock.locals.isAdmin, true);
 		});
 
-		it('should expose privileges in res.locals.privileges and isSelf=true', (done) => {
-			const middleware = require('../src/middleware');
+		it('should expose privileges in res.locals.privileges and isSelf=true', async () => {
 			const reqMock = { user: { uid: adminUid }, params: { uid: adminUid } };
 			const resMock = { locals: {} };
-			middleware.exposePrivileges(reqMock, resMock, () => {
-				assert(resMock.locals.privileges);
-				assert.strictEqual(resMock.locals.privileges.isAdmin, true);
-				assert.strictEqual(resMock.locals.privileges.isGmod, false);
-				assert.strictEqual(resMock.locals.privileges.isPrivileged, true);
-				assert.strictEqual(resMock.locals.privileges.isSelf, true);
-				done();
+			await new Promise((resolve, reject) => {
+				middleware.exposePrivileges(reqMock, resMock, (err) => {
+					if (err) reject(err);
+					else resolve();
+				});
 			});
+			assert(resMock.locals.privileges);
+			assert.strictEqual(resMock.locals.privileges.isAdmin, true);
+			assert.strictEqual(resMock.locals.privileges.isGmod, false);
+			assert.strictEqual(resMock.locals.privileges.isPrivileged, true);
+			assert.strictEqual(resMock.locals.privileges.isSelf, true);
 		});
 
-		it('should expose privileges in res.locals.privileges and isSelf=false', (done) => {
-			const middleware = require('../src/middleware');
+		it('should expose privileges in res.locals.privileges and isSelf=false', async () => {
 			const reqMock = { user: { uid: 0 }, params: { uid: adminUid } };
 			const resMock = { locals: {} };
-			middleware.exposePrivileges(reqMock, resMock, () => {
-				assert(resMock.locals.privileges);
-				assert.strictEqual(resMock.locals.privileges.isAdmin, false);
-				assert.strictEqual(resMock.locals.privileges.isGmod, false);
-				assert.strictEqual(resMock.locals.privileges.isPrivileged, false);
-				assert.strictEqual(resMock.locals.privileges.isSelf, false);
-				done();
+			await new Promise((resolve, reject) => {
+				middleware.exposePrivileges(reqMock, resMock, (err) => {
+					if (err) reject(err);
+					else resolve();
+				});
 			});
+			assert(resMock.locals.privileges);
+			assert.strictEqual(resMock.locals.privileges.isAdmin, false);
+			assert.strictEqual(resMock.locals.privileges.isGmod, false);
+			assert.strictEqual(resMock.locals.privileges.isPrivileged, false);
+			assert.strictEqual(resMock.locals.privileges.isSelf, false);
 		});
 
-		it('should expose privilege set', (done) => {
-			const middleware = require('../src/middleware');
+		it('should expose privilege set', async () => {
 			const reqMock = { user: { uid: adminUid } };
 			const resMock = { locals: {} };
-			middleware.exposePrivilegeSet(reqMock, resMock, () => {
-				assert(resMock.locals.privileges);
-				assert.deepStrictEqual(resMock.locals.privileges, {
-					chat: true,
-					'chat:privileged': true,
-					'upload:post:image': true,
-					'upload:post:file': true,
-					signature: true,
-					invite: true,
-					'group:create': true,
-					'search:content': true,
-					'search:users': true,
-					'search:tags': true,
-					'view:users': true,
-					'view:tags': true,
-					'view:groups': true,
-					'local:login': true,
-					ban: true,
-					mute: true,
-					'view:users:info': true,
-					'admin:dashboard': true,
-					'admin:categories': true,
-					'admin:privileges': true,
-					'admin:admins-mods': true,
-					'admin:users': true,
-					'admin:groups': true,
-					'admin:tags': true,
-					'admin:settings': true,
-					superadmin: true,
+			await new Promise((resolve, reject) => {
+				middleware.exposePrivilegeSet(reqMock, resMock, (err) => {
+					if (err) reject(err);
+					else resolve();
 				});
-				done();
+			});
+			assert(resMock.locals.privileges);
+			assert.deepStrictEqual(resMock.locals.privileges, {
+				chat: true,
+				'chat:privileged': true,
+				'upload:post:image': true,
+				'upload:post:file': true,
+				signature: true,
+				invite: true,
+				'group:create': true,
+				'search:content': true,
+				'search:users': true,
+				'search:tags': true,
+				'view:users': true,
+				'view:tags': true,
+				'view:groups': true,
+				'local:login': true,
+				ban: true,
+				mute: true,
+				'view:users:info': true,
+				'admin:dashboard': true,
+				'admin:categories': true,
+				'admin:privileges': true,
+				'admin:admins-mods': true,
+				'admin:users': true,
+				'admin:groups': true,
+				'admin:tags': true,
+				'admin:settings': true,
+				superadmin: true,
 			});
 		});
 	});
@@ -116,20 +123,23 @@ describe('Middlewares', () => {
 		});
 
 		it('should be absent on non-existent routes, for guests', async () => {
-			const { response } = await request.get(`${nconf.get('url')}/${utils.generateUUID()}`);
-
+			const { response } = await request.get(
+				`${nconf.get('url')}/${utils.generateUUID()}`
+			);
 			assert.strictEqual(response.statusCode, 404);
 			assert(!Object.keys(response.headers).includes('cache-control'));
 		});
 
 		it('should be set to "private" on non-existent routes, for logged in users', async () => {
-			const { response } = await request.get(`${nconf.get('url')}/${utils.generateUUID()}`, {
-				jar,
-				headers: {
-					accept: 'text/html',
-				},
-			});
-
+			const { response } = await request.get(
+				`${nconf.get('url')}/${utils.generateUUID()}`,
+				{
+					jar,
+					headers: {
+						accept: 'text/html',
+					},
+				}
+			);
 			assert.strictEqual(response.statusCode, 404);
 			assert(Object.keys(response.headers).includes('cache-control'));
 			assert.strictEqual(response.headers['cache-control'], 'private');
@@ -137,21 +147,18 @@ describe('Middlewares', () => {
 
 		it('should be absent on regular routes, for guests', async () => {
 			const { response } = await request.get(nconf.get('url'));
-
 			assert.strictEqual(response.statusCode, 200);
 			assert(!Object.keys(response.headers).includes('cache-control'));
 		});
 
 		it('should be absent on api routes, for guests', async () => {
 			const { response } = await request.get(`${nconf.get('url')}/api`);
-
 			assert.strictEqual(response.statusCode, 200);
 			assert(!Object.keys(response.headers).includes('cache-control'));
 		});
 
 		it('should be set to "private" on regular routes, for logged-in users', async () => {
 			const { response } = await request.get(nconf.get('url'), { jar });
-
 			assert.strictEqual(response.statusCode, 200);
 			assert(Object.keys(response.headers).includes('cache-control'));
 			assert.strictEqual(response.headers['cache-control'], 'private');
@@ -159,19 +166,19 @@ describe('Middlewares', () => {
 
 		it('should be set to "private" on api routes, for logged-in users', async () => {
 			const { response } = await request.get(`${nconf.get('url')}/api`, { jar });
-
 			assert.strictEqual(response.statusCode, 200);
 			assert(Object.keys(response.headers).includes('cache-control'));
 			assert.strictEqual(response.headers['cache-control'], 'private');
 		});
 
 		it('should be set to "private" on apiv3 routes, for logged-in users', async () => {
-			const { response } = await request.get(`${nconf.get('url')}/api/v3/users/${uid}`, { jar });
-
+			const { response } = await request.get(
+				`${nconf.get('url')}/api/v3/users/${uid}`,
+				{ jar }
+			);
 			assert.strictEqual(response.statusCode, 200);
 			assert(Object.keys(response.headers).includes('cache-control'));
 			assert.strictEqual(response.headers['cache-control'], 'private');
 		});
 	});
 });
-
