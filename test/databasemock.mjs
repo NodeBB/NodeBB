01b039adcc93a46ad5077a2e3884c5807e5e8943
Michael Banucu <michael.banucu@googlemail.com>
2025-05-10 19:58:29 +0200

    remove unnecessary log in test

--- test/databasemock.mjs
+++ test/databasemock.mjs
@@ -2,11 +2,9 @@
 
 import './helpers/cleanup.mjs';
 import { doesNotReject } from 'assert';
-import winston from 'winston';
 
 describe('Database Mock', function () {
     it('should not throw when loading the database mock', async function () {
-        winston.verbose('Loading database mock...');
         await doesNotReject(async () => {
             await import('./mocks/databasemock.mjs');
         });
