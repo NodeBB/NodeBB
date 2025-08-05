f8a0a7e194b0acd3242c00ec65a598b0de7d6ef0
Barış Soner Uşaklı <barisusakli@gmail.com>
2025-08-05 18:00:44 -0400

    test: fix notification tests

--- src/socket.io/notifications.js
+++ src/socket.io/notifications.js
@@ -35,7 +35,8 @@ SocketNotifs.markUnread = async function (socket, nid) {
 };
 
 SocketNotifs.markAllRead = async function (socket, data) {
-	await notifications.markAllRead(socket.uid, data.filter);
+	const filter = data && data.filter ? data.filter : '';
+	await notifications.markAllRead(socket.uid, filter);
 	user.notifications.pushCount(socket.uid);
 };
 
