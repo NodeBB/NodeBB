06c38247402948b758c98697129d04899d2bc092
Julian Lam <julian@nodebb.org>
2025-08-13 14:41:44 -0400

    fix: regression caused by cc6fd49c4d2ddc6970ea23011dece5ba91517ec0

--- src/posts/parse.js
+++ src/posts/parse.js
@@ -88,16 +88,9 @@ module.exports = function (Posts) {
 		while (current !== null) {
 			if (current[1]) {
 				try {
-					parsed = new URL(current[1]);
-					if (!parsed.protocol) {
-						if (current[1].startsWith('/')) {
-							// Internal link
-							absolute = nconf.get('base_url') + current[1];
-						} else {
-							// External link
-							absolute = `//${current[1]}`;
-						}
-
+					parsed = new URL(current[1], nconf.get('url'));
+					absolute = parsed.toString();
+					if (absolute !== current[1]) {
 						const offset = current[0].indexOf(current[1]);
 						content = content.slice(0, current.index + offset) +
 						absolute +

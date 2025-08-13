NodeBB/NodeBB

commit cc6fd49c4d2ddc6970ea23011dece5ba91517ec0

fix: protocol-relative URLs being accidentally munged, #13592

src/posts/parse.js
@@ -88,7 +88,7 @@ module.exports = function (Posts) {
 		while (current !== null) {
 			if (current[1]) {
 				try {
-					parsed = url.parse(current[1]);
+					parsed = new URL(current[1]);
 					if (!parsed.protocol) {
 						if (current[1].startsWith('/')) {
 							// Internal link

commit 06c38247402948b758c98697129d04899d2bc092

fix: regression caused by cc6fd49c4d2ddc6970ea23011dece5ba91517ec0

src/posts/parse.js
@@ -88,16 +88,9 @@ module.exports = function (Posts) {
 		while (current !== null) {
 			if (current[1]) {
 				try {
-					parsed = new URL(current[1]);
-					if (!parsed.protocol) {
-						if (current[1].startsWith('/')) {
-							// Internal link
-							absolute = nconf.get('base_url') + current[1];
-						} else {
-							// External link
-							absolute = `//${current[1]}`;
-						}
-
+					parsed = new URL(current[1], nconf.get('url'));
+					absolute = parsed.toString();
+					if (absolute !== current[1]) {
 						const offset = current[0].indexOf(current[1]);
 						content = content.slice(0, current.index + offset) +
 						absolute +

test/posts.js
@@ -763,18 +763,18 @@ describe('Post\'s', () => {
 
 		it('should turn relative links in post body to absolute urls', (done) => {
 			const nconf = require('nconf');
-			const content = '<a href="/users">test</a> <a href="youtube.com">youtube</a>';
+			const content = '<a href="/users">test</a> <a href="//youtube.com">youtube</a>';
 			const parsedContent = posts.relativeToAbsolute(content, posts.urlRegex);
-			assert.equal(parsedContent, `<a href="${nconf.get('base_url')}/users">test</a> <a href="//youtube.com">youtube</a>`);
+			assert.equal(parsedContent, `<a href="${nconf.get('base_url')}/users">test</a> <a href="https://youtube.com/">youtube</a>`);
 			done();
 		});
 
 		it('should turn relative links in post body to absolute urls', (done) => {
 			const nconf = require('nconf');
-			const content = '<a href="/users">test</a> <a href="youtube.com">youtube</a> some test <img src="/path/to/img"/>';
+			const content = '<a href="/users">test</a> <a href="//youtube.com">youtube</a> some test <img src="/path/to/img"/>';
 			let parsedContent = posts.relativeToAbsolute(content, posts.urlRegex);
 			parsedContent = posts.relativeToAbsolute(parsedContent, posts.imgRegex);
-			assert.equal(parsedContent, `<a href="${nconf.get('base_url')}/users">test</a> <a href="//youtube.com">youtube</a> some test <img src="${nconf.get('base_url')}/path/to/img"/>`);
+			assert.equal(parsedContent, `<a href="${nconf.get('base_url')}/users">test</a> <a href="https://youtube.com/">youtube</a> some test <img src="${nconf.get('base_url')}/path/to/img"/>`);
 			done();
 		});
 	});