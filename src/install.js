b153941cf389732b3a93ca30e5e8de65aaae2809
Julian Lam <julian@nodebb.org>
2025-10-09 14:01:08 -0400

    feat: auto-enable link-preview plugin on new installations

--- src/install.js
+++ src/install.js
@@ -534,6 +534,7 @@ async function enableDefaultPlugins() {
 		'nodebb-rewards-essentials',
 		'nodebb-plugin-emoji',
 		'nodebb-plugin-emoji-android',
+		'nodebb-plugin-link-preview',
 	];
 	let customDefaults = nconf.get('defaultplugins') || nconf.get('defaultPlugins');
 
