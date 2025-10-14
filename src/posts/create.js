NodeBB/NodeBB

commit d3b3720915f5846e8f5a8e0bee9c17b3ff233902

refactor: move post attachment handling directly into posts.create

src/posts/create.js
@@ -71,6 +71,8 @@ module.exports = function (Posts) {
 		const topicData = await topics.getTopicFields(tid, ['cid', 'pinned']);
 		postData.cid = topicData.cid;
 
+		const hasAttachment = _activitypub.attachment && _activitypub.attachment.length;
+
 		await Promise.all([
 			db.sortedSetAdd('posts:pid', timestamp, postData.pid),
 			utils.isNumber(pid) ? db.incrObjectField('global', 'postCount') : null,
@@ -79,7 +81,8 @@ module.exports = function (Posts) {
 			categories.onNewPostMade(topicData.cid, topicData.pinned, postData),
 			groups.onNewPostMade(postData),
 			addReplyTo(postData, timestamp),
-			Posts.uploads.sync(postData.pid),
+			Posts.uploads.sync(pid),
+			hasAttachment ? Posts.attachments.update(pid, _activitypub.attachment) : null,
 		]);
 
 		const result = await plugins.hooks.fire('filter:post.get', { post: postData, uid: data.uid });

src/activitypub/notes.js
@@ -210,7 +210,7 @@ Notes.assert = async (uid, input, options = { skipChecks: false }) => {
 		activitypub.helpers.log(`[notes/assert] ${count} new note(s) found.`);
 
 		if (!hasTid) {
-			const { to, cc, attachment } = mainPost._activitypub;
+			const { to, cc } = mainPost._activitypub;
 			const tags = await Notes._normalizeTags(mainPost._activitypub.tag || []);
 
 			try {
@@ -239,7 +239,6 @@ Notes.assert = async (uid, input, options = { skipChecks: false }) => {
 					id: tid,
 					path: mainPost._activitypub.image,
 				}) : null,
-				posts.attachments.update(mainPid, attachment),
 			]);
 
 			if (context) {
@@ -249,16 +248,13 @@ Notes.assert = async (uid, input, options = { skipChecks: false }) => {
 		}
 
 		for (const post of unprocessed) {
-			const { to, cc, attachment } = post._activitypub;
+			const { to, cc } = post._activitypub;
 
 			try {
 				// eslint-disable-next-line no-await-in-loop
 				await topics.reply(post);
 				// eslint-disable-next-line no-await-in-loop
-				await Promise.all([
-					Notes.updateLocalRecipients(post.pid, { to, cc }),
-					posts.attachments.update(post.pid, attachment),
-				]);
+				await Notes.updateLocalRecipients(post.pid, { to, cc });
 			} catch (e) {
 				activitypub.helpers.log(`[activitypub/notes.assert] Could not add reply (${post.pid}): ${e.message}`);
 			}

commit af5efbd71d6ad9bedbf1ad74e1ede162f3a7fe31

fix: regression caused by d3b3720915f5846e8f5a8e0bee9c17b3ff233902

src/posts/create.js
@@ -18,6 +18,7 @@ module.exports = function (Posts) {
 		const content = data.content.toString();
 		const timestamp = data.timestamp || Date.now();
 		const isMain = data.isMain || false;
+		let hasAttachment = false;
 
 		if (!uid && parseInt(uid, 10) !== 0) {
 			throw new Error('[[error:invalid-uid]]');
@@ -46,23 +47,25 @@ module.exports = function (Posts) {
 			if (_activitypub.audience) {
 				postData.audience = _activitypub.audience;
 			}
-		}
 
-		// Rewrite emoji references to inline image assets
-		if (_activitypub && _activitypub.tag && Array.isArray(_activitypub.tag)) {
-			_activitypub.tag
-				.filter(tag => tag.type === 'Emoji' &&
-					tag.icon && tag.icon.type === 'Image')
-				.forEach((tag) => {
-					if (!tag.name.startsWith(':')) {
-						tag.name = `:${tag.name}`;
-					}
-					if (!tag.name.endsWith(':')) {
-						tag.name = `${tag.name}:`;
-					}
+			// Rewrite emoji references to inline image assets
+			if (_activitypub && _activitypub.tag && Array.isArray(_activitypub.tag)) {
+				_activitypub.tag
+					.filter(tag => tag.type === 'Emoji' &&
+								tag.icon && tag.icon.type === 'Image')
+					.forEach((tag) => {
+						if (!tag.name.startsWith(':')) {
+							tag.name = `:${tag.name}`;
+						}
+						if (!tag.name.endsWith(':')) {
+							tag.name = `${tag.name}:`;
+						}
 
-					postData.content = postData.content.replace(new RegExp(tag.name, 'g'), `<img class="not-responsive emoji" src="${tag.icon.url}" title="${tag.name}" />`);
-				});
+						postData.content = postData.content.replace(new RegExp(tag.name, 'g'), `<img class="not-responsive emoji" src="${tag.icon.url}" title="${tag.name}" />`);
+					});
+			}
+
+			hasAttachment = _activitypub && _activitypub.attachment && _activitypub.attachment.length;
 		}
 
 		({ post: postData } = await plugins.hooks.fire('filter:post.create', { post: postData, data: data }));
@@ -70,8 +73,6 @@ module.exports = function (Posts) {
 
 		const topicData = await topics.getTopicFields(tid, ['cid', 'pinned']);
 		postData.cid = topicData.cid;
-
-		const hasAttachment = _activitypub && _activitypub.attachment && _activitypub.attachment.length;
 
 		await Promise.all([
 			db.sortedSetAdd('posts:pid', timestamp, postData.pid),