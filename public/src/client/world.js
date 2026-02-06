f2925aa7ad3e03c10ad3c8700d95fd57b4247168
Julian Lam <julian@nodebb.org>
2026-02-06 14:06:33 -0500

    fix: missing handleIgnoreWatch in world client side js

--- public/src/client/world.js
+++ public/src/client/world.js
@@ -53,6 +53,10 @@ define('forum/world', [
 			});
 		}
 
+		ajaxify.data.categories.forEach(function (category) {
+			handleIgnoreWatch(category.cid);
+		});
+
 		hooks.fire('action:topics.loaded', { topics: ajaxify.data.topics });
 		hooks.fire('action:category.loaded', { cid: ajaxify.data.cid });
 	};
@@ -154,6 +158,37 @@ define('forum/world', [
 				message: content.join('\n'),
 				size: 'large',
 			});
+		});
+	}
+
+	function handleIgnoreWatch(cid) {
+		const category = $('[data-cid="' + cid + '"]');
+		category.find(
+			'[component="category/watching"], [component="category/tracking"], [component="category/ignoring"], [component="category/notwatching"]'
+		).on('click', async (e) => {
+			const state = e.currentTarget.getAttribute('data-state');
+			const { uid } = ajaxify.data;
+
+			const { modified } = await api.put(`/categories/${encodeURIComponent(cid)}/watch`, { state, uid });
+			updateDropdowns(modified, state);
+			alerts.success('[[category:' + state + '.message]]');
+		});
+	}
+
+	function updateDropdowns(modified_cids, state) {
+		modified_cids.forEach(function (cid) {
+			const category = $('[data-cid="' + cid + '"]');
+			category.find('[component="category/watching/menu"]').toggleClass('hidden', state !== 'watching');
+			category.find('[component="category/watching/check"]').toggleClass('fa-check', state === 'watching');
+
+			category.find('[component="category/tracking/menu"]').toggleClass('hidden', state !== 'tracking');
+			category.find('[component="category/tracking/check"]').toggleClass('fa-check', state === 'tracking');
+
+			category.find('[component="category/notwatching/menu"]').toggleClass('hidden', state !== 'notwatching');
+			category.find('[component="category/notwatching/check"]').toggleClass('fa-check', state === 'notwatching');
+
+			category.find('[component="category/ignoring/menu"]').toggleClass('hidden', state !== 'ignoring');
+			category.find('[component="category/ignoring/check"]').toggleClass('fa-check', state === 'ignoring');
 		});
 	}
 
