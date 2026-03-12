08bed89b5f6382e699e33157b49e0b4561c99946
Barış Soner Uşaklı <barisusakli@gmail.com>
2026-03-12 11:10:08 -0400

    test: make tests happy

--- src/activitypub/helpers.js
+++ src/activitypub/helpers.js
@@ -42,11 +42,11 @@ Helpers._test = (method, args) => {
 let _lastLog;
 Helpers.log = (message) => {
 	if (!message) {
-		return _lastLog || 'run in development mode to see last log';
+		return _lastLog;
 	}
-
+	_lastLog = message;
 	if (process.env.NODE_ENV === 'development') {
-		_lastLog = message;
+
 		winston.verbose(message);
 	}
 };
