NodeBB/NodeBB

commit e2131d1d2e1c6f14cb8867ac7e22840da3f4c63f

refactor: /world sorting logic to always use topics/sorted logic
- New params for getSortedTopics (includeRemote, followingOnly)
- Ability to show latest (followers only) or latest (all), ?all query param to discriminate
- World now always shows posts from the local forum
- Popular sort will be followers-only + local

src/topics/sorted.js
@@ -46,7 +46,8 @@ module.exports = function (Topics) {
 		let tids;
 		if (params.term !== 'alltime') {
 			if (params.sort === 'posts') {
-				tids = await getTidsWithMostPostsInTerm(params.cids, params.uid, params.term);
+				const { cids, uid, term, includeRemote, followingOnly } = params;
+				tids = await getTidsWithMostPostsInTerm({ cids, uid, term, includeRemote, followingOnly });
 			} else {
 				const cids = await getCids(params.cids, params.uid);
 				tids = await Topics.getLatestTidsFromSet(
@@ -74,17 +75,18 @@ module.exports = function (Topics) {
 	}
 
 	async function getInbox(tids, params) {
-		if (!Array.isArray(params.cids) || !params.cids.includes('-1')) {
+		if (!params.includeRemote) {
 			return tids;
 		}
 
 		let inbox;
+		const set = params.followingOnly ? `uid:${params.uid}:inbox` : 'cid:-1:tids';
 		if (params.term !== 'alltime') {
 			const method = params.sort === 'old' ?
 				'getSortedSetRangeByScore' :
 				'getSortedSetRevRangeByScore';
 			inbox = await db[method](
-				`uid:${params.uid}:inbox`,
+				set,
 				0,
 				1000,
 				'+inf',
@@ -94,7 +96,7 @@ module.exports = function (Topics) {
 			const method = params.sort === 'old' ?
 				'getSortedSetRange' :
 				'getSortedSetRevRange';
-			inbox = await db[method](`uid:${params.uid}:inbox`, 0, meta.config.recentMaxTopics - 1);
+			inbox = await db[method](set, 0, meta.config.recentMaxTopics - 1);
 		}
 
 		return _.uniq(tids.concat(inbox));
@@ -115,33 +117,41 @@ module.exports = function (Topics) {
 		return 'topics:recent';
 	}
 
-	async function getCids(cids, uid) {
+	async function getCids(cids, uid, includeRemote) {
 		if (Array.isArray(cids)) {
 			cids = await privileges.categories.filterCids('topics:read', cids, uid);
 		} else {
 			cids = await categories.getCidsByPrivilege('categories:cid', uid, 'topics:read');
 			cids = cids.filter(cid => cid !== -1);
 		}
+
+		if (includeRemote) {
+			const remoteCids = await db.getObjectValues('handle:cid');
+			cids = [-1, ...cids, ...remoteCids];
+		}
 		return cids;
 	}
 
-	async function getTidsWithMostPostsInTerm(cids, uid, term) {
-		cids = await getCids(cids, uid);
-		const pids = await db.getSortedSetRevRangeByScore(
-			cids.map(cid => `cid:${cid}:pids`),
+	async function getTidsWithMostPostsInTerm({ cids, uid, term, includeRemote, followingOnly }) {
+		cids = await getCids(cids, uid, includeRemote);
+		const sets = cids.map(cid => `cid:${cid}:tids`);
+		if (followingOnly && sets.includes('cid:-1:tids')) {
+			sets.splice(sets.indexOf('cid:-1:tids'), 1, `uid:${uid}:inbox`);
+		}
+		const tids = await db.getSortedSetRevRangeByScore(
+			sets,
 			0,
 			1000,
 			'+inf',
 			Date.now() - Topics.getSinceFromTerm(term)
 		);
-		const postObjs = await db.getObjectsFields(pids.map(pid => `post:${pid}`), ['tid']);
 		const tidToCount = {};
-		postObjs.forEach((post) => {
-			tidToCount[post.tid] = tidToCount[post.tid] || 0;
-			tidToCount[post.tid] += 1;
+		tids.forEach((tid) => {
+			tidToCount[tid] = tidToCount[tid] || 0;
+			tidToCount[tid] += 1;
 		});
 
-		return _.uniq(postObjs.map(post => String(post.tid)))
+		return _.uniq(tids)
 			.sort((t1, t2) => tidToCount[t2] - tidToCount[t1]);
 	}
 
@@ -200,7 +210,7 @@ module.exports = function (Topics) {
 	}
 
 	async function sortTids(tids, params) {
-		if (params.term === 'alltime' && !params.cids && !params.tags.length && params.filter !== 'watched' && !params.floatPinned) {
+		if (params.term === 'alltime' && !params.cids && !params.tags.length && params.filter !== 'watched' && !params.floatPinned && !params.includeRemote) {
 			return tids;
 		}
 

src/controllers/activitypub/topics.js
@@ -2,7 +2,6 @@
 
 const _ = require('lodash');
 
-const db = require('../../database');
 const meta = require('../../meta');
 const user = require('../../user');
 const topics = require('../../topics');
@@ -20,8 +19,8 @@ controller.list = async function (req, res) {
 	const { topicsPerPage } = await user.getSettings(req.uid);
 	let { page, after } = req.query;
 	page = parseInt(page, 10) || 1;
-	let start = Math.max(0, (page - 1) * topicsPerPage);
-	let stop = start + topicsPerPage - 1;
+	const start = Math.max(0, (page - 1) * topicsPerPage);
+	const stop = start + topicsPerPage - 1;
 
 	const [userSettings, userPrivileges] = await Promise.all([
 		user.getSettings(req.uid),
@@ -50,27 +49,24 @@ controller.list = async function (req, res) {
 	if (req.query.sort === 'popular') {
 		cidQuery = {
 			...cidQuery,
-			cids: ['-1'],
 			sort: 'posts',
 			term: req.query.term || 'day',
+			includeRemote: true,
+			followingOnly: !req.query.all || !parseInt(req.query.all, 10),
 		};
 		delete cidQuery.cid;
 		({ tids, topicCount } = await topics.getSortedTopics(cidQuery));
 		tids = tids.slice(start, stop !== -1 ? stop + 1 : undefined);
 	} else {
-		if (after) {
-			// Update start/stop with values inferred from `after`
-			const set = await categories.buildTopicsSortedSet(cidQuery);
-			const index = await db.sortedSetRevRank(set, decodeURIComponent(after));
-			if (index && start - index < 1) {
-				const count = stop - start;
-				start = index + 1;
-				stop = start + count;
-			}
-		}
-
-		tids = await categories.getTopicIds({ ...cidQuery, start, stop });
-		topicCount = await categories.getTopicCount(cidQuery);
+		cidQuery = {
+			...cidQuery,
+			term: req.query.term,
+			includeRemote: true,
+			followingOnly: !req.query.all || !parseInt(req.query.all, 10),
+		};
+		delete cidQuery.cid;
+		({ tids, topicCount } = await topics.getSortedTopics(cidQuery));
+		tids = tids.slice(start, stop !== -1 ? stop + 1 : undefined);
 	}
 	data.topicCount = topicCount;
 

commit 1af835641e65fa813e99ec47bff5b4aef19f1c09

fix: restored popular calculation behaviour that was broken by e2131d1d2e1c6f14cb8867ac7e22840da3f4c63f, removed followingOnly arg passing for popular

src/topics/sorted.js
@@ -46,8 +46,8 @@ module.exports = function (Topics) {
 		let tids;
 		if (params.term !== 'alltime') {
 			if (params.sort === 'posts') {
-				const { cids, uid, term, includeRemote, followingOnly } = params;
-				tids = await getTidsWithMostPostsInTerm({ cids, uid, term, includeRemote, followingOnly });
+				const { cids, uid, term, includeRemote } = params;
+				tids = await getTidsWithMostPostsInTerm({ cids, uid, term, includeRemote });
 			} else {
 				const cids = await getCids(params.cids, params.uid);
 				tids = await Topics.getLatestTidsFromSet(
@@ -132,26 +132,24 @@ module.exports = function (Topics) {
 		return cids;
 	}
 
-	async function getTidsWithMostPostsInTerm({ cids, uid, term, includeRemote, followingOnly }) {
+	async function getTidsWithMostPostsInTerm({ cids, uid, term, includeRemote }) {
 		cids = await getCids(cids, uid, includeRemote);
-		const sets = cids.map(cid => `cid:${cid}:tids`);
-		if (followingOnly && sets.includes('cid:-1:tids')) {
-			sets.splice(sets.indexOf('cid:-1:tids'), 1, `uid:${uid}:inbox`);
-		}
-		const tids = await db.getSortedSetRevRangeByScore(
+		const sets = cids.map(cid => `cid:${cid}:pids`);
+		const pids = await db.getSortedSetRevRangeByScore(
 			sets,
 			0,
 			1000,
 			'+inf',
 			Date.now() - Topics.getSinceFromTerm(term)
 		);
+		const postObjs = await db.getObjectsFields(pids.map(pid => `post:${pid}`), ['tid']);
 		const tidToCount = {};
-		tids.forEach((tid) => {
-			tidToCount[tid] = tidToCount[tid] || 0;
-			tidToCount[tid] += 1;
+		postObjs.forEach((post) => {
+			tidToCount[post.tid] = tidToCount[post.tid] || 0;
+			tidToCount[post.tid] += 1;
 		});
 
-		return _.uniq(tids)
+		return _.uniq(postObjs.map(post => String(post.tid)))
 			.sort((t1, t2) => tidToCount[t2] - tidToCount[t1]);
 	}
 