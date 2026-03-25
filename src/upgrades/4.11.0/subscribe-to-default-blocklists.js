24bd002996c7d34c1c1368f51986f2b1cb0488a0
Julian Lam <julian@nodebb.org>
2026-03-25 15:19:16 -0400

    fix: avoid db calls in upgrade scripts, just add blocklists to db, no refresh

--- src/upgrades/4.11.0/subscribe-to-default-blocklists.js
+++ src/upgrades/4.11.0/subscribe-to-default-blocklists.js
@@ -1,14 +1,14 @@
 'use strict';
 
-const activitypub = require('../../activitypub');
+const db = require('../../database');
 
 module.exports = {
 	name: 'Subscribe to IFTAS DNI and AUD denylists',
 	timestamp: Date.UTC(2026, 2, 23),
 	method: async () => {
-		await Promise.all([
-			activitypub.blocklists.add('https://about.iftas.org/wp-content/uploads/2025/10/iftas-dni-latest.csv'),
-			activitypub.blocklists.add('https://about.iftas.org/wp-content/uploads/2025/10/iftas-abandoned-unmanaged-latest.csv'),
+		await db.sortedSetAdd('blocklists', [Date.now(), Date.now()], [
+			'https://about.iftas.org/wp-content/uploads/2025/10/iftas-dni-latest.csv',
+			'https://about.iftas.org/wp-content/uploads/2025/10/iftas-abandoned-unmanaged-latest.csv',
 		]);
 	},
 };
