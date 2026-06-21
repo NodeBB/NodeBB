ce224206d371048a63f2c9728348b1a7d0283241
Barış Soner Uşaklı <barisusakli@gmail.com>
2026-06-20 21:34:37 -0400

    test: fix topic test

--- test/categories.js
+++ test/categories.js
@@ -256,7 +256,7 @@ describe('Categories', () => {
 
 			assert.deepStrictEqual(
 				data.topics.map(t => t.title),
-				['This topic is deleted!', 'Test Topic Title', 'Test Topic Title'],
+				['[[topic:topic-is-deleted]]', 'Test Topic Title', 'Test Topic Title'],
 				JSON.stringify(data.topics, null, 2),
 			);
 		});
