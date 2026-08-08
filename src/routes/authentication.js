c408d1dc599ca96fba601718b074aaab6eda84c5
Julian Lam <julian@nodebb.org>
2026-08-08 00:50:00 -0400

    fix: reject SSO auth via callback handler, if state parameter isn't passed in by client (unless checkState is false)

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -113,7 +113,7 @@ Auth.reloadRoutes = async function (params) {
 				return next();
 			}
 
-			next(req.query.state !== req.session.ssoState ? new Error('[[error:csrf-invalid]]') : null);
+			next((!req.session.ssoState || req.query.state !== req.session.ssoState) ? new Error('[[error:csrf-invalid]]') : null);
 		}, (req, res, next) => {
 			// Trigger registration interstitial checks
 			req.session.registration = req.session.registration || {};
