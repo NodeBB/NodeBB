0d073823e966cb18cce3670848e2a736fceff00f
Barış Soner Uşaklı <barisusakli@gmail.com>
2026-08-08 15:26:11 -0400

    fix: closes #14569, prevent socket.emit('admin.xxx') if
    relogin is required

--- public/src/admin/modules/relogin-timer.js
+++ public/src/admin/modules/relogin-timer.js
@@ -1,32 +1,28 @@
-import { translate } from 'translator';
-import { alert as bootboxAlert } from 'bootbox';
+import { alert as modalsAlert } from 'modals';
 
 let logoutTimer = 0;
-let logoutMessage;
-
+let alertShown = false;
 export function start(adminReloginDuration) {
 	clearTimer();
 	if (adminReloginDuration <= 0) {
 		return;
 	}
 
-	// pre-translate language string gh#9046
-	if (!logoutMessage) {
-		translate('[[login:logged-out-due-to-inactivity]]', function (translated) {
-			logoutMessage = translated;
-		});
-	}
-
 	const timeoutMs = adminReloginDuration * 60000;
-	logoutTimer = setTimeout(function () {
-		bootboxAlert({
+	logoutTimer = setTimeout(show, timeoutMs);
+}
+
+export function show() {
+	if (!alertShown) {
+		modalsAlert({
 			closeButton: false,
-			message: logoutMessage,
+			message: '[[login:logged-out-due-to-inactivity]]',
 			callback: function () {
 				window.location.reload();
 			},
 		});
-	}, timeoutMs);
+		alertShown = true;
+	}
 }
 
 function clearTimer() {
