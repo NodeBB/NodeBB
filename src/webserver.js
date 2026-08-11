19ddee278ddc73eecd4eea4763e99844542c52f1
Barış Soner Uşaklı <barisusakli@gmail.com>
2026-08-10 20:59:03 -0400

    refactor: show stack trace of missing store

--- src/webserver.js
+++ src/webserver.js
@@ -41,7 +41,8 @@ app.renderAsync = async (tpl, data) => {
 	if (!Object.hasOwn(data, '_i18n')) {
 		const store = als.getStore();
 		if (!store) {
-			winston.warn(`[app.renderAsync] No ALS store found, unable to determine user language for template rendering, using default language instead.`);
+			const stack = new Error().stack.split('\n')[2];
+			winston.warn(`[app.renderAsync] No ALS store found, unable to determine user language for template rendering, using default language instead. Template: ${tpl}\n${stack}`);
 		}
 		const uid = store?.uid || 0;
 		const { userLang, acpLang } = await user.getSettings(uid);
