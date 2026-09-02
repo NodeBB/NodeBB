c1081a0cdd3b906ad1b2501b0e7a3c45f4ea9256
Barış Soner Uşaklı <barisusakli@gmail.com>
2026-09-02 13:16:06 -0400

    fix: closes #14743, handle uid groupnames in isMember methods
    it can be non-numeric groupname

--- test/groups.js
+++ test/groups.js
@@ -289,6 +289,22 @@ describe('Groups', () => {
 			const isMembers = await Groups.isMemberOfGroups(-1, ['guests', 'registered-users', 'spiders']);
 			assert.deepStrictEqual(isMembers, [false, false, true]);
 		});
+
+		it('should work for group names that start with a 0/-1', async () => {
+			const groupName = '0group';
+			await Groups.create({
+				name: groupName,
+				description: 'group that starts with a 0',
+			});
+			await Groups.join(['parentgroup'], groupName);
+			const [member] = await db.getSortedSetRevRange(`group:parentgroup:members`, 0, -1);
+			assert.strictEqual(member, groupName);
+			assert.strictEqual(await Groups.isMember(groupName, 'parentgroup'), true);
+			assert.deepStrictEqual(await Groups.isMemberOfGroups(groupName, ['parentgroup']), [true]);
+			assert.deepStrictEqual(await Groups.isMembers([groupName], 'parentgroup'), [true]);
+			assert.deepStrictEqual(await Groups.isMembers([groupName], 'guests'), [false]);
+			assert.deepStrictEqual(await Groups.isMembers(['-1something'], 'spiders'), [false]);
+		});
 	});
 
 	describe('.isMemberOfGroupList', () => {
