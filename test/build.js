3c0051edf7257ae033906857854423c9fba6e1d9
Barış Soner Uşaklı <barisusakli@gmail.com>
2026-09-09 16:04:48 -0400

    test: update build test

--- test/build.js
+++ test/build.js
@@ -77,7 +77,7 @@ describe('minifier', () => {
 	it('.css.bundle() should minify styles', (done) => {
 		minifier.css.bundle(styles, paths, true, false, 'ltr', (err, bundle) => {
 			assert.ifError(err);
-			assert.strictEqual(bundle.ltr.code, '.help{margin:10px}.yellow{background:#ff0}.help{display:block}.help .blue{background:#00f}');
+			assert.strictEqual(bundle.ltr.code, '.help{margin:10px}.yellow{background:yellow}.help{display:block}.help .blue{background:blue}');
 			done();
 		});
 	});
